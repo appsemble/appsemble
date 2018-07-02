f756b2c15735da670b109dbe0e45ff0ba4928920
Remco Haszing <remcohaszing@gmail.com>
2018-07-02 16:22:50 +0200

    feat(sdk): provide a default MuiPopover container
    This makes Mui portal elements work in the shadow DOM by default.

--- sdk/mui.jsx
+++ sdk/mui.jsx
@@ -1,6 +1,8 @@
 import {
   createGenerateClassName,
+  createMuiTheme,
   jssPreset,
+  MuiThemeProvider,
 } from '@material-ui/core';
 import {
   create,
@@ -16,23 +18,36 @@ import {
  * A HOC which provides `jss` in a block written in React, in a way that works in the shadow DOM.
  */
 // eslint-disable-next-line import/prefer-default-export
-export function provideJss(Component) {
+export function provideMui(Component) {
   return class extends React.Component {
     static propTypes = {
       reactRoot: PropTypes.instanceOf(HTMLElement).isRequired,
+      shadowRoot: PropTypes.instanceOf(ShadowRoot).isRequired,
     };
 
     render() {
       const {
         reactRoot,
+        shadowRoot,
       } = this.props;
 
       const jss = create(jssPreset());
       jss.options.insertionPoint = reactRoot;
+      const popoverContainer = shadowRoot.appendChild(document.createElement('div'));
 
       return (
         <JssProvider jss={jss} generateClassName={createGenerateClassName()}>
-          <Component {...this.props} />
+          <MuiThemeProvider
+            theme={createMuiTheme({
+              props: {
+                MuiPopover: {
+                  container: () => popoverContainer,
+                },
+              },
+            })}
+          >
+            <Component {...this.props} />
+          </MuiThemeProvider>
         </JssProvider>
       );
     }
