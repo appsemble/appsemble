811424141469df3471fbf0d69c56f6d55d4e3553
Remco Haszing <remcohaszing@gmail.com>
2018-08-03 14:07:51 +0200

    test(stylelint): add selector-max-specifity rule

--- stylelint.config.js
+++ stylelint.config.js
@@ -12,6 +12,7 @@ module.exports = {
     'string-quotes': 'single',
     'value-keyword-case': 'lower',
     'font-family-no-missing-generic-family-keyword': null,
+    'selector-max-specificity': ['0,1,0', { ignoreSelectors: [':global'] }],
     'plugin/no-unsupported-browser-features': true,
   },
 };
