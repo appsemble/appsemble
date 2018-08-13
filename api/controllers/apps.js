d945b19ca5c4fd66f4eb122ea3a6ae374efceee5
Remco Haszing <remcohaszing@gmail.com>
2018-08-13 15:21:00 +0200

    feat: initial database setup

--- api/controllers/apps.js
+++ api/controllers/apps.js
@@ -1,31 +1,44 @@
-const apps = [];
+import {
+  insert,
+  select,
+} from '../utils/db';
+
+
+function rowToJson(row) {
+  return {
+    defaultPage: row.defaultPage,
+    definitions: JSON.parse(row.definitions),
+    id: row.id,
+    name: row.name,
+    pages: JSON.parse(row.pages),
+  };
+}
 
 
 export async function create(ctx) {
   const { body } = ctx.request;
-  apps.push(body);
+
+  const result = await insert('App', body);
   ctx.body = {
     ...body,
-    id: apps.indexOf(body),
+    id: result.insertId,
   };
 }
 
 
 export async function getOne(ctx) {
   const { id } = ctx.params;
-  const app = apps[id];
-  if (app != null) {
-    ctx.body = {
-      ...app,
-      id,
-    };
+  const apps = await select('App', { id });
+  if (apps.length === 0) {
+    ctx.throw(404);
+    return;
   }
+  const [app] = apps;
+  ctx.body = rowToJson(app);
 }
 
 
 export async function query(ctx) {
-  ctx.body = apps.map((app, id) => ({
-    ...app,
-    id,
-  }));
+  const apps = await select('App');
+  ctx.body = apps.map(rowToJson);
 }
---
+++ api/utils/db.mjs
@@ -0,0 +1,42 @@
+import { promisify } from 'util';
+
+import { mapValues, memoize } from 'lodash-es';
+import mysql from 'mysql';
+
+
+export const getPool = memoize(() => {
+  const pool = mysql.createPool({
+    database: process.env.MYSQL_DATABASE || 'appsemble',
+    host: process.env.MYSQL_HOST || 'localhost',
+    password: process.env.MYSQL_ROOT_PASSWORD || 'password',
+    user: process.env.MYSQL_USER || 'root',
+  });
+  return {
+    end: promisify(pool.end.bind(pool)),
+    query: promisify(pool.query.bind(pool)),
+  };
+});
+
+
+export function insert(table, object) {
+  const pool = getPool();
+  const data = mapValues(object, (value) => {
+    if (value instanceof Object) {
+      return JSON.stringify(value);
+    }
+    return value;
+  });
+  return pool.query('INSERT INTO ?? SET ?', [table, data]);
+}
+
+
+export async function select(table, query = {}) {
+  const pool = getPool();
+  const data = mapValues(query, (value) => {
+    if (value instanceof Object) {
+      return JSON.stringify(value);
+    }
+    return value;
+  });
+  return pool.query('SELECT * FROM ?? WHERE ?', [table, data]);
+}
