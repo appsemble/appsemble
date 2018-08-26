9251cbf0de81b36ee84d4ace4f17c9de3a770633
Remco Haszing <remcohaszing@gmail.com>
2018-08-26 11:47:09 +0200

    feat: render real marker data from Amsterdam

--- blocks/map/src/loadMarkers.js
+++ blocks/map/src/loadMarkers.js
@@ -7,17 +7,15 @@ import shadowUrl from 'leaflet/dist/images/marker-shadow.png';
 export default async function loadMarkers(map, actions, resources) {
   const response = await resources.marker.query();
 
-  response.data.forEach(({ data, lat, lon }) => {
-    new Marker([lat, lon], {
+  response.data.forEach((data) => {
+    new Marker([data.latitude, data.longitude], {
       icon: new Icon({
         iconUrl,
         iconRetinaUrl,
         shadowUrl,
       }),
     })
-      .on('click', () => {
-        actions.markerClick.dispatch({ data, lat, lon });
-      })
+      .on('click', actions.markerClick.dispatch.bind(null, data))
       .addTo(map);
   });
 }
