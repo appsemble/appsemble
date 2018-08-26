6f886bb6360e0c908cf3690892420c2297833444
Remco Haszing <remcohaszing@gmail.com>
2018-08-26 12:03:27 +0200

    fix: position markers based on the bottom edge

--- blocks/map/src/loadMarkers.js
+++ blocks/map/src/loadMarkers.js
@@ -1,7 +1,12 @@
+import { Point } from 'leaflet/src/geometry';
 import { Icon, Marker } from 'leaflet/src/layer';
 import iconUrl from 'leaflet/dist/images/marker-icon.png';
 import iconRetinaUrl from 'leaflet/dist/images/marker-icon-2x.png';
 import shadowUrl from 'leaflet/dist/images/marker-shadow.png';
+
+
+const MARKER_ICON_WIDTH = 25;
+const MARKER_ICON_HEIGHT = 41;
 
 
 export default async function loadMarkers(map, actions, resources) {
@@ -12,6 +17,7 @@ export default async function loadMarkers(map, actions, resources) {
       icon: new Icon({
         iconUrl,
         iconRetinaUrl,
+        iconAnchor: new Point(MARKER_ICON_WIDTH / 2, MARKER_ICON_HEIGHT),
         shadowUrl,
       }),
     })
