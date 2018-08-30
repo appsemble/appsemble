3bd826f0566a44cc5fcf63a0f157c0a0006218f1
Remco Haszing <remcohaszing@gmail.com>
2018-08-30 14:59:37 +0200

    feat: add global favicon
    This shows up in several places, might just as well serve one.

--- api/routes/index.js
+++ api/routes/index.js
@@ -7,9 +7,9 @@ import manifestHandler from './manifestHandler';
 
 
 const router = new Router();
+router.get('/:id(\\d+)?/favicon.ico', faviconHandler);
 router.get('/:id(\\d+)/manifest.json', manifestHandler);
 router.get('/:id(\\d+)/icon-:size(\\d+).png', iconHandler);
-router.get('/:id(\\d+)/favicon.ico', faviconHandler);
 router.get('/:id(\\d+)', indexHandler);
 router.get('/:id(\\d+)/:path', indexHandler);
 
