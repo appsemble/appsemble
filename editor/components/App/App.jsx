b0b448c566ac3c9e52b0dffb39675071ea768716
Wessel Kuipers <wessel@d-centralize.nl>
2018-09-05 13:12:00 +0200

    refactor: css layout using flex

--- editor/components/App/App.jsx
+++ editor/components/App/App.jsx
@@ -41,19 +41,19 @@ export default class App extends React.Component {
     return (
       <div className={styles.editor}>
         <div className={styles.leftPanel}>
-          <form onSubmit={this.onSubmit}>
+          <form className={styles.editorForm} onSubmit={this.onSubmit}>
             <div className={styles.editorToolbar}>
               <button type="submit">Save</button>
               { !valid
                 && <p className={styles.editorError}>Invalid YAML</p>
               }
             </div>
-            <textarea className={styles.editor} value={recipe} onChange={this.onChange} />
+            <textarea className={styles.editorText} value={recipe} onChange={this.onChange} />
           </form>
         </div>
 
         <div className={styles.rightPanel}>
-          <iframe title="Appsemble App Preview" ref={(ref) => { this.frame = ref; }} src="/1" />
+          <iframe className={styles.appFrame} title="Appsemble App Preview" ref={(ref) => { this.frame = ref; }} src="/1" />
         </div>
       </div>
     );
