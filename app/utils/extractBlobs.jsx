78c663fd62584cd618facf574c214419c935b4f9
Remco Haszing <remcohaszing@gmail.com>
2018-09-05 14:57:29 +0200

    feat: allow to upload selected blobs from a block
    At this moment, it works with the custom Amsterdam upload API.

---
+++ app/utils/extractBlobs.jsx
@@ -0,0 +1,45 @@
+export const placeholder = Symbol('resourceFiles.placeholder');
+
+
+function defaultReplacer() {
+  return placeholder;
+}
+
+
+/**
+ * Extract blobs from an object.
+ *
+ * This deeply replaces the blobs in the given object using the return value of the replacer
+ * function.
+ *
+ * @param {Object} object The object whose blobs to replace.
+ * @param {Function<Blob>} replacer The function which should be used to determine what to replace a
+ * blob by.
+ * @returns {Array<Object,Array<Blob>>} An array whose first element is the object with the blobs
+ * replaced, and the second argument an array of all extracted blobs.
+ */
+export default function extractBlobs(object, replacer = defaultReplacer) {
+  let result;
+  const files = [];
+  if (object instanceof Blob) {
+    files.push(object);
+    result = replacer(object);
+  } else if (Array.isArray(object)) {
+    result = object.map((value) => {
+      const [nestedResult, nestedFiles] = extractBlobs(value, replacer);
+      files.push(...nestedFiles);
+      return nestedResult;
+    });
+  } else if (object instanceof Object) {
+    result = Object.entries(object)
+      .reduce((acc, [key, value]) => {
+        const [nestedResult, nestedFiles] = extractBlobs(value, replacer);
+        files.push(...nestedFiles);
+        acc[key] = nestedResult;
+        return acc;
+      }, {});
+  } else {
+    result = object;
+  }
+  return [result, files];
+}
