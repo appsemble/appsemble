6c9ce6746a0a48d02724c38f9bf03e57699e16e3
Wessel Kuipers <wessel@d-centralize.nl>
2018-09-10 12:41:51 +0200

    refactor: removed CodeMirror and Ace editors

--- editor/components/App/App.jsx
+++ editor/components/App/App.jsx
@@ -1,23 +1,13 @@
-import 'codemirror/lib/codemirror.css';
-import 'codemirror/theme/material.css';
-import 'codemirror/mode/yaml/yaml';
-
-import AceEditor from 'react-ace';
 import axios from 'axios';
-import 'brace';
-import { Controlled as CodeMirror } from 'react-codemirror2';
 import MonacoEditor from 'react-monaco-editor';
 import yaml from 'js-yaml';
 import React from 'react';
 import styles from './app.css';
-import 'brace/mode/yaml';
-import 'brace/theme/chrome';
 
 export default class App extends React.Component {
   state = {
     recipe: '',
     valid: true,
-    editor: 'Monaco',
   };
 
   frame = null;
@@ -59,7 +49,7 @@ export default class App extends React.Component {
   };
 
   render() {
-    const { recipe, valid, editor } = this.state;
+    const { recipe, valid } = this.state;
 
     return (
       <div className={styles.editor}>
@@ -67,52 +57,18 @@ export default class App extends React.Component {
           <form className={styles.editorForm} onSubmit={this.onSubmit}>
             <div className={styles.editorToolbar}>
               <button type="submit">Save</button>
-              <select onChange={(e) => { this.setState({ editor: e.target.value }); }}>
-                <option value="Monaco">Monaco</option>
-                <option value="Ace">Ace</option>
-                <option value="CodeMirror">CodeMirror</option>
-              </select>
               { !valid
                 && <p className={styles.editorError}>Invalid YAML</p>
               }
             </div>
-
-            {editor === 'Ace'
-            && (
-              <AceEditor
-                mode="yaml"
-                theme="chrome"
-                width="100%"
-                height="500px"
-                className={styles.aceEditor}
-                tabSize={2}
-                value={recipe}
-                onChange={this.onChange}
-              />
-            )}
-
-            {editor === 'Monaco' && (
-              <MonacoEditor
-                language="yaml"
-                theme="vs"
-                value={recipe}
-                className={styles.aceEditor}
-                options={{ tabSize: 2 }}
-                onChange={this.onMonacoChange}
-              />
-            )}
-
-            {editor === 'CodeMirror' && (
-              <CodeMirror
-                value={recipe}
-                className={styles.aceEditor}
-                options={{
-                  tabSize: 2, mode: 'yaml', lineNumbers: true, lint: true,
-                }}
-                onBeforeChange={(e, data, value) => { this.onChange(value); }}
-              />
-            )}
-
+            <MonacoEditor
+              language="yaml"
+              theme="vs"
+              value={recipe}
+              className={styles.monacoEditor}
+              options={{ tabSize: 2 }}
+              onChange={this.onMonacoChange}
+            />
           </form>
         </div>
 
