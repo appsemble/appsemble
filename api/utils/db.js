89e17d417dd83949f35cd733f646d27357007aef
Wessel Kuipers <wessel@d-centralize.nl>
2018-09-20 11:07:08 +0200

    feat(db): added method to initialize database connection

--- api/utils/db.js
+++ api/utils/db.js
@@ -3,8 +3,53 @@ import { promisify } from 'util';
 import { isEmpty, mapValues, memoize } from 'lodash-es';
 import mysql from 'mysql';
 
+import Sequelize from 'sequelize';
+
+
+export const getSequelizePool = memoize(async () => {
+  const connectionString = process.env.DATABASE_URL || 'mysql://root:password@localhost:3306/appsemble';
+  const db = new Sequelize(connectionString);
+
+  try {
+    await db.authenticate();
+    console.log('successfully connected to db');
+  } catch (e) {
+    console.log(e);
+  }
+
+
+  const Apps = db.define('App', {
+    id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
+    definition: { type: Sequelize.JSON, allowNull: false },
+  }, {
+    tableName: 'App2',
+    freezeTableName: true,
+    paranoid: true,
+  });
+
+  await db.sync();
+
+  const model = await db.model('App').findById(1);
+  console.log(model); // returns model with fields that can be retrieved with .get('attribute')
+
+  const model2 = await db.model('App').findById(4);
+  console.log(model2); // does not exist, returns null
+
+  const model3 = await db.model('App').find({ where: { definition: { name: 'TestResult' } } });
+  console.log(model3); // retrieves first record with name 'TestResult' field in its definition
+
+  const { Op } = Sequelize;
+  const model4 = await db.model('App').find({ where: { definition: { id: { [Op.gt]: 2 } } } });
+  // equal to:   await db.model('App').find({ where: { 'definition.id': { [Op.gt]: 2 } } });
+  console.log(model4);
+
+
+  return db;
+});
 
 export const getPool = memoize(() => {
+  const foo = getSequelizePool();
+
   const pool = mysql.createPool({
     database: process.env.MYSQL_DATABASE || 'appsemble',
     host: process.env.MYSQL_HOST || 'localhost',
@@ -12,6 +57,7 @@ export const getPool = memoize(() => {
     port: process.env.MYSQL_PORT || 3306,
     user: process.env.MYSQL_USER || 'root',
   });
+
   return {
     end: promisify(pool.end.bind(pool)),
     query: promisify(pool.query.bind(pool)),
