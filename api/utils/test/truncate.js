3124d5fb81e7549f1855f42c9317ff1a54fdb9c0
Remco Haszing <remcohaszing@gmail.com>
2018-09-25 11:05:24 +0200

    fix(api): wait for all models to be destroyed in truncate

--- api/utils/test/truncate.js
+++ api/utils/test/truncate.js
@@ -3,13 +3,7 @@ import setupModels from '../setupModels';
 export default async function truncate() {
   const models = await setupModels(true);
 
-  return Object.keys(models).map(async (key) => {
-    if (['sequelize', 'Sequelize'].includes(key)) {
-      return null;
-    }
-
-    const result = await models[key].destroy({ where: {}, force: true });
-    console.log(`Truncating ${key}, result from truncate: ${result}`);
-    return result;
-  });
+  await Promise.all(Object.entries(models)
+    .filter(([key]) => /^sequelize$/i.test(key))
+    .map(([, model]) => model.destroy({ where: {}, force: true })));
 }
