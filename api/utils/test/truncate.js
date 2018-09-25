70a3b66fe92c01fc54b66ba22038e3588ad11b7b
Remco Haszing <remcohaszing@gmail.com>
2018-09-25 11:47:57 +0200

    fix(api): reverse regex test in truncate
    Oops……

--- api/utils/test/truncate.js
+++ api/utils/test/truncate.js
@@ -4,6 +4,6 @@ export default async function truncate() {
   const models = await setupModels(true);
 
   await Promise.all(Object.entries(models)
-    .filter(([key]) => /^sequelize$/i.test(key))
+    .filter(([key]) => !/^sequelize$/i.test(key))
     .map(([, model]) => model.destroy({ where: {}, force: true })));
 }
