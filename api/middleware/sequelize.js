4b4a5b44aefd363e52d311fe56e58e0b3cb3764c
Wessel Kuipers <wessel@d-centralize.nl>
2018-09-25 13:29:55 +0200

    fix(api): await db initialization in sequelize middleware
    Apparently I forgot to await this, causing the promise to be stored in the state instead of the result.

--- api/middleware/sequelize.js
+++ api/middleware/sequelize.js
@@ -6,6 +6,6 @@ const db = setupModels(true);
  * Koa middleware for setting up the Sequelize models.
  */
 export default async function sequelize(ctx, next) {
-  ctx.state.db = db;
+  ctx.state.db = await db;
   await next();
 }
