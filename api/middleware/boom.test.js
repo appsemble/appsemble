5d4b07f997c6b43deb513efec83a278df3917839
Wessel Kuipers <wessel@d-centralize.nl>
2018-09-28 16:35:30 +0200

    test: add header testing unit tests for boom middleware

--- api/middleware/boom.test.js
+++ api/middleware/boom.test.js
@@ -1,20 +1,59 @@
 import Boom from 'boom';
+import Koa from 'koa';
+import request from 'supertest';
 
 import boomMiddleware from './boom';
 
 describe('boomMiddleware', () => {
-  it('catches boom errors', () => {
+  it('catches boom errors', async () => {
     const ctx = {};
+
     const mockNext = jest.fn(() => {
       throw Boom.notFound('Error not found');
     });
 
-    boomMiddleware(ctx, mockNext);
+    await boomMiddleware(ctx, mockNext);
 
     expect(ctx.body).toBeDefined();
     expect(ctx.body).toEqual({ statusCode: 404, error: 'Not Found', message: 'Error not found' });
     expect(ctx.status).toBe(404);
+
     expect(mockNext).toHaveBeenCalledTimes(1);
+  });
+
+  it('calls the set method if headers are present', async () => {
+    const ctx = {
+      set: jest.fn((name, value) => {
+        expect(name).toBe('WWW-Authenticate');
+        expect(value).toBe('testKey, testKey2');
+      }),
+    };
+
+    const mockNext = jest.fn(() => {
+      throw Boom.unauthorized('Unauthorized error', ['testKey, testKey2']);
+    });
+
+    await boomMiddleware(ctx, mockNext);
+
+    expect(ctx.body).toBeDefined();
+    expect(ctx.body).toEqual({ statusCode: 401, error: 'Unauthorized', message: 'Unauthorized error' });
+    expect(ctx.status).toBe(401);
+
+    expect(ctx.set).toHaveBeenCalledTimes(1);
+    expect(mockNext).toHaveBeenCalledTimes(1);
+  });
+
+  it('sets Koa headers correctly', async () => {
+    const koa = new Koa();
+    koa.use(boomMiddleware);
+    koa.use(async () => {
+      throw Boom.unauthorized('Not authorized!', 'TestHeader');
+    });
+
+    const response = await request(koa.callback()).get('/');
+
+    expect(response.headers).toBeDefined();
+    expect(response.headers['www-authenticate']).toBe('TestHeader error="Not authorized!"');
   });
 
   it('rethrows non-boom errors', async () => {
--- api/middleware/sequelize.test.js
+++ api/middleware/sequelize.test.js
@@ -4,13 +4,13 @@ describe('sequelizeMiddleware', () => {
   it('should include the database in ctx', async () => {
     const ctx = { state: {} };
 
-    const mockNext = jest.fn(() => {
-      expect(ctx.state.db).toBeDefined();
-      expect(ctx.state.db.sequelize).toBeDefined();
-      expect(ctx.state.db.Sequelize).toBeDefined();
-    });
+    const mockNext = jest.fn();
 
     await sequelizeMiddleware(ctx, mockNext);
     expect(mockNext).toHaveBeenCalledTimes(1);
+
+    expect(ctx.state.db).toBeDefined();
+    expect(ctx.state.db.sequelize).toBeDefined();
+    expect(ctx.state.db.Sequelize).toBeDefined();
   });
 });
