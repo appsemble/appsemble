7dc21a4c48c2863b01d439a76dce0b4d178a5887
Wessel Kuipers <wessel@d-centralize.nl>
2018-10-03 16:33:38 +0200

    refactor(database): change type of data in Asset from BLOB to LONGBLOB
    Allows the user to upload files larger than 64kb

--- api/models/Asset.js
+++ api/models/Asset.js
@@ -2,7 +2,7 @@ export default (sequelize, DataTypes) => sequelize.define('Asset', {
   id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
   mime: { type: DataTypes.STRING, allowNull: false },
   filename: { type: DataTypes.STRING, allowNull: false },
-  data: { type: DataTypes.BLOB, allowNull: false },
+  data: { type: DataTypes.BLOB('long'), allowNull: false },
 }, {
   freezeTableName: true,
   paranoid: true,
