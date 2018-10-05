b28ca63fb483a2613cf50c13b3c70d63fbc77071
Wessel Kuipers <wessel@d-centralize.nl>
2018-10-05 11:09:34 +0200

    test(db): set up and drop schemas for tests

---
+++ api/utils/test/testSchema.js
@@ -0,0 +1,30 @@
+import Sequelize from 'sequelize';
+import SqlString from 'sequelize/lib/sql-string';
+import uuid from 'uuid/v4';
+
+
+import setupModels from '../setupModels';
+
+export default async function testSchema() {
+  const database = process.env.DATABASE_URL || 'mysql://root:password@localhost:3306';
+  const root = new Sequelize(database, {
+    // logging: false,
+    // XXX: This removes a pesky sequelize warning. Remove this when updating to sequelize@^5.
+    operatorsAliases: Sequelize.Op.Aliases,
+  });
+
+  const dbName = SqlString.escape(`appsemble-test-${uuid()}`).replace(/'/g, '').replace(/-/g, '_');
+
+  await root.query(`CREATE SCHEMA IF NOT EXISTS ${dbName}`);
+  const db = await setupModels({ sync: true, database: `${database}/${dbName}` });
+
+  return {
+    ...db,
+    async close() {
+      await db.sequelize.close();
+
+      await root.query(`DROP SCHEMA IF EXISTS ${dbName}`);
+      await root.close();
+    },
+  };
+}
