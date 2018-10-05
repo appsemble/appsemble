633055fb4bc21c5bdd301707d90c06e88d039ecc
Remco Haszing <remcohaszing@gmail.com>
2018-10-05 12:17:10 +0200

    fix(app): fix schema validation tests

--- app/utils/validate.test.jsx
+++ app/utils/validate.test.jsx
@@ -1,8 +1,26 @@
-import validate from './validate';
+import validate, { SchemaValidationError } from './validate';
 
 
-xdescribe('validate', () => {
-  it('should validate a JSON schema error', async () => {
-    await expect(validate({ type: 'object' }, 'string')).rejects;
+describe('validate', () => {
+  it('should validate data against a JSON schema', async () => {
+    let error;
+    try {
+      await validate({ type: 'object' }, 'string');
+    } catch (err) {
+      error = err;
+    }
+    expect(error).toBeInstanceOf(SchemaValidationError);
+    expect(error.message).toBe('Schema Validation Failed');
+    expect(error.data).toEqual({ '': {} });
+  });
+
+  it('should convertg required property errors', async () => {
+    let error;
+    try {
+      await validate({ required: ['skills'] }, {});
+    } catch (err) {
+      error = err;
+    }
+    expect(error.data).toEqual({ skills: {} });
   });
 });
