f084dca0a176c4e3fa4478fa125b556d00cacd8a
Claudia Pinhao <c.pinhao@amsterdam.nl>
2018-10-10 10:29:05 +0200

    refactor: add fonts

--- blocks/form/components/FormBlock/messages.jsx
+++ blocks/form/components/FormBlock/messages.jsx
@@ -1,6 +1,6 @@
 import { defineMessages } from 'react-intl';
 
 export default defineMessages({
-  submit: 'Submit',
+  submit: 'VESTUUR',
   unsupported: 'Field “{name}” of type “{type}” is not supported.',
 });
