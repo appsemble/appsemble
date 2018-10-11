8793b564a6baccee85d34659a2f10f9c7abf67aa
Wessel Kuipers <wessel@d-centralize.nl>
2018-10-11 09:36:04 +0200

    feat: add necessary fields for oauth authentication in db

--- api/models/OAuthAuthorization.js
+++ api/models/OAuthAuthorization.js
@@ -2,7 +2,12 @@ export default (sequelize, DataTypes) =>
   sequelize.define(
     'OAuthAuthorization',
     {
-      id: { type: DataTypes.STRING, primaryKey: true },
+      id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+      token: { type: DataTypes.STRING },
+      tokenExpires: { type: DataTypes.DATE, allowNull: false },
+      clientId: { type: DataTypes.STRING, allowNull: false },
+      refreshToken: { type: DataTypes.STRING, allowNull: false },
+      refreshTokenExpires: { type: DataTypes.DATE, allowNull: false },
       provider: { type: DataTypes.STRING, allowNull: false },
     },
     {
