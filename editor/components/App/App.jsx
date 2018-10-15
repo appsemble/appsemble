f7f6060903215cefac514e0165a79c6c98a18292
Wessel Kuipers <wessel@d-centralize.nl>
2018-10-15 14:04:12 +0200

    feat: add login form to editor

--- editor/components/App/App.jsx
+++ editor/components/App/App.jsx
@@ -1,17 +1,43 @@
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import React from 'react';
+import { connect } from 'react-redux';
+import { IntlProvider } from 'react-intl';
 
 import Editor from '../Editor';
+import EmailLogin from '../../../app/components/EmailLogin';
 
-export default class App extends React.Component {
+const authentication = {
+  url: `${window.location.origin}/oauth/token`,
+  refreshURL: `${window.location.origin}/oauth/token`,
+  clientId: 'appsemble-editor',
+  scope: 'editor',
+};
+
+export class App extends React.Component {
   render() {
+    const { user } = this.props;
+
     return (
-      <Router>
-        <Route
-          path="/editor/:id"
-          render={props => <Editor id={props.match.params.id} {...props} />}
-        />
-      </Router>
+      <IntlProvider locale="en-US" defaultLocale="en-US" textComponent={React.Fragment}>
+        {!user ? (
+          <EmailLogin
+            key="appsemble-editor-email-login"
+            authentication={{
+              method: 'email',
+              ...authentication,
+            }}
+          />
+        ) : (
+          <Router>
+            <Route
+              path="/editor/:id"
+              render={props => <Editor id={props.match.params.id} {...props} />}
+            />
+          </Router>
+        )}
+      </IntlProvider>
     );
   }
 }
+
+export default connect(state => ({ user: state.user }))(App);
--- editor/index.jsx
+++ editor/index.jsx
@@ -1,6 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { applyMiddleware, compose, createStore } from 'redux';
+import thunk from 'redux-thunk';
 
 import App from './components/App';
+import getDb from '../app/utils/getDB';
+import user from '../app/actions/user';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+async function getStore() {
+  const db = await getDb({ id: 'appsemble-editor' });
+
+  const composeEnhancers =
+    // eslint-disable-next-line no-underscore-dangle
+    (process.env.NODE_ENV !== 'production' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+    compose;
+  return createStore(user, { app: { db } }, composeEnhancers(applyMiddleware(thunk)));
+}
+
+getStore().then(store => {
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById('app'),
+  );
+});
