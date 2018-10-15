2d53c2833611a606c610944851d8d3270f78074c
Wessel Kuipers <wessel@d-centralize.nl>
2018-10-15 14:01:07 +0200

    feat(db): add editor as OAuthClient by default

--- api/setupdb.js
+++ api/setupdb.js
@@ -5,7 +5,12 @@ async function main() {
   const database = process.env.DATABASE_URL || 'mysql://root:password@localhost:3306/appsemble';
 
   // Drop the tables related to every model and create them.
-  const { sequelize } = await setupModels({ sync: true, force: true, database });
+  const { sequelize, OAuthClient } = await setupModels({ sync: true, force: true, database });
+  await OAuthClient.create({
+    clientId: 'appsemble-editor',
+    clientSecret: 'appsemble-editor-secret',
+    redirectUri: '/editor',
+  });
   sequelize.close();
 }
 
