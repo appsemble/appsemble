8ba6503a88029dedddc7ec6a7f110a1155af13c1
Wessel Kuipers <wessel@d-centralize.nl>
2018-10-16 11:24:14 +0200

    refactor: revert unused OAuthAuthorization changes

--- api/models/OAuthAuthorization.js
+++ api/models/OAuthAuthorization.js
@@ -2,12 +2,7 @@ export default (sequelize, DataTypes) =>
   sequelize.define(
     'OAuthAuthorization',
     {
-      id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-      token: { type: DataTypes.STRING },
-      tokenExpires: { type: DataTypes.DATE, allowNull: false },
-      clientId: { type: DataTypes.STRING, allowNull: false },
-      refreshToken: { type: DataTypes.STRING, allowNull: false },
-      refreshTokenExpires: { type: DataTypes.DATE, allowNull: false },
+      id: { type: DataTypes.STRING, primaryKey: true },
       provider: { type: DataTypes.STRING, allowNull: false },
     },
     {
