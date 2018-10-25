4644a92a49aa06464ab7df6a8bb99c1f515b0f52
Remco Haszing <remcohaszing@gmail.com>
2018-10-25 14:58:24 +0200

    refactor: replace material-ui based loading screen
    - The app bar is now rendered by the backend. The same app bar is used
      by both the app and the no-script page.
    - A new spinner is created based on Bulma spinners.
    
    This saves an indigo flash which was part of the loading screen.

--- app/components/TitleBar/TitleBar.jsx
+++ app/components/TitleBar/TitleBar.jsx
@@ -1,9 +1,9 @@
-import { Navbar, NavbarBrand, NavbarItem } from '@appsemble/react-bulma';
+import { NavbarBrand, NavbarItem } from '@appsemble/react-bulma';
 import PropTypes from 'prop-types';
 import React from 'react';
 
+import Portal from '../Portal';
 import SideMenuButton from '../SideMenuButton';
-import styles from './TitleBar.css';
 
 /**
  * The title bar on the top of the page.
@@ -23,7 +23,7 @@ export default class TitleBar extends React.Component {
     const { app } = this.props;
 
     return (
-      <Navbar className={styles.root} fixed="top">
+      <Portal element={document.getElementsByClassName('navbar')[0]}>
         <NavbarBrand>
           <NavbarItem>
             <SideMenuButton />
@@ -32,7 +32,7 @@ export default class TitleBar extends React.Component {
             {app?.name || 'Appsemble'}
           </NavbarItem>
         </NavbarBrand>
-      </Navbar>
+      </Portal>
     );
   }
 }
