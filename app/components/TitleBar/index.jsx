c4db22a7cc7f84222ce5a36d47045ae9fddb4cdc
Remco Haszing <remcohaszing@gmail.com>
2018-10-26 16:24:12 +0200

    feat(app): the title bar now contains the page name
    This replaces the previously used app name.

--- app/components/TitleBar/index.jsx
+++ app/components/TitleBar/index.jsx
@@ -1,11 +1,1 @@
-import { connect } from 'react-redux';
-
-import TitleBar from './TitleBar';
-
-function mapStateToProps(state) {
-  return {
-    app: state.app.app,
-  };
-}
-
-export default connect(mapStateToProps)(TitleBar);
+export { default } from './TitleBar';
