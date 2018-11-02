f0c47d462ed0aade8607501e7b4e78656ecee08c
Wessel Kuipers <wessel@d-centralize.nl>
2018-11-02 10:49:57 +0100

    feat: add oauth2handler for facebook

--- api/middleware/oauth2Handlers.js
+++ api/middleware/oauth2Handlers.js
@@ -30,6 +30,23 @@ const oauth2Handlers = {
 
     return null;
   },
+
+  async facebook(token) {
+    const { data } = await axios.get('https://graph.facebook.com/v3.2/me?fields=name,email', {
+      headers: { Authorization: `Bearer ${token.access_token}` },
+    });
+
+    if (data) {
+      return {
+        id: data.id,
+        name: data.name,
+        email: data.email,
+        verified: data.id && data.name && data.email,
+      };
+    }
+
+    return null;
+  },
 };
 
 export default oauth2Handlers;
