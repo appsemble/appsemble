9ee1cd9b8da414e4b9073e94718671cc75ef2066
Wessel Kuipers <wessel@d-centralize.nl>
2018-11-06 16:07:40 +0100

    feat: implement cards with links to each app

--- editor/components/AppList/AppList.jsx
+++ editor/components/AppList/AppList.jsx
@@ -1,13 +1,13 @@
+import '@fortawesome/fontawesome-free/css/all.css';
+
 import axios from 'axios';
 import React from 'react';
 
-import styles from './applist.css';
-
-function chunkArray(items, chunk_size) {
+function chunkArray(items, size) {
   const results = [];
 
   while (items.length) {
-    results.push(items.splice(0, chunk_size));
+    results.push(items.splice(0, size));
   }
 
   return results;
@@ -27,20 +27,44 @@ export default class AppList extends React.Component {
   render() {
     const { apps } = this.state;
     if (!apps) {
-      return <div>Loading...</div>;
+      return <p>Loading...</p>;
     }
 
-    const f = apps.map(app => (
+    if (!apps.length) {
+      return <p>No apps!</p>;
+    }
+
+    const appTiles = apps.map(app => (
       <div key={app.id} className="tile is-parent is-4">
-        <article className="tile is-child box">
-          <figure className="image is-64x64">
-            <img alt="Logo" src={`/${app.id}/icon-64.png`} />
-          </figure>
-          <a href={`/editor/${app.id}`}>{app.name}</a>
-        </article>
+        <div className="card is-child tile">
+          <header className="card-header">
+            <p className="card-header-title">{app.name}</p>
+          </header>
+          <div className="card-content">
+            <div className="content has-text-centered is-centered">
+              <figure className="image is-64x64">
+                <img alt="Logo" src={`/${app.id}/icon-64.png`} style={{ margin: '0 auto' }} />
+              </figure>
+            </div>
+          </div>
+          <footer className="card-footer">
+            <a className="card-footer-item" href={`/${app.path}`}>
+              View
+            </a>
+            <a className="card-footer-item" href={`/editor/${app.id}`}>
+              Edit
+            </a>
+          </footer>
+        </div>
       </div>
     ));
 
-    return chunkArray(f, 3).map(chunk => <div className="tile is-ancestor">{chunk}</div>);
+    const chunkTiles = chunkArray(appTiles, 3).map(chunk => (
+      <div key={`chunk-${chunk[0].name}`} className="tile is-ancestor">
+        {chunk}
+      </div>
+    ));
+
+    return <div className="is-half">{chunkTiles}</div>;
   }
 }
