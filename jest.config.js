e6b62353cfacb946aa8ebe779855e2b47bd70268
Remco Haszing <remcohaszing@gmail.com>
2018-11-22 16:24:07 +0100

    refactor: remove reference to non-existent file mock

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,6 @@ module.exports = {
   collectCoverageFrom: ['blocks/**', 'packages/**', 'api/**', 'app/**'],
   moduleFileExtensions: ['js', 'jsx'],
   moduleNameMapper: {
-    [/\.(gif|jpe?g|png|svg|yaml)$/.source]: '<rootDir>/__mocks__/fileMock.js',
     [/\.css$/.source]: 'identity-obj-proxy',
   },
   snapshotSerializers: ['enzyme-to-json/serializer'],
