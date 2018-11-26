06ee63bf362af12fcdce951e9c62e6a4902178e8
Wessel Kuipers <wessel@d-centralize.nl>
2018-11-26 10:44:33 +0100

    test: include tests for uploading stylesheets

--- api/controllers/apps.test.js
+++ api/controllers/apps.test.js
@@ -81,6 +81,7 @@ describe('app controller', () => {
         JSON.stringify({
           name: 'Test App',
           defaultPage: 'Test Page',
+          path: 'a',
           pages: [
             {
               name: 'Test page',
@@ -92,6 +93,18 @@ describe('app controller', () => {
 
     const { body: retrieved } = await request(server).get(`/api/apps/${created.id}`);
     expect(retrieved).toStrictEqual(created);
+  });
+
+  it('should not allow an upload without an app when creating an app', async () => {
+    const response = await request(server)
+      .post('/api/apps')
+      .set('Authorization', token)
+      .attach('style', Buffer.from('body { color: red; }'), {
+        contentType: 'text/css',
+        filename: 'style.css',
+      });
+
+    expect(response.status).toBe(400);
   });
 
   it('should handle app path conflicts on create', async () => {
@@ -136,6 +149,83 @@ describe('app controller', () => {
       message: 'Another app with path “a” already exists',
       statusCode: 409,
     });
+  });
+
+  it('should allow stylesheets to be included when creating an app', async () => {
+    const response = await request(server)
+      .post('/api/apps')
+      .set('Authorization', token)
+      .field(
+        'app',
+        JSON.stringify({
+          name: 'Foobar',
+          defaultPage: 'Test Page',
+          pages: [
+            {
+              name: 'Test page',
+              blocks: [{ type: 'testblock' }],
+            },
+          ],
+        }),
+      )
+      .attach('style', Buffer.from('body { color: blue; }'), {
+        contentType: 'text/css',
+        filename: 'test.css',
+      });
+    const style = await request(server).get(`/api/apps/${response.body.id}/style/core`);
+
+    expect(response.status).toBe(201);
+    expect(style.status).toBe(200);
+    expect(style.text).toStrictEqual('body { color: blue; }');
+  });
+
+  it('should not allow invalid stylesheets when creating an app', async () => {
+    const responseA = await request(server)
+      .post('/api/apps')
+      .set('Authorization', token)
+      .field(
+        'app',
+        JSON.stringify({
+          name: 'Test App',
+          defaultPage: 'Test Page',
+          path: 'a',
+          pages: [
+            {
+              name: 'Test page',
+              blocks: [{ type: 'testblock' }],
+            },
+          ],
+        }),
+      )
+      .attach('style', Buffer.from('this is invalid css'), {
+        contentType: 'text/css',
+        filename: 'test.css',
+      });
+
+    const responseB = await request(server)
+      .post('/api/apps')
+      .set('Authorization', token)
+      .field(
+        'app',
+        JSON.stringify({
+          name: 'Test App',
+          defaultPage: 'Test Page',
+          path: 'a',
+          pages: [
+            {
+              name: 'Test page',
+              blocks: [{ type: 'testblock' }],
+            },
+          ],
+        }),
+      )
+      .attach('style', Buffer.from('.foo { margin: 0 auto; }'), {
+        contentType: 'application/json',
+        filename: 'test.css',
+      });
+
+    expect(responseA.status).toBe(400);
+    expect(responseB.status).toBe(400);
   });
 
   it('should not update a non-existent app', async () => {
@@ -220,6 +310,23 @@ describe('app controller', () => {
     expect(response.status).toBe(400);
   });
 
+  it('should not allow an upload without an app when updating an app', async () => {
+    const app = await App.create(
+      { path: 'test-app', definition: { name: 'Test App', defaultPage: 'Test Page' } },
+      { raw: true },
+    );
+
+    const response = await request(server)
+      .put(`/api/apps/${app.id}`)
+      .set('Authorization', token)
+      .attach('style', Buffer.from('body { color: red; }'), {
+        contentType: 'text/css',
+        filename: 'style.css',
+      });
+
+    expect(response.status).toBe(400);
+  });
+
   it('should prevent path conflicts when updating an app', async () => {
     await App.create(
       { path: 'foo', definition: { name: 'Test App', defaultPage: 'Test Page' } },
@@ -248,5 +355,94 @@ describe('app controller', () => {
       );
 
     expect(response.status).toBe(409);
+  });
+
+  it('should validate and update css when updating an app', async () => {
+    const app = await App.create(
+      { path: 'bar', definition: { name: 'Test App', defaultPage: 'Test Page' } },
+      { raw: true },
+    );
+
+    const response = await request(server)
+      .put(`/api/apps/${app.id}`)
+      .set('Authorization', token)
+      .field(
+        'app',
+        JSON.stringify({
+          path: 'foo',
+          name: 'Foobar',
+          defaultPage: app.definition.defaultPage,
+          pages: [
+            {
+              name: 'Test page',
+              blocks: [{ type: 'testblock' }],
+            },
+          ],
+        }),
+      )
+      .attach('style', Buffer.from('body { color: yellow; }'), {
+        contentType: 'text/css',
+        filename: 'style.css',
+      });
+
+    const style = await request(server).get(`/api/apps/${response.body.id}/style/core`);
+
+    expect(response.status).toBe(200);
+    expect(style.status).toBe(200);
+    expect(style.text).toStrictEqual('body { color: yellow; }');
+  });
+
+  it('should not allow invalid stylesheets when updating an app', async () => {
+    const app = await App.create(
+      { path: 'bar', definition: { name: 'Test App', defaultPage: 'Test Page' } },
+      { raw: true },
+    );
+
+    const responseA = await request(server)
+      .put(`/api/apps/${app.id}`)
+      .set('Authorization', token)
+      .field(
+        'app',
+        JSON.stringify({
+          name: 'Test App',
+          defaultPage: 'Test Page',
+          path: 'a',
+          pages: [
+            {
+              name: 'Test page',
+              blocks: [{ type: 'testblock' }],
+            },
+          ],
+        }),
+      )
+      .attach('style', Buffer.from('this is invalid css'), {
+        contentType: 'text/css',
+        filename: 'style.css',
+      });
+
+    const responseB = await request(server)
+      .put(`/api/apps/${app.id}`)
+      .set('Authorization', token)
+      .field(
+        'app',
+        JSON.stringify({
+          name: 'Test App',
+          defaultPage: 'Test Page',
+          path: 'a',
+          pages: [
+            {
+              name: 'Test page',
+              blocks: [{ type: 'testblock' }],
+            },
+          ],
+        }),
+      )
+      .attach('style', Buffer.from('.foo { margin: 0 auto; }'), {
+        contentType: 'application/json',
+        filename: 'style.json',
+      });
+
+    expect(responseA.status).toBe(400);
+    expect(responseB.status).toBe(400);
   });
 });
