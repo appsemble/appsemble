7f36e0358c7b4ec1ceb701fae8dd579d213bd6a6
Wessel Kuipers <wessel@d-centralize.nl>
2018-12-05 09:12:56 +0100

    test: increase timeout for async jest tests

--- config/jest/setupTestFramework.js
+++ config/jest/setupTestFramework.js
@@ -2,3 +2,6 @@ import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
+
+// eslint-disable-next-line no-undef
+jest.setTimeout(10000);
