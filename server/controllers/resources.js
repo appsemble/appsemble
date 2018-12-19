c5ccc9fd269e9d8f4aa08136e0a0d2c403f47e0c
Wessel Kuipers <wessel@d-centralize.nl>
2018-12-19 15:30:16 +0100

    feat(server): implement OData-based filtering

--- server/controllers/resources.js
+++ server/controllers/resources.js
@@ -1,5 +1,6 @@
 import Boom from 'boom';
 import validate, { SchemaValidationError } from '@appsemble/utils/validate';
+import parseOData from 'odata-sequelize';
 
 function verifyResourceDefinition(app, resourceType) {
   if (!app) {
@@ -13,17 +14,67 @@ function verifyResourceDefinition(app, resourceType) {
   if (!app.definition.definitions[resourceType]) {
     throw Boom.notFound(`App does not have resources called ${resourceType}`);
   }
+
+  return app.definition.definitions[resourceType];
 }
 
+function generateQuery(ctx) {
+  if (ctx.querystring) {
+    try {
+      return parseOData(decodeURIComponent(ctx.querystring), ctx.db);
+    } catch (e) {
+      return {};
+    }
+  }
+
+  return {};
+}
+
+const deepRename = (object, keys) => {
+  if (!object) {
+    return {};
+  }
+
+  const obj = Array.isArray(object) ? [...object] : { ...object };
+  Object.entries(obj).forEach(([key, value]) => {
+    if (keys.some(k => k === key)) {
+      obj[`data.${key}`] = value;
+      delete obj[key];
+    }
+
+    if (!!object[key] && (object[key] instanceof Object || Array.isArray(object[key]))) {
+      obj[key] = deepRename(obj[key], keys);
+    }
+  });
+
+  return obj;
+};
+
 export async function getAll(ctx) {
+  const query = generateQuery(ctx);
+  console.log(query);
   const { appId, resourceType } = ctx.params;
   const { App } = ctx.db.models;
 
   const app = await App.findByPk(appId);
-  verifyResourceDefinition(app, resourceType);
+  const keys = Object.keys(verifyResourceDefinition(app, resourceType).properties);
+  const renamedQuery = deepRename(query, keys);
+  console.log(renamedQuery);
 
-  const resources = await app.getResources({ type: resourceType });
-  ctx.body = resources.map(resource => ({ id: resource.id, ...resource.data }));
+  try {
+    const resources = await app.getResources({
+      ...renamedQuery,
+      type: resourceType,
+    });
+
+    ctx.body = resources.map(resource => ({ id: resource.id, ...resource.data }));
+  } catch (e) {
+    if (query) {
+      throw Boom.badRequest('Unable to process this query');
+    }
+
+    throw e;
+  }
 }
 
 export async function getOne(ctx) {
@@ -33,7 +84,7 @@ export async function getOne(ctx) {
   const app = await App.findByPk(appId);
   verifyResourceDefinition(app, resourceType);
 
-  const [resource] = await app.getResources({ where: { id: resourceId } });
+  const [resource] = await app.getResources({ where: { id: resourceId }, raw: true });
 
   if (!resource) {
     throw Boom.notFound('Resource not found');
