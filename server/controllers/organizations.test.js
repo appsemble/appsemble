829b7462e6addc168417188a3aa4e7292a8fb82a
Wessel Kuipers <wessel@d-centralize.nl>
2019-01-08 09:12:44 +0100

    test(server): include organization block style tests

--- server/controllers/organizations.test.js
+++ server/controllers/organizations.test.js
@@ -5,7 +5,9 @@ import truncate from '../utils/test/truncate';
 import testSchema from '../utils/test/testSchema';
 
 describe('organization controller', () => {
+  let BlockDefinition;
   let Organization;
+  let OrganizationBlockStyle;
   let db;
   let server;
   let organizationId;
@@ -14,7 +16,7 @@ describe('organization controller', () => {
     db = await testSchema('organizations');
 
     server = await createServer({ db });
-    ({ Organization } = db.models);
+    ({ BlockDefinition, Organization, OrganizationBlockStyle } = db.models);
   });
 
   beforeEach(async () => {
@@ -164,6 +166,123 @@ describe('organization controller', () => {
 
   it('should return an empty response on non-existant core stylesheets', async () => {
     const response = await request(server).get(`/api/organizations/${organizationId}/style/core`);
+
+    expect(response.text).toBe('');
+    expect(response.type).toBe('text/css');
+    expect(response.status).toBe(200);
+  });
+
+  it('should validate and update block stylesheets when uploading block stylesheets for an organization', async () => {
+    await BlockDefinition.create({
+      id: '@appsemble/testblock',
+      description: 'This is a test block for testing purposes.',
+    });
+
+    const responseA = await request(server)
+      .post(`/api/organizations/${organizationId}/style/block/@appsemble/testblock`)
+      .attach('style', Buffer.from('body { color: blue; }'), {
+        contentType: 'text/css',
+        filename: 'style.css',
+      });
+
+    const responseB = await request(server).get(
+      `/api/organizations/${organizationId}/style/block/@appsemble/testblock`,
+    );
+
+    expect(responseA.status).toBe(204);
+    expect(responseB.status).toBe(200);
+    expect(responseB.text).toStrictEqual('body { color: blue; }');
+  });
+
+  it('should set block stylesheets to null when uploading empty stylesheets for an organization', async () => {
+    await BlockDefinition.create({
+      id: '@appsemble/testblock',
+      description: 'This is a test block for testing purposes.',
+    });
+
+    const responseA = await request(server)
+      .post(`/api/organizations/${organizationId}/style/block/@appsemble/testblock`)
+      .attach('style', Buffer.from('body { color: blue; }'), {
+        contentType: 'text/css',
+        filename: 'style.css',
+      });
+
+    const responseB = await request(server)
+      .post(`/api/organizations/${organizationId}/style/block/@appsemble/testblock`)
+      .attach('style', Buffer.from(' '), {
+        contentType: 'text/css',
+        filename: 'style.css',
+      });
+
+    const style = await OrganizationBlockStyle.findOne({
+      where: { OrganizationId: organizationId, BlockDefinitionId: '@appsemble/testblock' },
+    });
+
+    expect(responseA.status).toBe(204);
+    expect(responseB.status).toBe(204);
+    expect(style.style).toBeNull();
+  });
+
+  it('should not allow invalid stylesheets when uploading block stylesheets to an organization', async () => {
+    await BlockDefinition.create({
+      id: '@appsemble/testblock',
+      description: 'This is a test block for testing purposes.',
+    });
+
+    const response = await request(server)
+      .post(`/api/organizations/${organizationId}/style/block/@appsemble/testblock`)
+      .attach('style', Buffer.from('invalidCss'));
+    expect(response.body).toStrictEqual({
+      statusCode: 400,
+      error: 'Bad Request',
+      message: 'Provided CSS was invalid.',
+    });
+  });
+
+  it('should not allow uploading block stylesheets to non-existant organizations', async () => {
+    await BlockDefinition.create({
+      id: '@appsemble/testblock',
+      description: 'This is a test block for testing purposes.',
+    });
+
+    const response = await request(server)
+      .post(`/api/organizations/0/style/block/@appsemble/testblock`)
+      .attach('style', Buffer.from('body { color: red; }'), {
+        contentType: 'text/css',
+        filename: 'style.css',
+      });
+
+    expect(response.body).toStrictEqual({
+      statusCode: 404,
+      error: 'Not Found',
+      message: 'Organization not found.',
+    });
+  });
+
+  it('should not allow uploading block stylesheets for non-existant blocks', async () => {
+    await BlockDefinition.create({
+      id: '@appsemble/testblock',
+      description: 'This is a test block for testing purposes.',
+    });
+
+    const response = await request(server)
+      .post(`/api/organizations/${organizationId}/style/block/@appsemble/doesntexist`)
+      .attach('style', Buffer.from('body { color: red; }'), {
+        contentType: 'text/css',
+        filename: 'style.css',
+      });
+
+    expect(response.body).toStrictEqual({
+      statusCode: 404,
+      error: 'Not Found',
+      message: 'Block not found.',
+    });
+  });
+
+  it('should return an empty response on non-existant block stylesheets', async () => {
+    const response = await request(server).get(
+      `/api/organizations/${organizationId}/style/block/@appsemble/doesntexist`,
+    );
 
     expect(response.text).toBe('');
     expect(response.type).toBe('text/css');
