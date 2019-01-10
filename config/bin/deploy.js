a11d62d7926a0e13f69bf6b9f246512639658088
Remco Haszing <remcohaszing@gmail.com>
2019-01-10 10:48:22 +0100

    ci: add logging to deploy script

--- config/bin/deploy.js
+++ config/bin/deploy.js
@@ -49,20 +49,26 @@ async function check() {
  * Wait until the Appsemble server is up and running.
  *
  * @param {Object} options
- * @param {Number} options.retries How often to check if the API is up and running.
+ * @param {Number} options.tries How often to check if the API is up and running.
  * @param {Number} options.interval How long to wait between checks in milliseconds.
  */
 async function waitForServer({ tries, interval }) {
   for (let i = 0; i < tries; i += 1) {
     try {
+      // eslint-disable-next-line no-console
+      console.log(`Checking for API status. Try ${i}.`);
       // eslint-disable-next-line no-await-in-loop
       await check();
     } catch (err) {
+      // eslint-disable-next-line no-console
+      console.log(`The API is down. Retrying in ${interval} milliseconds…`);
       // eslint-disable-next-line no-await-in-loop
       await sleep(interval);
       // eslint-disable-next-line no-continue
       continue;
     }
+    // eslint-disable-next-line no-console
+    console.log('The API is up!');
     break;
   }
 }
