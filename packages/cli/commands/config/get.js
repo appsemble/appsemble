700a84086298290374b0f465bf924998c92fec86
Remco Haszing <remcohaszing@gmail.com>
2019-01-10 12:07:01 +0100

    feat(cli): add config API
    This is a simple API for setting and getting appsembleServer properties
    from a project package.json.

---
+++ packages/cli/commands/config/get.js
@@ -0,0 +1,18 @@
+import readPkgUp from 'read-pkg-up';
+
+export const command = 'get <key>';
+export const description = 'Get an Appsemble configuration option from package.json.';
+
+export function builder(yargs) {
+  return yargs.positional('key', {
+    describe: 'The key whose value to get',
+  });
+}
+
+export async function handler({ key }) {
+  const { pkg } = await readPkgUp({ normalize: false });
+  if (Object.prototype.hasOwnProperty.call(pkg, 'appsembleServer')) {
+    // eslint-disable-next-line no-console
+    console.log(pkg.appsembleServer[key]);
+  }
+}
