4b34534cb19437d87e865becaf8a17f0cb53861e
Remco Haszing <remcohaszing@gmail.com>
2019-01-23 14:50:44 +0100

    ci: add OAuth variables on master branch
    This should enable login using GitLab or Google on
    staging.appsemble.com.

--- config/kubernetes/appsembleDeployment.js
+++ config/kubernetes/appsembleDeployment.js
@@ -2,6 +2,7 @@ const {
   CI_COMMIT_REF_NAME,
   CI_ENVIRONMENT_SLUG,
   CI_ENVIRONMENT_URL,
+  CI_PROJECT_PATH,
   CI_REGISTRY_IMAGE,
 } = process.env;
 
@@ -82,7 +83,29 @@ export default {
                 name: 'HOST',
                 value: CI_ENVIRONMENT_URL,
               },
-            ],
+            ].concat(
+              CI_PROJECT_PATH === 'dcentralized/appsemble/appsemble' &&
+                CI_COMMIT_REF_NAME === 'master'
+                ? [
+                    {
+                      name: 'OAUTH_KEY_SECRET',
+                      valueFrom: { secretKeyRef: { name: 'gitlab', key: 'key' } },
+                    },
+                    {
+                      name: 'OAUTH_GITLAB_SECRET',
+                      valueFrom: { secretKeyRef: { name: 'gitlab', key: 'secret' } },
+                    },
+                    {
+                      name: 'OAUTH_GOOGLE_KEY',
+                      valueFrom: { secretKeyRef: { name: 'google', key: 'key' } },
+                    },
+                    {
+                      name: 'OAUTH_GOOGLE_KEY',
+                      valueFrom: { secretKeyRef: { name: 'google', key: 'secret' } },
+                    },
+                  ]
+                : [],
+            ),
             ports: [{ containerPort: 9999 }],
             resources: {
               requests: {
