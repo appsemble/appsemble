fb60133e5944693de6eb5fdc4865a98ba2ee4e51
Wessel Kuipers <wessel@d-centralize.nl>
2019-01-24 16:34:10 +0100

    feat(create-appsemble): include createReact template

---
+++ packages/create-appsemble/lib/create/react.js
@@ -0,0 +1,19 @@
+import path from 'path';
+
+import fs from 'fs-extra';
+import sortPackageJson from 'sort-package-json';
+
+export default async function react(outputPath, pkg) {
+  const { appsemble, dependencies } = await fs.readJSON(
+    path.resolve(__dirname, '../../templates/react/package.json'),
+  );
+
+  const outPkg = sortPackageJson({
+    ...pkg,
+    appsemble: { ...pkg.appsemble, ...appsemble },
+    dependencies: { ...pkg.dependencies, ...dependencies },
+  });
+
+  await fs.copy(path.resolve(__dirname, '../../templates/react'), outputPath);
+  await fs.outputJSON(path.join(outputPath, 'package.json'), outPkg, { spaces: 2 });
+}
