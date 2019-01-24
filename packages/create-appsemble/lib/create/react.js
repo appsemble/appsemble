9cac200ebac025e954320674a57a8b31ad058aa0
Wessel Kuipers <wessel@d-centralize.nl>
2019-01-24 16:36:11 +0100

    refactor(create-appsemble): use .appsemblerc.yaml instead of package.json

--- packages/create-appsemble/lib/create/react.js
+++ packages/create-appsemble/lib/create/react.js
@@ -4,13 +4,12 @@ import fs from 'fs-extra';
 import sortPackageJson from 'sort-package-json';
 
 export default async function react(outputPath, pkg) {
-  const { appsemble, dependencies } = await fs.readJSON(
+  const { dependencies } = await fs.readJSON(
     path.resolve(__dirname, '../../templates/react/package.json'),
   );
 
   const outPkg = sortPackageJson({
     ...pkg,
-    appsemble: { ...pkg.appsemble, ...appsemble },
     dependencies: { ...pkg.dependencies, ...dependencies },
   });
 
