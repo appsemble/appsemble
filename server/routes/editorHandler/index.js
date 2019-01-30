e9d8384d4b1afe861b68a2611c645e9079e15fa0
Remco Haszing <remcohaszing@gmail.com>
2019-01-30 11:18:07 +0100

    fix: fix serving index.html in production
    This time for real.

--- server/routes/editorHandler/index.js
+++ server/routes/editorHandler/index.js
@@ -1,24 +1,24 @@
+import fs from 'fs';
 import path from 'path';
-
-import fs from 'fs-extra';
 
 /**
  * Serve `index.html` for editor related routes.
  */
 export default async function editorHandler(ctx) {
-  const { fs } = ctx.state;
-  if (process.env.NODE_ENV === 'production') {
-    ctx.body = await fs.readFile(path.resolve(__dirname, '../../../dist/index.html'))
-  } else {
-    ctx.body = await new Promise((resolve, reject) => {
-      fs.readFile('/index.html', (err, buffer) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(buffer);
-        }
-      });
-    });
+  let filesystem = fs;
+  let filename = path.resolve(__dirname, '../../../dist/index.html');
+  if (process.env.NODE_ENV !== 'production') {
+    filesystem = ctx.state.fs;
+    filename = '/index.html';
   }
+  ctx.body = await new Promise((resolve, reject) => {
+    filesystem.readFile(filename, (err, buffer) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(buffer);
+      }
+    });
+  });
   ctx.type = 'text/html';
 }
