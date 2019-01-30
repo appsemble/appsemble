906d7e15b687d42b40cfeb8f5a63b166fc9b92ac
Wessel Kuipers <wessel@d-centralize.nl>
2019-01-30 16:08:03 +0100

    docs: add type definitions for Appsemble SDK

---
+++ packages/sdk/index.d.ts
@@ -0,0 +1,87 @@
+/**
+ * An action that can be called from within a block.
+ * @typedef {Actions} Action
+ * @property {Function} dispatch A function which can be called to dispatch the action.
+ * @property {string} type The type of the action.
+ */
+export interface Action {
+  dispatch: Function;
+  type: string;
+}
+
+/**
+ * A block that is displayed on a page.
+ */
+export interface Block {
+  /**
+   * The type of the block.
+   *
+   * A block type follow the format `@organization/name`.
+   * If the organization is _appsemble_, it may be omitted.
+   *
+   * Pattern:
+   * ^(@[a-z]([a-z\d-]{0,30}[a-z\d])?\/)?[a-z]([a-z\d-]{0,30}[a-z\d])$
+   *
+   * Examples:
+   * - `form`
+   * - `@amsterdam/splash`
+   */
+  type: string;
+
+  /**
+   * A [semver](https://semver.org) representation of the block version.
+   *
+   * Pattern:
+   * ^\d+\.\d+\.\d+$
+   */
+  version: string;
+
+  /**
+   * A free form mapping of named paramters.
+   *
+   * The exact meaning of the parameters depends on the block type.
+   */
+  parameters?: Object;
+
+  /**
+   * A mapping of actions that can be fired by the block to action handlers.
+   *
+   * The exact meaning of the parameters depends on the block type.
+   * */
+  actions?: { type: Function };
+}
+
+/**
+ * The parameters that get passed to the bootstrap function.
+ */
+export interface BootstrapParams {
+  /** The actions that may be dispatched by the block. */
+  actions: { [key: string]: Action };
+
+  /** The block as it is defined in the app definition. */
+  block: Block;
+
+  /** Any kind of data that has been passed in by some context. */
+  data: Object;
+
+  /** The shadow root to which DOM elements may be appended. */
+  shadowRoot: ShadowRoot;
+}
+
+/**
+ * Register a boostrap function.
+ *
+ * @param fn The bootstrap function to register
+ */
+export function bootstrap(fn: Function<BootstrapParams>): void;
+
+/**
+ * Attach the returned node to the shadow root.
+ *
+ * This convenience wrapper attaches nodes returned by the bootstrap function to the shadow root.
+ * This means that the initialization function for a block simply has to return a node, or an
+ * iterator yielding nodes.
+ *
+ * @param fn The bootstrap function to register.
+ */
+export function attach(fn: (_: BootstrapParams) => void): void;
