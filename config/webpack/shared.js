77c1c06f51455bae2c491cb884d58bd13efa9036
Remco Haszing <remcohaszing@gmail.com>
2019-03-09 12:29:37 +0100

    Revert "feat: implement ts-loader in webpack for packages that use typescript"
    This reverts commit eae2ecce285b6de155514343f5dcd43fb70f3e50.

--- config/webpack/shared.js
+++ config/webpack/shared.js
@@ -13,7 +13,7 @@ module.exports = (env, { mode }) => {
 
   return {
     resolve: {
-      extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      extensions: ['.js', '.jsx', 'ts', 'tsx'],
       alias: {
         // These are required by leaflet CSS in a way which doesn’t work with webpack by default.
         './images/layers.png$': 'leaflet/dist/images/layers.png',
@@ -67,7 +67,6 @@ module.exports = (env, { mode }) => {
             },
           ],
         },
-        { test: /\.tsx?$/, loader: 'ts-loader' },
       ],
     },
     plugins: [new CaseSensitivePathsPlugin()],
