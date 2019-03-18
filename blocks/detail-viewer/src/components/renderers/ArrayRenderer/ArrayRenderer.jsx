f98507ad81db8133dcf28a9fa0a85cee1910d89a
Wessel Kuipers <wessel@d-centralize.nl>
2019-03-18 16:27:55 +0100

    update label rendering for ArrayRenderer

--- blocks/detail-viewer/src/components/renderers/ArrayRenderer/ArrayRenderer.jsx
+++ blocks/detail-viewer/src/components/renderers/ArrayRenderer/ArrayRenderer.jsx
@@ -1,4 +1,4 @@
-import { Container } from '@appsemble/react-bulma';
+import { Container, Label } from '@appsemble/react-bulma';
 import PropTypes from 'prop-types';
 import React from 'react';
 import { remapData } from '@appsemble/utils/remap';
@@ -45,22 +45,26 @@ export default class ArrayRenderer extends React.Component {
   render() {
     const { field, value, block, data } = this.props;
     const Component = renderers[field.arrayType] || renderers.string;
+    console.log(field);
 
     return (
-      <Container className={styles.root}>
-        {(value || []).map((v, index) => {
-          return (
-            <Component
-              // eslint-disable-next-line react/no-array-index-key
-              key={index}
-              block={block}
-              data={data}
-              field={field}
-              value={field.arrayName ? remapData(field.arrayName, v) : v}
-            />
-          );
-        })}
-      </Container>
+      <React.Fragment>
+        {field.label && <Label>{field.label}</Label>}
+        <Container className={styles.root}>
+          {(value || []).map((v, index) => {
+            return (
+              <Component
+                // eslint-disable-next-line react/no-array-index-key
+                key={index}
+                block={block}
+                data={data}
+                field={{ name: field.arrayName || undefined }}
+                value={field.arrayName ? remapData(field.arrayName, v) : v}
+              />
+            );
+          })}
+        </Container>
+      </React.Fragment>
     );
   }
 }
