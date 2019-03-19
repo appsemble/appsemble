81ce776626c38c74e632d795b31b4dd8ef666651
Wessel Kuipers <wessel@d-centralize.nl>
2019-03-19 16:33:59 +0100

    remove basic react-bulma components

--- packages/react-bulma/base/index.jsx
+++ packages/react-bulma/base/index.jsx
@@ -8,8 +8,7 @@ export { default as CardHeaderTitle } from './CardHeaderTitle';
 export { default as Content } from './Content';
 export { default as Container } from './Container';
 export { default as Control } from './Control';
-export { default as Fab } from './Fab';
-export { default as Fas } from './Fas';
+
 export { default as Field } from './Field';
 export { default as FieldBody } from './FieldBody';
 export { default as FieldLabel } from './FieldLabel';
@@ -20,29 +19,16 @@ export { default as FileInput } from './FileInput';
 export { default as FileLabel } from './FileLabel';
 export { default as FileName } from './FileName';
 export { default as Help } from './Help';
-export { default as Icon } from './Icon';
-export { default as Image } from './Image';
 export { default as Input } from './Input';
 export { default as Label } from './Label';
 export { default as Menu } from './Menu';
-export { default as MenuList } from './MenuList';
-export { default as Message } from './Message';
-export { default as MessageBody } from './MessageBody';
-export { default as ModalBackground } from './ModalBackground';
-export { default as ModalClose } from './ModalClose';
-export { default as ModalContainer } from './ModalContainer';
-export { default as ModalContent } from './ModalContent';
-export { default as Navbar } from './Navbar';
-export { default as NavbarBrand } from './NavbarBrand';
-export { default as NavbarBurger } from './NavbarBurger';
+
 export { default as NavbarEnd } from './NavbarEnd';
-export { default as NavbarItem } from './NavbarItem';
-export { default as NavbarMenu } from './NavbarMenu';
+
 export { default as NavbarStart } from './NavbarStart';
 export { default as Subtitle } from './Subtitle';
 export { default as Tab } from './Tab';
-export { default as Table } from './Table';
-export { default as TableRow } from './TableRow';
+
 export { default as TabItem } from './TabItem';
 export { default as Textarea } from './Textarea';
 export { default as Title } from './Title';
