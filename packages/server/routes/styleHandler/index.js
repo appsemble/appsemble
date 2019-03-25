771d31c615ef06274871e1b0dd4b537e1c6a4303
Wessel Kuipers <wessel@d-centralize.nl>
2019-03-25 14:33:27 +0100

    implement dynamic sass variables

--- packages/server/routes/styleHandler/index.js
+++ packages/server/routes/styleHandler/index.js
@@ -1,7 +1,29 @@
-import fs from 'fs';
 import path from 'path';
+import fs from 'fs';
 
 import sass from 'node-sass';
+
+/**
+ * Process SASS styles based on given parameters.
+ *
+ * @param {Object} params
+ * @returns {string} SASS string containing the base Appsemble style augmented by user parameters.
+ */
+function processStyle(params) {
+  const style = fs.readFileSync(path.resolve(__dirname, 'bulma.scss'), 'utf8').split(/\r?\n/);
+  const bulmaPath = path.resolve(__dirname, '../../../../node_modules', 'bulma', 'bulma.sass');
+  const mappings = { primaryColor: 'primary', linkColor: 'link' };
+
+  Object.entries(params).forEach(([key, value]) => {
+    if (mappings[key]) {
+      style.push(`$${mappings[key]}: ${value};`);
+    }
+  });
+
+  style.push(`@import "${bulmaPath}";`);
+
+  return style.join('\n');
+}
 
 /**
  * Serve the minified Bulma CSS.
@@ -9,10 +31,12 @@ import sass from 'node-sass';
  * @param {Koa.Context} ctx The Koa context.
  */
 export function bulmaHandler(ctx) {
-  const { css } = sass.renderSync({
-    file: path.resolve(__dirname, 'bulma.scss'),
+  const options = {
+    data: processStyle(ctx.query),
     outputStyle: 'compressed',
-  });
+  };
+
+  const { css } = sass.renderSync(options);
 
   ctx.body = css;
   ctx.type = 'text/css';
