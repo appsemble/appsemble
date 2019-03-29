1c51a3445ee020d877e240c314bbed5205316c59
Wessel Kuipers <wessel@d-centralize.nl>
2019-03-29 10:42:33 +0100

    include holiday app template

--- packages/editor/templates/index.js
+++ packages/editor/templates/index.js
@@ -1,4 +1,5 @@
 import Empty from './empty';
 import PersonApp from './person';
+import HolidayApp from './holiday';
 
-export default [Empty, PersonApp];
+export default [Empty, PersonApp, HolidayApp];
