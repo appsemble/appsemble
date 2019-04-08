d051a31f88bcd77fa4e271e442302898d891d8f9
Wessel Kuipers <wessel@d-centralize.nl>
2019-04-08 15:32:28 +0200

    update translations

--- packages/editor/components/ResourceTable/messages.jsx
+++ packages/editor/components/ResourceTable/messages.jsx
@@ -9,10 +9,10 @@ export default defineMessages({
   deleteError: 'Something went wrong when deleting this resource.',
   updateError: 'Something went wrong when updating this resource.',
   loadError: 'Something went wrong when loading this resource.',
-  createSuccess: 'Successfully created resource {id}!',
+  createSuccess: 'Successfully created resource {id}.',
   deleteSuccess: 'Successfully deleted resource {id}.',
-  updateSuccess: 'Successfully updated resource!',
-  notManaged: 'This resource is not managed by Appsemble. You can find this resource at: ',
+  updateSuccess: 'Successfully updated resource.',
+  notManaged: 'This resource is not managed by Appsemble. You can find this resource at {link}',
   resourceWarningTitle: 'Deletion Warning',
   resourceWarning:
     'Are you sure you want to delete this resource? Deleted resources can not be recovered.',
