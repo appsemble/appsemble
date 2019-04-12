764a600d167d3d277921c5d142641fa174853229
Remco Haszing <remcohaszing@gmail.com>
2019-04-12 10:40:04 +0200

    Use render function to render the editor index

--- packages/server/routes/editorHandler/index.js
+++ packages/server/routes/editorHandler/index.js
@@ -1,24 +1,8 @@
-import fs from 'fs';
-import path from 'path';
-
 /**
  * Serve `index.html` for editor related routes.
  */
 export default async function editorHandler(ctx) {
-  let filesystem = fs;
-  let filename = path.resolve(__dirname, '../../../../dist/index.html');
-  if (process.env.NODE_ENV !== 'production') {
-    filesystem = ctx.state.fs;
-    filename = '/index.html';
-  }
-  ctx.body = await new Promise((resolve, reject) => {
-    filesystem.readFile(filename, (err, buffer) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(buffer);
-      }
-    });
-  });
+  const { render } = ctx.state;
+  ctx.body = await render('index.html');
   ctx.type = 'text/html';
 }
