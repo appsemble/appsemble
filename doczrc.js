d9eedcabba641e5e7c1bf0947400ca4ba5be4330
Wessel Kuipers <wessel@d-centralize.nl>
2019-04-18 10:47:04 +0200

    build path based on environment variable

--- doczrc.js
+++ doczrc.js
@@ -1,5 +1,7 @@
 module.exports = {
-  base: process.env.CI_ENVIRONMENT_URL || '/',
+  base: process.env.CI_ENVIRONMENT_URL
+    ? new URL(process.env.CI_ENVIRONMENT_URL).pathname.replace(/index\.html$/, '')
+    : '/',
   src: './docs',
   title: 'Appsemble',
 };
