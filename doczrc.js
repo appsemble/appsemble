ae7cc333cbd2cff4531fa56c509bc8f27a8deb71
Wessel Kuipers <wessel@d-centralize.nl>
2019-04-19 11:07:42 +0200

    include favicon

--- doczrc.js
+++ doczrc.js
@@ -1,3 +1,19 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import FaviconsWebpackPlugin from 'favicons-webpack-plugin';
+
+const PUBLIC = '/public';
+
+const modifyBundlerConfig = config => {
+  config.plugins.push(
+    new FaviconsWebpackPlugin({
+      logo: `${PUBLIC}/favicon.svg`,
+      inject: true,
+    }),
+  );
+
+  return config;
+};
+
 module.exports = {
   base: process.env.CI_ENVIRONMENT_URL
     ? new URL(process.env.CI_ENVIRONMENT_URL).pathname.replace(/index\.html$/, '')
@@ -5,4 +21,6 @@ module.exports = {
   src: './docs',
   title: 'Appsemble',
   menu: ['Getting Started', 'Architecture', 'Blocks', 'Development', 'Deployment'],
+  public: PUBLIC,
+  modifyBundlerConfig,
 };
