d3b3eae16ea16e108c26ae470a881813b87a74ae
Wessel Kuipers <wessel@d-centralize.nl>
2019-04-19 13:25:21 +0200

    Revert "include favicon"
    This reverts commit ae7cc333cbd2cff4531fa56c509bc8f27a8deb71.

--- doczrc.js
+++ doczrc.js
@@ -1,19 +1,3 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
-import FaviconsWebpackPlugin from 'favicons-webpack-plugin';
-
-const PUBLIC = '/public';
-
-const modifyBundlerConfig = config => {
-  config.plugins.push(
-    new FaviconsWebpackPlugin({
-      logo: `${PUBLIC}/favicon.svg`,
-      inject: true,
-    }),
-  );
-
-  return config;
-};
-
 module.exports = {
   base: process.env.CI_ENVIRONMENT_URL
     ? new URL(process.env.CI_ENVIRONMENT_URL).pathname.replace(/index\.html$/, '')
@@ -21,6 +5,4 @@ module.exports = {
   src: './docs',
   title: 'Appsemble',
   menu: ['Getting Started', 'Architecture', 'Blocks', 'Development', 'Deployment'],
-  public: PUBLIC,
-  modifyBundlerConfig,
 };
