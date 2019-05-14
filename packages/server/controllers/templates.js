947c72281da08fea60b7fc29fd58649378d0d4bc
Wessel Kuipers <wessel@d-centralize.nl>
2019-05-14 12:29:31 +0200

    move app templates to server

---
+++ packages/server/controllers/templates.js
@@ -0,0 +1,44 @@
+import Boom from 'boom';
+import normalize from '@appsemble/utils/normalize';
+
+import templates from '../templates/apps';
+
+export async function getAppTemplates(ctx) {
+  ctx.body = templates.map(({ name, description, resources }) => ({
+    name,
+    description,
+    resources: !!(resources && resources.length),
+  }));
+}
+
+export async function createTemplateApp(ctx) {
+  const { template: reqTemplate, name, description, organizationId } = ctx.request.body;
+  const { App } = ctx.db.models;
+  const { user } = ctx.state;
+
+  const template = templates.find(t => t.name === reqTemplate);
+
+  if (!user.organizations.some(organization => organization.id === organizationId)) {
+    throw Boom.forbidden('User does not belong in this organization.');
+  }
+
+  if (!template) {
+    throw Boom.notFound(`Template ${template} does not exist.`);
+  }
+  const app = await App.create(
+    {
+      definition: { ...template.definition, description, name: name || template },
+      OrganizationId: organizationId,
+      path: name ? normalize(name) : normalize(template),
+    },
+    { raw: true },
+  );
+
+  ctx.body = {
+    ...app.definition,
+    id: app.id,
+    path: app.path,
+    organizationId: app.OrganizationId,
+  };
+  ctx.status = 201;
+}
