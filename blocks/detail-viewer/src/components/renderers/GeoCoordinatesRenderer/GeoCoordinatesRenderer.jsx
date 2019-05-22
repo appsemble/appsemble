b09d9ed5690de428900c3a1cef0c33eab4072568
Remco Haszing <remcohaszing@gmail.com>
2019-05-22 12:21:51 +0200

    Extract Location block from GeoCoordinatesRenderer
    It can be reused by other blocks.

--- blocks/detail-viewer/src/components/renderers/GeoCoordinatesRenderer/GeoCoordinatesRenderer.jsx
+++ blocks/detail-viewer/src/components/renderers/GeoCoordinatesRenderer/GeoCoordinatesRenderer.jsx
@@ -1,17 +1,11 @@
 import 'leaflet/dist/leaflet.css';
+import { Location } from '@appsemble/react-components';
 import PropTypes from 'prop-types';
-import { Point } from 'leaflet/src/geometry';
-import { Icon, Marker, TileLayer } from 'leaflet/src/layer';
-import { Map } from 'leaflet/src/map';
-import { CircleMarker } from 'leaflet/src/layer/vector';
 import React from 'react';
 import { remapData } from '@appsemble/utils/remap';
 
 import iconUrl from '../../../../../../themes/amsterdam/core/marker.svg';
 import styles from './GeoCoordinatesRenderer.css';
-
-const MARKER_ICON_WIDTH = 40;
-const MARKER_ICON_HEIGHT = 40;
 
 /**
  * An map for an object type schema which implements GeoCoordinates.
@@ -20,7 +14,6 @@ const MARKER_ICON_HEIGHT = 40;
  */
 export default class GeoCoordinatesRenderer extends React.Component {
   static propTypes = {
-    reactRoot: PropTypes.instanceOf(HTMLElement).isRequired,
     /**
      * The current value.
      */
@@ -42,18 +35,11 @@ export default class GeoCoordinatesRenderer extends React.Component {
     value: {},
   };
 
-  ref = React.createRef();
-
-  locationMarker = new CircleMarker(null, {
-    // eslint-disable-next-line react/destructuring-assignment
-    color: getComputedStyle(this.props.reactRoot).getPropertyValue('--primary-color'),
-  });
-
-  componentDidMount() {
+  render() {
     const {
-      value,
       data,
-      field: { longitude, latitude },
+      field: { label, latitude, longitude },
+      value,
     } = this.props;
 
     let lat;
@@ -69,34 +55,18 @@ export default class GeoCoordinatesRenderer extends React.Component {
       lng = remapData(longitude, data);
     }
 
-    const map = new Map(this.ref.current, { attributionControl: false })
-      .on('locationfound', ({ latlng }) => {
-        this.locationMarker.setLatLng(latlng).addTo(map);
-      })
-      .locate()
-      .setView([lat, lng], 16);
-    new TileLayer(
-      'https://cartodb-basemaps-c.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png',
-    ).addTo(map);
-    new Marker(null, {
-      icon: new Icon({
-        iconUrl,
-        iconAnchor: new Point(MARKER_ICON_WIDTH / 2, MARKER_ICON_HEIGHT),
-      }),
-    })
-      .setLatLng([lat, lng])
-      .addTo(map);
-  }
-
-  render() {
-    const {
-      field: { label },
-    } = this.props;
-
     return (
       <div className={styles.root}>
         {label && <h1 className="label">{label}</h1>}
-        <div ref={this.ref} className={styles.map} />
+
+        <Location
+          className={styles.map}
+          iconHeight={40}
+          iconUrl={iconUrl}
+          iconWidth={40}
+          latitude={lat}
+          longitude={lng}
+        />
       </div>
     );
   }
---
+++ packages/react-components/components/Location/Location.tsx
@@ -0,0 +1,50 @@
+import { CircleMarker, Icon, LocationEvent, Map, Marker, Point, TileLayer } from 'leaflet';
+import * as React from 'react';
+import { BlockProps } from '@appsemble/react';
+
+export interface LocationProps {
+  className?: string;
+  iconHeight: number;
+  iconUrl: string;
+  iconWidth: number;
+  latitude: number;
+  longitude: number;
+}
+
+/**
+ * Render a location based marker based on leaflet.
+ */
+export default class Location extends React.Component<LocationProps & BlockProps> {
+  ref = React.createRef<HTMLDivElement>();
+
+  componentDidMount(): void {
+    const { iconHeight, iconUrl, iconWidth, latitude, longitude, reactRoot } = this.props;
+
+    const locationMarker = new CircleMarker(null, {
+      color: getComputedStyle(reactRoot).getPropertyValue('--primary-color'),
+    });
+
+    const map = new Map(this.ref.current, { attributionControl: false })
+      .on('locationfound', ({ latlng }: LocationEvent) => {
+        locationMarker.setLatLng(latlng).addTo(map);
+      })
+      .locate()
+      .setView([latitude, longitude], 16);
+    new TileLayer(
+      'https://cartodb-basemaps-c.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png',
+    ).addTo(map);
+    new Marker(null, {
+      icon: new Icon({
+        iconUrl,
+        iconAnchor: new Point(iconWidth / 2, iconHeight),
+      }),
+    })
+      .setLatLng([latitude, longitude])
+      .addTo(map);
+  }
+
+  render(): JSX.Element {
+    const { className } = this.props;
+    return <div ref={this.ref} className={className} />;
+  }
+}
