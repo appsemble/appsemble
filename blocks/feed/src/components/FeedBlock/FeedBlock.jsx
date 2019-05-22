cb51d72a59ca329a62b7308faab8bd33ece41a5b
Remco Haszing <remcohaszing@gmail.com>
2019-05-22 12:19:55 +0200

    Fix empty state for feed block
    If the data was empty, it was showing a loader indefinitely.

--- blocks/feed/src/components/FeedBlock/FeedBlock.jsx
+++ blocks/feed/src/components/FeedBlock/FeedBlock.jsx
@@ -2,8 +2,10 @@ import { Loader } from '@appsemble/react-components';
 import { compileFilters } from '@appsemble/utils/remap';
 import React from 'react';
 import PropTypes from 'prop-types';
+import { FormattedMessage } from 'react-intl';
 
 import Card from '../Card';
+import messages from './messages';
 
 function createRemapper(mapper) {
   return mapper ? compileFilters(mapper) : () => null;
@@ -30,6 +32,7 @@ export default class FeedBlock extends React.Component {
 
   state = {
     data: [],
+    loading: false,
   };
 
   async componentDidMount() {
@@ -48,11 +51,17 @@ export default class FeedBlock extends React.Component {
 
     if (parameters.listen) {
       events.on(parameters.listen, data => {
-        this.setState({ data });
+        this.setState({
+          data,
+          loading: false,
+        });
       });
     } else {
       const data = await actions.load.dispatch();
-      this.setState({ data });
+      this.setState({
+        data,
+        loading: false,
+      });
     }
   }
 
@@ -62,10 +71,14 @@ export default class FeedBlock extends React.Component {
   };
 
   render() {
-    const { data } = this.state;
+    const { data, loading } = this.state;
+
+    if (loading) {
+      return <Loader />;
+    }
 
     if (!data.length) {
-      return <Loader />;
+      return <FormattedMessage {...messages.empty} />;
     }
 
     return data.map(content => (
