2c95dff8e0a544947e79ac876246e325e1d09e54
Remco Haszing <remcohaszing@gmail.com>
2019-05-29 13:14:12 +0200

    Add version tag to the editor header
    When clicked, the tag links to the specific release on GitLab.

--- packages/editor/components/Toolbar/Toolbar.jsx
+++ packages/editor/components/Toolbar/Toolbar.jsx
@@ -1,9 +1,9 @@
-import classNames from 'classnames';
 import PropTypes from 'prop-types';
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
 import { Link } from 'react-router-dom';
 
+import { version } from '../../package.json';
 import styles from './Toolbar.css';
 import messages from './messages';
 
@@ -18,17 +18,27 @@ export default class Toolbar extends React.Component {
     const { intl, isLoggedIn, logout } = this.props;
 
     return (
-      <nav className={classNames('navbar', 'is-fixed-top', 'is-dark', styles.root)}>
-        <Link className="navbar-brand" to="/">
-          <header className="navbar-item title">
-            <img
-              alt={intl.formatMessage(messages.iconAlt)}
-              className={styles.icon}
-              src="/icon-64.png"
-            />
-            <h1 className="has-text-white title">Appsemble</h1>
-          </header>
-        </Link>
+      <nav className={`navbar is-fixed-top is-dark ${styles.root}`}>
+        <div className="navbar-brand">
+          <Link to="/">
+            <header className="navbar-item title">
+              <img
+                alt={intl.formatMessage(messages.iconAlt)}
+                className={styles.icon}
+                src="/icon-64.png"
+              />
+              <h1 className="has-text-white title">Appsemble</h1>
+            </header>
+          </Link>
+          <a
+            className={`is-rounded is-warning tag ${styles.tag}`}
+            href={`https://gitlab.com/appsemble/appsemble/tags/${version}`}
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            {`alpha ${version}`}
+          </a>
+        </div>
         <div className="navbar-brand">
           <span className="navbar-item">
             <a
