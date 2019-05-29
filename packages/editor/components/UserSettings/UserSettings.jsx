ec7a5ff80c8d0677cce4bc0302676b5e47aba1a3
Wessel Kuipers <wessel@d-centralize.nl>
2019-05-29 15:54:05 +0200

    fetch user data from api instead of jwt in UserSettings

--- packages/editor/components/UserSettings/UserSettings.jsx
+++ packages/editor/components/UserSettings/UserSettings.jsx
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import axios from 'axios';
+import { Loader } from '@appsemble/react-components';
 
 export default class UserSettings extends Component {
-  static propTypes = {
-    user: PropTypes.shape().isRequired,
-  };
+  state = { user: undefined, loading: true };
+
+  async componentDidMount() {
+    const { data: user } = await axios.get('/api/user');
+    this.setState({ user, loading: false });
+  }
 
   render() {
-    const { user } = this.props;
+    const { user, loading } = this.state;
+
+    if (loading) {
+      return <Loader />;
+    }
+
     return <pre>{JSON.stringify(user, null, 2)}</pre>;
   }
 }
--- packages/editor/components/UserSettings/index.jsx
+++ packages/editor/components/UserSettings/index.jsx
@@ -1,11 +1,1 @@
-import { connect } from 'react-redux';
-
-import UserSettings from './UserSettings';
-
-function mapStateToProps(state) {
-  return {
-    user: state.user.user,
-  };
-}
-
-export default connect(mapStateToProps)(UserSettings);
+export default from './UserSettings';
