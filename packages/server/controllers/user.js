0b01acea09c11915c81bc06ad360aa472feae19b
Wessel Kuipers <wessel@d-centralize.nl>
2019-06-03 12:53:06 +0200

    include email in user query

--- packages/server/controllers/user.js
+++ packages/server/controllers/user.js
@@ -1,16 +1,20 @@
 import Boom from 'boom';
 
 export async function getUser(ctx) {
-  const { User, Organization } = ctx.db.models;
+  const { User, Organization, EmailAuthorization } = ctx.db.models;
   const { user } = ctx.state;
 
   const dbUser = await User.findOne({
     where: { id: user.id },
-    include: {
-      model: Organization,
-      through: { where: { UserId: user.id } },
-      attributes: ['id'],
-    },
+    include: [
+      {
+        model: Organization,
+        attributes: ['id'],
+      },
+      {
+        model: EmailAuthorization,
+      },
+    ],
   });
 
   ctx.body = {
@@ -18,25 +22,34 @@ export async function getUser(ctx) {
     name: dbUser.name,
     primaryEmail: dbUser.primaryEmail,
     organizations: dbUser.Organizations.map(({ id }) => ({ id })),
+    emails: dbUser.EmailAuthorizations.map(({ email, verified }) => ({
+      email,
+      verified,
+      primary: dbUser.primaryEmail === email,
+    })),
   };
 }
 
 export async function updateUser(ctx) {
-  const { User, EmailAuthentication, Organization } = ctx.db.models;
+  const { User, EmailAuthorization, Organization } = ctx.db.models;
   const { user } = ctx.state;
   const { name, primaryEmail } = ctx.request.body;
 
   const dbUser = await User.findOne({
     where: { id: user.id },
-    include: {
-      model: Organization,
-      through: { where: { UserId: user.id } },
-      attributes: ['id'],
-    },
+    include: [
+      {
+        model: Organization,
+        attributes: ['id'],
+      },
+      {
+        model: EmailAuthorization,
+      },
+    ],
   });
 
   if (primaryEmail !== dbUser.primaryEmail && primaryEmail !== null) {
-    const email = await EmailAuthentication.findOne({
+    const email = await EmailAuthorization.findOne({
       where: { email: primaryEmail },
     });
 
