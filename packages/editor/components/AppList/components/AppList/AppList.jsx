029d7631947cf61b7876b9a539456ec9037c7065
Wessel Kuipers <wessel@d-centralize.nl>
2019-06-14 09:31:44 +0200

    add instructions when user is logged in without any apps or organizations

--- packages/editor/components/AppList/components/AppList/AppList.jsx
+++ packages/editor/components/AppList/components/AppList/AppList.jsx
@@ -1,10 +1,13 @@
 import { Loader } from '@appsemble/react-components';
 import PropTypes from 'prop-types';
 import React from 'react';
+import { FormattedMessage } from 'react-intl';
+import { Link } from 'react-router-dom';
 
 import AppCard from '../AppCard';
 import CreateAppCard from '../CreateAppCard';
 import styles from './AppList.css';
+import messages from './messages';
 
 export default class AppList extends React.Component {
   static propTypes = {
@@ -30,12 +33,33 @@ export default class AppList extends React.Component {
     }
 
     return (
-      <div className={styles.appList}>
-        {apps.map(app => (
-          <AppCard key={app.id} app={app} />
-        ))}
-        {user && user.organizations.length > 1 && <CreateAppCard />}
-      </div>
+      <React.Fragment>
+        <div className={styles.appList}>
+          {apps.map(app => (
+            <AppCard key={app.id} app={app} />
+          ))}
+          {user && user.organizations.length > 1 && <CreateAppCard />}
+        </div>
+        {user && user.organizations.length === 0 && apps.length === 0 && (
+          <div className={styles.noApps}>
+            <span>
+              <i className={`fas fa-folder-open ${styles.noAppsIcon}`} />
+            </span>
+            <span>
+              <FormattedMessage
+                {...messages.createOrganizationInstruction}
+                values={{
+                  link: (
+                    <Link to="/_/settings/organizations">
+                      <FormattedMessage {...messages.here} />
+                    </Link>
+                  ),
+                }}
+              />
+            </span>
+          </div>
+        )}
+      </React.Fragment>
     );
   }
 }
