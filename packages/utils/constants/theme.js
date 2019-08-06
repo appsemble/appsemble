6226a2ff1b1211e07ec9646434b8b6a6fa1b612a
Wessel Kuipers <wessel@d-centralize.nl>
2019-08-06 16:14:52 +0200

    re-export utils and introduce theme constants

---
+++ packages/utils/constants/theme.js
@@ -0,0 +1,10 @@
+export default {
+  primaryColor: '#5291ff',
+  linkColor: '#3273dc',
+  infoColor: '#209cee',
+  successColor: '#23d160',
+  warningColor: '#ffdd57',
+  dangerColor: '#ff3860',
+  splashColor: '#ffffff',
+  themeColor: '#ffffff',
+};
---
+++ packages/utils/index.js
@@ -0,0 +1,5 @@
+export * from './constants';
+export { default as normalize } from './normalize';
+export * from './remap';
+export { default as validate } from './validate';
+export { default as validateStyle } from './validateStyle';
