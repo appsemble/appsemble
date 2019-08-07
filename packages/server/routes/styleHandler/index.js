e5ceaa73ca37c8d54c8f6249fb850af192e3799d
Wessel Kuipers <wessel@d-centralize.nl>
2019-08-07 11:33:09 +0200

    rework how default style gets loaded in

--- packages/server/routes/styleHandler/index.js
+++ packages/server/routes/styleHandler/index.js
@@ -1,7 +1,6 @@
+import { theme } from '@appsemble/utils';
 import fs from 'fs';
 import sass from 'node-sass';
-
-const baseStyle = fs.readFileSync(require.resolve('./bulma.scss'), 'utf8').split(/\r?\n/);
 
 /**
  * Process SASS styles based on given parameters.
@@ -10,52 +9,41 @@ const baseStyle = fs.readFileSync(require.resolve('./bulma.scss'), 'utf8').split
  * @returns {string} SASS string containing the base Appsemble style augmented by user parameters.
  */
 function processStyle(params) {
-  const style = [...baseStyle];
   const bulmaPath = require.resolve('bulma/bulma.sass');
-  const mappings = {
-    primaryColor: 'primary',
-    linkColor: 'link',
-    infoColor: 'info',
-    successColor: 'success',
-    warningColor: 'warning',
-    dangerColor: 'danger',
-  };
-
-  Object.entries(params).forEach(([key, value]) => {
-    if (mappings[key]) {
-      style.push(`$${mappings[key]}: ${value};`);
-    }
-  });
-
-  style.push(`@import "${bulmaPath}";`);
-
-  // Syntax: https://sass-lang.com/documentation/breaking-changes/css-vars
-  style.push(
-    `$themeColor: ${params.themeColor || '#fff'};`,
-    `$splashColor: ${params.splashColor || '#fff'};`,
-    ':root {',
-    '--primary-color: #{$primary};',
-    '--link-color: #{$link};',
-    '--success-color: #{$success};',
-    '--info-color: #{$info};',
-    '--warning-color: #{$warning};',
-    '--danger-color: #{$danger};',
-    '--success-color: #{$success};',
-    '--theme-color: #{$themeColor};',
-    '--splash-color: #{$splashColor};',
-    '--primary-color-invert: #{$primary-invert};',
-    '--link-color-invert: #{$link-invert};',
-    '--success-color-invert: #{$success-invert};',
-    '--info-color-invert: #{$info-invert};',
-    '--warning-color-invert: #{$warning-invert};',
-    '--danger-color-invert: #{$danger-invert};',
-    '--success-color-invert: #{$success-invert};',
-    '--theme-color-invert: #{findColorInvert($themeColor)};',
-    '--splash-color-invert: #{findColorInvert($splashColor)};',
-    '}',
-  );
-
-  return style.join('\n');
+  return `
+    @charset "utf-8";
+    @import url(https://fonts.googleapis.com/css?family=Libre+Franklin|Open+Sans);
+    $family-sans-serif: 'Libre Franklin', sans-serif !default;
+    $primary: ${params.primaryColor || theme.primaryColor};
+    $link: ${params.linkColor || theme.linkColor};
+    $info: ${params.infoColor || theme.infoColor};
+    $success: ${params.successColor || theme.successColor};
+    $warning: ${params.warningColor || theme.warningColor};
+    $danger: ${params.dangerColor || theme.dangerColor};
+    $themeColor: ${params.themeColor || theme.themeColor};
+    $splashColor: ${params.splashColor || theme.splashColor};
+    @import "${bulmaPath}";
+    // Syntax: https://sass-lang.com/documentation/breaking-changes/css-vars
+    :root {
+      --primary-color: #{$primary};
+      --link-color: #{$link};
+      --success-color: #{$success};
+      --info-color: #{$info};
+      --warning-color: #{$warning};
+      --danger-color: #{$danger};
+      --success-color: #{$success};
+      --theme-color: #{$themeColor};
+      --splash-color: #{$splashColor};
+      --primary-color-invert: #{$primary-invert};
+      --link-color-invert: #{$link-invert};
+      --success-color-invert: #{$success-invert};
+      --info-color-invert: #{$info-invert};
+      --warning-color-invert: #{$warning-invert};
+      --danger-color-invert: #{$danger-invert};
+      --success-color-invert: #{$success-invert};
+      --theme-color-invert: #{findColorInvert($themeColor)};
+      --splash-color-invert: #{findColorInvert($splashColor)};
+    }`;
 }
 
 /**
