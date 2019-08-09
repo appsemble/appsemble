771766fc129ad39a4fe5e8e590e122d551c547f4
Remco Haszing <remcohaszing@gmail.com>
2019-08-09 10:51:59 +0200

    Disable logging in tests

--- config/jest/setupFilesAfterEnv.ts
+++ config/jest/setupFilesAfterEnv.ts
@@ -1,6 +1,9 @@
+import { setLogLevel } from '@appsemble/node-utils';
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import { toMatchImageSnapshot } from 'jest-image-snapshot';
+
+setLogLevel(0);
 
 expect.extend({ toMatchImageSnapshot });
 
