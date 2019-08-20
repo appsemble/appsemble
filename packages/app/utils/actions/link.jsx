ec1438bd1e22e635bd0314c86d7c4e8f46940bb0
Wessel Kuipers <wessel@d-centralize.nl>
2019-08-20 10:25:23 +0200

    allow linking to tab subPages directly

--- packages/app/utils/actions/link.jsx
+++ packages/app/utils/actions/link.jsx
@@ -3,19 +3,24 @@ import { compileFilters, normalize } from '@appsemble/utils';
 import mapValues from '../mapValues';
 
 export default function link({ definition: { to, parameters = {} }, app: { pages }, history }) {
-  const toPage = pages.find(({ name }) => name === to);
-  if (toPage == null) {
-    throw new Error(`Invalid link reference ${to}`);
+  const [toBase, toSub] = [].concat(to);
+
+  const toPage = pages.find(({ name }) => name === toBase);
+  const subPage = toSub ? toPage.subPages.find(({ name }) => name === toSub) : null;
+
+  if (toPage == null || (toSub && subPage === null)) {
+    throw new Error(`Invalid link reference ${[].concat(to).join('/')}`);
   }
 
   const mappers = mapValues(parameters || {}, compileFilters);
 
   function href(data = {}) {
     return `/${[
-      normalize(to),
+      normalize(toPage.name),
       ...(toPage.parameters || []).map(name =>
         Object.hasOwnProperty.call(mappers, name) ? mappers[name](data) : data[name],
       ),
+      ...(subPage ? [normalize(subPage.name)] : []),
     ].join('/')}`;
   }
 
