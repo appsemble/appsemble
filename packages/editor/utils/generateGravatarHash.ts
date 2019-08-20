1897d6567ddc0c54339c06728620925296a2dee5
Remco Haszing <remcohaszing@gmail.com>
2019-08-20 13:28:01 +0200

    Fix linting issues

--- packages/editor/utils/generateGravatarHash.ts
+++ packages/editor/utils/generateGravatarHash.ts
@@ -6,6 +6,6 @@ import md5 from 'md5';
  * @param email Email to get gravatar of.
  * @param size Size of the image in pixels.
  */
-export default function generateGravatarHash(email: string, size: number = 64): string {
+export default function generateGravatarHash(email: string, size = 64): string {
   return `https://www.gravatar.com/avatar/${md5(email.trim().toLowerCase())}?s=${size}&d=mp`;
 }
