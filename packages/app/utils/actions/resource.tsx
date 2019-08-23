8657d1eac6d8eaf69a67a3ccace84745ffd1287b
Wessel Kuipers <wessel@d-centralize.nl>
2019-08-23 17:25:27 +0200

    fix blobs serialize error

--- packages/app/utils/actions/resource.tsx
+++ packages/app/utils/actions/resource.tsx
@@ -28,7 +28,7 @@ function getBlobs(resource: Resource): BlobUploadType {
   const method = (blobs && blobs.method) || 'post';
   const url = (blobs && blobs.url) || '/api/assets';
 
-  return { type, method, url, serialize: blobs.serialize };
+  return { type, method, url, ...(blobs && blobs.serialize && { serialize: blobs.serialize }) };
 }
 
 function get(args: MakeActionParameters<ResourceGetActionDefinition>): ResourceGetAction {
