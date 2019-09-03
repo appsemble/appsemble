4c157cadf52f04fca6bcb1901530a0378714fd06
Remco Haszing <remcohaszing@gmail.com>
2019-09-03 10:15:50 +0200

    Convert Preact class components to functions
    These won’t be processed by
    `babel-plugin-transform-react-class-to-function`.

--- packages/preact-components/Icon/Icon.tsx
+++ packages/preact-components/Icon/Icon.tsx
@@ -1,7 +1,7 @@
 /** @jsx h */
 import { IconName, IconPrefix } from '@fortawesome/fontawesome-common-types';
 import classNames from 'classnames';
-import { Component, h, VNode } from 'preact';
+import { h, VNode } from 'preact';
 
 interface IconProps {
   className?: string;
@@ -11,22 +11,18 @@ interface IconProps {
   size?: 'small' | 'medium' | 'large';
 }
 
-const iconSizeMap = { medium: 'lg', large: '2x' };
+const iconSizeMap: { medium: 'lg'; large: '2x' } = { medium: 'lg', large: '2x' };
 
-export default class Icon extends Component<IconProps> {
-  render(): VNode {
-    const {
-      className,
-      icon,
-      prefix = 'fas',
-      size,
-      iconSize = iconSizeMap[size as 'medium' | 'large'],
-    } = this.props;
-
-    return (
-      <span className={classNames('icon', size && `is-${size}`, className)}>
-        <i className={classNames(prefix, `fa-${icon}`, iconSize && `fa-${iconSize}`)} />
-      </span>
-    );
-  }
+export default function Icon({
+  className,
+  icon,
+  prefix = 'fas',
+  size,
+  iconSize = iconSizeMap[size as 'medium' | 'large'],
+}: IconProps): VNode {
+  return (
+    <span className={classNames('icon', size && `is-${size}`, className)}>
+      <i className={classNames(prefix, `fa-${icon}`, iconSize && `fa-${iconSize}`)} />
+    </span>
+  );
 }
