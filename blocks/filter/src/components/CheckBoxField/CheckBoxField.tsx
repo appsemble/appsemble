7446d4a5033c85e9bd22b587fb09e2af4001c974
Wessel Kuipers <wessel@d-centralize.nl>
2019-10-01 15:41:58 +0200

    style checkboxes

--- blocks/filter/src/components/CheckBoxField/CheckBoxField.tsx
+++ blocks/filter/src/components/CheckBoxField/CheckBoxField.tsx
@@ -2,20 +2,33 @@ import { Icon } from '@appsemble/react-components';
 import React from 'react';
 
 import { Enum } from '../../../types';
+import styles from './CheckBoxField.css';
 
 const CheckBoxField: React.StatelessComponent<{
   value: string[];
+  name: string;
   enumerator: Enum[];
   onChange: React.ChangeEventHandler<HTMLElement>;
 }> = p => {
   const { enumerator, value: parentValue, ...props } = p;
+
   return (
-    <div>
+    <div className={styles.container}>
       {enumerator.map(({ icon, value, label }) => (
-        <label key={value}>
-          <input checked={parentValue.includes(value)} type="checkbox" value={value} {...props} />
-          {icon && <Icon icon={icon} />} {label || value}
-        </label>
+        <div key={`${p.name}${value}`} className={styles.inputContainer}>
+          <input
+            checked={parentValue.includes(value)}
+            className={styles.input}
+            id={`${p.name}${value}`}
+            type="checkbox"
+            value={value}
+            {...props}
+          />
+          <label className={styles.label} htmlFor={`${p.name}${value}`}>
+            {icon && <Icon className={styles.icon} icon={icon} />}
+            <span>{label || value}</span>
+          </label>
+        </div>
       ))}
     </div>
   );
