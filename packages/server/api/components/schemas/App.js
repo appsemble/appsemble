0038db5d99b6186888a97eedd77f3b812fe1f286
Wessel Kuipers <wessel@d-centralize.nl>
2019-10-04 10:11:03 +0200

    rename left -> left-menu

--- packages/server/api/components/schemas/App.js
+++ packages/server/api/components/schemas/App.js
@@ -48,7 +48,7 @@ export default {
       description: 'Determines whether this app should be included when fetching all apps.',
     },
     navigation: {
-      enum: ['bottom', 'left', 'hidden'],
+      enum: ['bottom', 'left-menu', 'hidden'],
       description: `The navigation type to use.
 
         If this is omitted, a collapsable side navigation menu will be rendered on the left.
