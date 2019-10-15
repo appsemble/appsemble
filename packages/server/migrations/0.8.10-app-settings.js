a998e0388cf0d908731c81e9c93ccca52a7fa8af
Wessel Kuipers <wessel@d-centralize.nl>
2019-10-15 11:42:33 +0200

    implement app settings migration

---
+++ packages/server/migrations/0.8.10-app-settings.js
@@ -0,0 +1,58 @@
+import { DataTypes } from 'sequelize';
+
+export default {
+  key: '0.8.10',
+
+  async up(db) {
+    const queryInterface = db.getQueryInterface();
+
+    await queryInterface.removeColumn('App', 'description');
+    await queryInterface.addColumn('App', 'private', {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+      allowNull: false,
+    });
+
+    const apps = await db.query(
+      "SELECT * FROM `App` WHERE JSON_CONTAINS(definition, ?, '$.private')",
+      { replacements: ['true'], type: db.QueryTypes.SELECT },
+    );
+
+    await Promise.all(
+      apps.map(({ id }) =>
+        db.query(
+          "UPDATE `App` SET private = true, definition = JSON_REMOVE(`definition`, '$.private') WHERE id = ?",
+          {
+            replacements: [id],
+            type: db.QueryTypes.UPDATE,
+          },
+        ),
+      ),
+    );
+  },
+
+  async down(db) {
+    const queryInterface = db.getQueryInterface();
+
+    // Query all apps that are set to private and manually set private property
+    const apps = await db.query('SELECT * FROM `App` WHERE private = ?', {
+      replacements: [true],
+      type: db.QueryTypes.SELECT,
+    });
+
+    await Promise.all(
+      apps.map(({ id }) =>
+        db.query(
+          "UPDATE `App` SET definition = JSON_INSERT(`definition`, '$.private', true) WHERE id = ?",
+          { replacements: [id], type: db.QueryTypes.UPDATE },
+        ),
+      ),
+    );
+
+    await queryInterface.removeColumn('App', 'private');
+    await queryInterface.addColumn('App', 'description', {
+      type: DataTypes.STRING(80),
+      allowNull: true,
+    });
+  },
+};
