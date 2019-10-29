bd341cf92dac372eb0b170a3498fc4298db90a60
Remco Haszing <remcohaszing@gmail.com>
2019-10-29 13:10:55 +0100

    Remove unused code

--- packages/react-components/SimpleSubmit/index.tsx
+++ packages/react-components/SimpleSubmit/index.tsx
@@ -1,5 +1,1 @@
 export { default } from './SimpleSubmit';
-// import { withSimpleForm } from '../SimpleForm';
-// import SimpleSubmit, { SimpleSubmitProps } from './SimpleSubmit';
-
-// export default withSimpleForm<SimpleSubmitProps>(SimpleSubmit);
