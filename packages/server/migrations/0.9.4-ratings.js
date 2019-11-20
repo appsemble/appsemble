cc5530699121db5d1fa5fa9d0efd97ebc2ac2b2c
Wessel Kuipers <wessel@d-centralize.nl>
2019-11-20 18:03:10 +0100

    implement app rating model

---
+++ packages/server/migrations/0.9.4-ratings.js
@@ -0,0 +1,40 @@
+import { DataTypes } from 'sequelize';
+
+export default {
+  key: '0.9.4',
+
+  async up(db) {
+    const queryInterface = db.getQueryInterface();
+
+    await queryInterface.createTable('AppRating', {
+      rating: { type: DataTypes.INTEGER, allowNull: false },
+      description: { type: DataTypes.TEXT },
+      AppId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        unique: 'UniqueRatingIndex',
+        references: {
+          model: 'App',
+          key: 'id',
+        },
+      },
+      UserId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        unique: 'UniqueRatingIndex',
+        references: {
+          model: 'User',
+          key: 'id',
+        },
+      },
+      created: { allowNull: false, type: DataTypes.DATE },
+      updated: { allowNull: false, type: DataTypes.DATE },
+    });
+  },
+
+  async down(db) {
+    const queryInterface = db.getQueryInterface();
+
+    await queryInterface.dropTable('AppRating');
+  },
+};
--- packages/server/migrations/index.js
+++ packages/server/migrations/index.js
@@ -1,4 +1,5 @@
 import postgresql from './0.9.0-postgresql';
 import notifications from './0.9.3-notifications';
+import ratings from './0.9.4-ratings';
 
-export default [postgresql, notifications];
+export default [postgresql, notifications, ratings];
