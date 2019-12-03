a466b833f779708b56102f6eabb0a651f06ac89c
Wessel Kuipers <wessel@d-centralize.nl>
2019-12-03 16:25:10 +0100

    wip

---
+++ packages/server/utils/permissions.js
@@ -0,0 +1,8 @@
+export const ViewApps = 'ViewApps';
+export const ManageMembers = 'ManageMembers';
+export const PublishBlocks = 'PublishBlocks';
+export const Createapps = 'CreateApps';
+export const EditApps = 'EditApps';
+export const EditAppSettings = 'EditAppSettings';
+export const EditThemes = 'EditThemes';
+export const DeleteApps = 'DeleteApps';
