55aa44cccb092596ebe102bd79571478e7b881b1
Wessel Kuipers <wessel@d-centralize.nl>
2019-12-09 07:43:14 +0100

    remove redundant AppList route

--- packages/studio/components/App/App.jsx
+++ packages/studio/components/App/App.jsx
@@ -53,7 +53,6 @@ export default class App extends React.Component {
               <AnonymousRoute component={EditPassword} exact path="/edit-password" />
               <ProtectedRoute component={OrganizationInvite} exact path="/organization-invite" />
               <Route component={VerifyEmail} exact path="/verify" />
-              <Route component={AppList} exact path="/apps" />
               <Route component={ConnectOAuth} exact path="/connect" />
               <AnonymousRoute component={Login} exact path="/login" />
               {settings.enableRegistration && (
