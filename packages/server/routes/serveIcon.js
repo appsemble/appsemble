a928e2ca1592db03545fbdcd53366927b73a541b
Wessel Kuipers <wessel@d-centralize.nl>
2019-12-09 13:15:57 +0100

    update snapshots

--- packages/server/routes/serveIcon.js
+++ packages/server/routes/serveIcon.js
@@ -5,19 +5,17 @@ import getDefaultIcon from '../utils/getDefaultIcon';
 export default async function serveIcon(ctx, { icon, background, format, height, width }) {
   // Allow icon to be null.
   const finalIcon = icon || getDefaultIcon();
-  const img = sharp(finalIcon);
-  // XXX: As of Sharp 0.23.4 SVG handling is either broken or extremely slow.
-  // Check with future releases if this has been resolved.
-  // const metadata = await img.metadata();
+  let img = sharp(finalIcon);
+  const metadata = await img.metadata();
   // SVG images can be resized with a density much better than its metadata specified.
-  // if (metadata.format === 'svg') {
-  //   const density = Math.max(
-  //     metadata.density * Math.max(width / metadata.width, height / metadata.height),
-  //     // This is the maximum allowed value density allowed by sharp.
-  //     2400,
-  //   );
-  //   img = sharp(finalIcon, { density });
-  // }
+  if (metadata.format === 'svg') {
+    const density = Math.max(
+      metadata.density * Math.max(width / metadata.width, height / metadata.height),
+      // This is the maximum allowed value density allowed by sharp.
+      2400,
+    );
+    img = sharp(finalIcon, { density });
+  }
   img.resize(width, height);
   if (background) {
     img.flatten({ background });
