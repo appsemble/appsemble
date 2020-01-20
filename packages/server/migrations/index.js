04c532c17be9d56013aa1fa4c475a1921be46d65
Wessel Kuipers <wessel@d-centralize.nl>
2020-01-20 13:23:32 +0100

    add ResourceSubscription model

--- packages/server/migrations/index.js
+++ packages/server/migrations/index.js
@@ -3,5 +3,13 @@ import notifications from './0.9.3-notifications';
 import templates from './0.9.4-app-templates-ratings';
 import organizationRolesAndAuthentication from './0.10.0-organization-roles-and-authentication';
 import appRoles from './0.10.1-app-roles';
+import resourceSubscriptions from './0.10.2-resource-subscriptions';
 
-export default [postgresql, notifications, templates, organizationRolesAndAuthentication, appRoles];
+export default [
+  postgresql,
+  notifications,
+  templates,
+  organizationRolesAndAuthentication,
+  appRoles,
+  resourceSubscriptions,
+];
