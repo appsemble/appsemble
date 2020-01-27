fd7f52d575d4fc7c74777001b7f9ec6c400f8b02
Wessel Kuipers <wessel@d-centralize.nl>
2020-01-27 10:42:55 +0100

    reorganize docs

--- doczrc.js
+++ doczrc.js
@@ -2,7 +2,7 @@ const { CI_COMMIT_REF_NAME = 'dev' } = process.env;
 
 export default {
   src: './docs',
-  files: '**/*.{md,mdx,css}',
+  files: '**/*.{md,mdx}',
   title: `Appsemble ${CI_COMMIT_REF_NAME}`,
   menu: ['Getting Started', 'Architecture', 'Blocks', 'Development', 'Deployment'],
   gatsbyRemarkPlugins: [
