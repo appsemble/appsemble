a62dee66d79ada16aec363b33d41e5c14f492ee4
Remco Haszing <remcohaszing@gmail.com>
2020-01-27 16:02:07 +0100

    Fix login component typings

--- packages/app/components/Login/index.tsx
+++ packages/app/components/Login/index.tsx
@@ -1,8 +1,9 @@
 import { connect } from 'react-redux';
 
+import { State } from '../../actions';
 import Login from './Login';
 
-function mapStateToProps(state) {
+function mapStateToProps(state: State): Partial<React.ComponentPropsWithoutRef<typeof Login>> {
   return {
     definition: state.app.definition,
   };
