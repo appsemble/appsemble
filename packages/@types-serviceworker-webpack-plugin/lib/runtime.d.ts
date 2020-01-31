6d648ccdfad552a6425c8f0c61a971eeabecf17a
Remco Haszing <remcohaszing@gmail.com>
2020-01-31 09:46:40 +0100

    Use serviceworker-webpack-plugin types from DefinitelyTyped

--- packages/@types-serviceworker-webpack-plugin/lib/runtime.d.ts
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line filenames/match-exported
-declare interface Runtime {
-  register(): Promise<ServiceWorkerRegistration>;
-}
-
-declare const runtime: Runtime;
-
-export default runtime;
