15d64007e4c49f670f2096efad18331a7cba3ef2
Wessel Kuipers <wessel@d-centralize.nl>
2020-03-03 09:44:33 +0100

    add tsdocs

--- blocks/list/block.ts
+++ blocks/list/block.ts
@@ -1,22 +1,55 @@
 import { IconName } from '@fortawesome/fontawesome-common-types';
 
+/**
+ * An object representing how a data field should be displayed.
+ */
 export interface Field {
+  /**
+   * The name of the field to read from to determine the value to show.
+   *
+   * No value will be rendered if undefined.
+   */
   name?: string;
+
+  /**
+   * The label to display.
+   *
+   * Will not render if undefined.
+   */
   label?: string;
+
+  /**
+   * The FontAwesome icon to display in front of the label.
+   *
+   * Will not render if undefined.
+   */
   icon?: IconName;
 }
 
+/**
+ * A generic interface for data with an ID field.
+ */
 export interface Item {
   id?: number;
 }
 
 declare module '@appsemble/sdk' {
   interface Parameters {
+    /**
+     * The header text to display above the list of fields.
+     *
+     * Will not render if undefined.
+     */
     header?: string;
     fields?: Field[];
   }
 
   interface Actions {
+    /**
+     * Action that gets triggered when clicking on a list item.
+     *
+     * If defined, an indicator will show up to show that the list item has a click action.
+     */
     onClick: {};
   }
 
