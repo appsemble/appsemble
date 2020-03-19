ae9ac8fb2f1727668a35a5030d563c77031375a7
Remco Haszing <remcohaszing@gmail.com>
2020-03-19 12:04:09 +0100

    Revert moving of HelmetIntl
    It’s affected by
    https://github.com/akameco/babel-plugin-react-intl-auto/issues/98

--- packages/studio/src/components/HelmetIntl/index.tsx
+++ packages/studio/src/components/HelmetIntl/index.tsx
@@ -1,15 +1,6 @@
-import React from 'react';
-import { Helmet } from 'react-helmet';
-import { injectIntl, MessageDescriptor, WrappedComponentProps } from 'react-intl';
+import { injectIntl } from 'react-intl';
 
-interface HelmIntlProps extends WrappedComponentProps {
-  title: MessageDescriptor;
-  titleValues?: { [key: string]: string };
-}
-
-function HelmetIntl({ intl, title, titleValues = {} }: HelmIntlProps): React.ReactElement {
-  return <Helmet title={intl.formatMessage(title, titleValues)} />;
-}
+import HelmetIntl from './HelmetIntl';
 
 // XXX https://github.com/akameco/babel-plugin-react-intl-auto/issues/98
 export default injectIntl(HelmetIntl);
