1bd642528c715865a5bc10670b53033bfa76eac5
Wessel Kuipers <wessel@d-centralize.nl>
2020-03-31 14:33:56 +0200

    fix detail-viewer labels still falling back

--- blocks/detail-viewer/src/components/StringRenderer/index.tsx
+++ blocks/detail-viewer/src/components/StringRenderer/index.tsx
@@ -9,7 +9,7 @@ import { RendererProps, StringField } from '../../../block';
 export default function StringRenderer({ field, value = '' }: RendererProps<StringField>): VNode {
   return (
     <Fragment>
-      {field.label !== null && <h6 className="title is-6">{field.label ?? field.name}</h6>}
+      {field.label && <h6 className="title is-6">{field.label ?? field.name}</h6>}
       <div className="content">{typeof value === 'string' ? value : JSON.stringify(value)}</div>
     </Fragment>
   );
