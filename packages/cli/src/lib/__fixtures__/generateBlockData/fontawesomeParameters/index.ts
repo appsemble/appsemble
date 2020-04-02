26a08715b3a4f837208b1dddbab6b0c16ceedd7e
Terence Koch <terence.koch@hotmail.com>
2020-04-02 08:34:57 +0000

    Update index.ts

--- packages/cli/src/lib/__fixtures__/generateBlockData/fontawesomeParameters/index.ts
+++ packages/cli/src/lib/__fixtures__/generateBlockData/fontawesomeParameters/index.ts
@@ -1,5 +1,6 @@
 import '@appsemble/sdk';
 
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { IconName } from '@fortawesome/fontawesome-common-types';
 
 declare module '@appsemble/sdk' {
