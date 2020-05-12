10d04494b32a3450aef51a650748d1ce355cc7cb
Wessel Kuipers <wessel@d-centralize.nl>
2020-05-12 13:46:22 +0200

    apply stats block changes to template

--- packages/create-appsemble/templates/mini-jsx/src/index.tsx
+++ packages/create-appsemble/templates/mini-jsx/src/index.tsx
@@ -1,20 +1,14 @@
 import { attach } from '@appsemble/sdk';
-import { compileFilters, MapperFunction } from '@appsemble/utils';
 import { h } from 'mini-jsx';
 
 import styles from './index.css';
 
-attach(({ events, parameters: { fields } }) => {
+attach(({ events, parameters: { fields }, utils: { remap } }) => {
   const wrapper = (
     <div className={styles.wrapper}>
       <div className={styles.loader} />
     </div>
   );
-
-  const remappers: { [key: string]: MapperFunction } = {};
-  fields.forEach(({ name }) => {
-    remappers[name] = compileFilters(name);
-  });
 
   events.on.data((data, error) => {
     while (wrapper.lastElementChild) {
@@ -25,13 +19,17 @@ attach(({ events, parameters: { fields } }) => {
       wrapper.append('error');
     } else {
       wrapper.append(
-        ...fields.map(({ icon, label, name }) => (
-          <div className={styles.field}>
-            <i className={`fas fa-${icon} ${styles.icon}`} />
-            <div className={`has-text-weight-bold ${styles.value}`}>{remappers[name](data)}</div>
-            <div>{label ?? name}</div>
-          </div>
-        )),
+        ...fields.map(({ icon, label, value }) => {
+          const remappedLabel = remap(label, data);
+
+          return (
+            <div className={styles.field}>
+              <i className={`fas fa-${icon} ${styles.icon}`} />
+              <div className={`has-text-weight-bold ${styles.value}`}>{remap(value, data)}</div>
+              {remappedLabel && <div>{remappedLabel}</div>}
+            </div>
+          );
+        }),
       );
     }
   });
