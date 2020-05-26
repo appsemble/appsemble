d069bbf3bf23129165174a4d8d231262a7cb6b68
Remco Haszing <remcohaszing@gmail.com>
2020-05-26 10:21:46 +0200

    Convert some class based components to functional

--- packages/react-components/src/Modal/index.tsx
+++ packages/react-components/src/Modal/index.tsx
@@ -1,12 +1,12 @@
 import classNames from 'classnames';
 import * as React from 'react';
-import { injectIntl, WrappedComponentProps } from 'react-intl';
+import { useIntl } from 'react-intl';
 import { CSSTransition } from 'react-transition-group';
 
 import styles from './index.css';
 import messages from './messages';
 
-interface ModalProps<T extends React.ElementType> extends WrappedComponentProps {
+interface ModalProps<T extends React.ElementType> {
   /**
    * The child elements to render on the modal.
    */
@@ -27,7 +27,7 @@ interface ModalProps<T extends React.ElementType> extends WrappedComponentProps 
   /**
    * A function that will be called when the user closes the modal.
    */
-  onClose?: React.ReactEventHandler;
+  onClose?: React.EventHandler<React.SyntheticEvent>;
 
   /**
    * The title that is displayed at the top of the modal.
@@ -50,78 +50,67 @@ interface ModalProps<T extends React.ElementType> extends WrappedComponentProps 
 /**
  * Render an aria compliant modal overlay.
  */
-export class Modal<T extends React.ElementType = 'div'> extends React.Component<
-  ModalProps<T> & React.ComponentPropsWithoutRef<T>
-> {
-  static defaultProps: Partial<ModalProps<'div'>> = {
-    component: 'div',
-    children: null,
-    footer: null,
-    onClose() {},
-  };
+export default function Modal<T extends React.ElementType = 'div'>({
+  cardClassName,
+  children = null,
+  className,
+  closable = true,
+  component: Component = 'div' as T,
+  footer = null,
+  isActive,
+  onClose = () => {},
+  title,
+  ...props
+}: ModalProps<T> &
+  Omit<React.ComponentPropsWithoutRef<T>, keyof ModalProps<T>>): React.ReactElement {
+  const intl = useIntl();
 
-  onKeyDown: React.KeyboardEventHandler = (event) => {
-    const { onClose } = this.props;
+  const onKeyDown = React.useCallback(
+    (event) => {
+      if (event.key === 'Escape') {
+        onClose(event);
+      }
+    },
+    [onClose],
+  );
 
-    if (event.key === 'Escape') {
-      onClose(event);
-    }
-  };
-
-  render(): React.ReactElement {
-    const {
-      cardClassName,
-      children,
-      className,
-      closable = true,
-      component: Component,
-      footer,
-      intl,
-      isActive,
-      onClose,
-      title,
-      ...props
-    } = this.props;
-
-    return (
-      <CSSTransition
-        classNames={{
-          enter: styles.enter,
-          enterActive: styles.enterActive,
-          exit: styles.exit,
-          exitActive: styles.exitActive,
-        }}
-        in={isActive}
-        mountOnEnter
-        timeout={90}
-        unmountOnExit
-      >
-        <div className={`is-active modal ${styles.modal}`}>
-          <div
-            className="modal-background"
-            onClick={closable ? onClose : null}
-            onKeyDown={closable ? this.onKeyDown : null}
-            role="presentation"
-          />
-          <Component className={classNames('modal-card', cardClassName)} {...props}>
-            <div className="modal-card-head">
-              <p className="modal-card-title">{title}</p>
-              {closable && (
-                <button
-                  aria-label={intl.formatMessage(messages.closeDialog)}
-                  className="delete is-large"
-                  onClick={onClose}
-                  type="button"
-                />
-              )}
-            </div>
-            <div className={classNames('modal-card-body', className)}>{children}</div>
-            {footer && <footer className="card-footer">{footer}</footer>}
-          </Component>
-        </div>
-      </CSSTransition>
-    );
-  }
+  return (
+    <CSSTransition
+      classNames={{
+        enter: styles.enter,
+        enterActive: styles.enterActive,
+        exit: styles.exit,
+        exitActive: styles.exitActive,
+      }}
+      in={isActive}
+      mountOnEnter
+      timeout={90}
+      unmountOnExit
+    >
+      <div className={`is-active modal ${styles.modal}`}>
+        <div
+          className="modal-background"
+          onClick={closable ? onClose : null}
+          onKeyDown={closable ? onKeyDown : null}
+          role="presentation"
+        />
+        {/* @ts-expect-error */}
+        <Component className={classNames('modal-card', cardClassName)} {...props}>
+          <div className="modal-card-head">
+            <p className="modal-card-title">{title}</p>
+            {closable && (
+              <button
+                aria-label={intl.formatMessage(messages.closeDialog)}
+                className="delete is-large"
+                onClick={onClose}
+                type="button"
+              />
+            )}
+          </div>
+          <div className={classNames('modal-card-body', className)}>{children}</div>
+          {footer && <footer className="card-footer">{footer}</footer>}
+        </Component>
+      </div>
+    </CSSTransition>
+  );
 }
-
-export default injectIntl(Modal);
