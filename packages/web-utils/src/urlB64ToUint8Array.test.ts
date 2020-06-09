2eccacf7e929f8eb0132ea53ef933db64011db40
Remco Haszing <remcohaszing@gmail.com>
2020-06-09 13:33:28 +0200

    Simplify urlB64ToUint8Array
    As the tests show, manually adding the padding isn’t necessary. Also the
    loop has been replaced.

--- packages/web-utils/src/urlB64ToUint8Array.test.ts
+++ packages/web-utils/src/urlB64ToUint8Array.test.ts
@@ -1,9 +1,28 @@
 import urlB64ToUint8Array from './urlB64ToUint8Array';
 
 const tests = {
+  '': Uint8Array.from([]),
+  CG: Uint8Array.from([8]),
+  'CG==': Uint8Array.from([8]),
   '8J+QsQo': Uint8Array.from([240, 159, 144, 177, 10]),
+  '8J+QsQo=': Uint8Array.from([240, 159, 144, 177, 10]),
   '8J+QsfCfkLEK': Uint8Array.from([240, 159, 144, 177, 240, 159, 144, 177, 10]),
   '8J+QsfCfkLHwn5CxCg': Uint8Array.from([
+    240,
+    159,
+    144,
+    177,
+    240,
+    159,
+    144,
+    177,
+    240,
+    159,
+    144,
+    177,
+    10,
+  ]),
+  '8J+QsfCfkLHwn5CxCg==': Uint8Array.from([
     240,
     159,
     144,
--- packages/web-utils/src/urlB64ToUint8Array.ts
+++ packages/web-utils/src/urlB64ToUint8Array.ts
@@ -1,12 +1,5 @@
 export default function urlB64ToUint8Array(base64String: string): Uint8Array {
-  const padding = '='.repeat((4 - (base64String.length % 4)) % 4);
-  const base64 = (base64String + padding).replace(/-/g, '+').replace(/_/g, '/');
-  const rawData = atob(base64);
+  const base64 = base64String.replace(/-/g, '+').replace(/_/g, '/');
 
-  const outputArray = new Uint8Array(rawData.length);
-  for (let i = 0; i < rawData.length; i += 1) {
-    outputArray[i] = rawData.charCodeAt(i);
-  }
-
-  return outputArray;
+  return Uint8Array.from(atob(base64), (char) => char.charCodeAt(0));
 }
