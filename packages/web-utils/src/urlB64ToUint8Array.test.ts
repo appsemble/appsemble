25c2c80ebd3af53bceeb8f23d22c56d4f544156b
Remco Haszing <remcohaszing@gmail.com>
2020-06-09 13:02:20 +0200

    Add tests for urlB64ToUint8Array

---
+++ packages/web-utils/src/urlB64ToUint8Array.test.ts
@@ -0,0 +1,26 @@
+import urlB64ToUint8Array from './urlB64ToUint8Array';
+
+const tests = {
+  '8J+QsQo': Uint8Array.from([240, 159, 144, 177, 10]),
+  '8J+QsfCfkLEK': Uint8Array.from([240, 159, 144, 177, 240, 159, 144, 177, 10]),
+  '8J+QsfCfkLHwn5CxCg': Uint8Array.from([
+    240,
+    159,
+    144,
+    177,
+    240,
+    159,
+    144,
+    177,
+    240,
+    159,
+    144,
+    177,
+    10,
+  ]),
+};
+
+it.each(Object.entries(tests))('should convert %s to %s', (input, expected) => {
+  const result = urlB64ToUint8Array(input);
+  expect(result).toStrictEqual(expected);
+});
