3539376ed960db6f812d2f1d533fbde253e67fc6
Remco Haszing <remcohaszing@gmail.com>
2020-06-17 16:43:27 +0200

    Specify missing redirect prop in app

--- packages/app/src/components/OpenIDLogin/index.tsx
+++ packages/app/src/components/OpenIDLogin/index.tsx
@@ -1,4 +1,4 @@
-import { Content, OAuth2LoginButton } from '@appsemble/react-components';
+import { Content, OAuth2LoginButton, useQuery } from '@appsemble/react-components';
 import * as React from 'react';
 import { FormattedMessage } from 'react-intl';
 
@@ -7,12 +7,15 @@ import styles from './index.css';
 import messages from './messages';
 
 export default function OpenIDLogin(): React.ReactElement {
+  const qs = useQuery();
+
   return (
     <Content className={styles.root}>
       <OAuth2LoginButton
         authorizationUrl={String(new URL('/connect/authorize', settings.apiUrl))}
         clientId={`app:${settings.id}`}
         icon="user"
+        redirect={qs.get('redirect')}
         redirectUrl="/Callback"
         scope="email openid profile resources:manage"
       >
