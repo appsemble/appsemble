afd2805e69e2efd4c0595f72c560d6e7f6ad3f4e
Wessel Kuipers <wessel@d-centralize.nl>
2020-06-18 18:09:03 +0200

    move StringField requirement description to description of StringField

--- blocks/form/README.md
+++ blocks/form/README.md
@@ -4,23 +4,6 @@ A block used for inputting and submitting sets of data.
 
 ![Form screenshot](https://gitlab.com/appsemble/appsemble/-/raw/0.13.3/docs/images/form.png)
 
-### String fields
-
-Fields with the type of [`string`](#StringField) support requirements. This is an array of
-requirements that are used to validate the value the user inputs. Each requirement can be provided
-with its own custom error message, allowing for better feedback towards users.
-
-For example, the [`regex`](#RegexRequirement) requirement type allows you to validate a field using
-[`regex`](https://learnxinyminutes.com/docs/pcre/). So for example if you want a string field that
-requires a field to be an email address that ends with “@appsemble.com”, you could enforce this like
-so:
-
-```yaml
-requirements:
-  - regex: \w+@appsemble\.com
-    errorMessage: Value does not end with “@appsemble.com”
-```
-
 ### Notes
 
 > Loading initial form data does not work with inputs of type `geocoordinates`.
--- blocks/form/block.ts
+++ blocks/form/block.ts
@@ -240,6 +240,23 @@ export interface NumberField extends AbstractField {
   type: 'integer' | 'number';
 }
 
+/**
+ * Fields with the type of [`string`](#StringField) support requirements. This is an array of
+ * requirements that are used to validate the value the user inputs. Each requirement can be
+ * provided with its own custom error message, allowing for better feedback towards users.
+ *
+ * For example, the [`regex`](#RegexRequirement) requirement type allows you to validate a field
+ * using [Regular Expressions](https://learnxinyminutes.com/docs/pcre/). So for example if you want
+ * a string field that requires a field to be an email address that ends with “@appsemble.com”,
+ * you could enforce this like so:
+ *
+ * ```yaml
+ * type: string
+ * requirements:
+ *   - regex: \w+@appsemble\.com
+ *     errorMessage: Value does not end with “@appsemble.com”
+ * ```
+ */
 export interface StringField extends AbstractField {
   /**
    * The default value of the field.
