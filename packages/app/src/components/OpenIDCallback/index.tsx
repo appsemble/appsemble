baaa7096b12c8ffb049b2b67cf89c9781c1e8c3c
Remco Haszing <remcohaszing@gmail.com>
2020-06-19 11:00:58 +0200

    Guide app user to login screen on OAuth2 error
    This puts the user in charge of how they want to handle the error. For
    example, when multiple login methods are added, the user can try another
    method.

--- packages/app/src/components/OpenIDCallback/index.tsx
+++ packages/app/src/components/OpenIDCallback/index.tsx
@@ -1,11 +1,10 @@
-import { Content, Loader, Message, OAuth2LoginButton, useQuery } from '@appsemble/react-components';
+import { Content, Loader, Message, useQuery } from '@appsemble/react-components';
 import { normalize } from '@appsemble/utils';
 import { clearOAuth2State, loadOAuth2State } from '@appsemble/web-utils';
 import * as React from 'react';
 import { FormattedMessage } from 'react-intl';
-import { Redirect } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 
-import settings from '../../utils/settings';
 import { useAppDefinition } from '../AppDefinitionProvider';
 import { useUser } from '../UserProvider';
 import styles from './index.css';
@@ -26,6 +25,7 @@ export default function OpenIDCallback(): React.ReactElement {
 
   const [error, setError] = React.useState(false);
 
+  const { redirect } = session;
   const stateOk = state && session.state && state === session.state;
   const isOk = code && !errorMessage && !error && !isLoggedIn && stateOk;
 
@@ -47,7 +47,7 @@ export default function OpenIDCallback(): React.ReactElement {
   }, [isLoggedIn]);
 
   if (isLoggedIn) {
-    return <Redirect to={session.redirect || normalize(definition.defaultPage)} />;
+    return <Redirect to={redirect || normalize(definition.defaultPage)} />;
   }
 
   if (!isOk) {
@@ -60,16 +60,12 @@ export default function OpenIDCallback(): React.ReactElement {
             <FormattedMessage {...messages.error} />
           )}
         </Message>
-        <OAuth2LoginButton
-          authorizationUrl={String(new URL('/connect/authorize', settings.apiUrl))}
-          clientId={`app:${settings.id}`}
-          icon="user"
-          redirect={session.redirect}
-          redirectUrl="/Callback"
-          scope="email openid profile resources:manage"
+        <Link
+          className="button"
+          to={{ pathname: '/Login', search: String(new URLSearchParams({ redirect })) }}
         >
           <FormattedMessage {...messages.retry} />
-        </OAuth2LoginButton>
+        </Link>
       </Content>
     );
   }
