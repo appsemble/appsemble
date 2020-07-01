a7d0683b702b64b644d56a3f7a129b26e445e7a9
Remco Haszing <remcohaszing@gmail.com>
2020-07-01 13:57:24 +0200

    Add migrations

---
+++ packages/server/src/migrations/0.13.4-app-oauth2-provider.ts
@@ -0,0 +1,75 @@
+import { logger } from '@appsemble/node-utils';
+import { DataTypes } from 'sequelize';
+
+import type { Migration } from '../utils/migrate';
+
+export default {
+  key: '0.13.3',
+
+  /**
+   * Summary:
+   * - Query all blocks with events
+   * - Parse them back into JS objects
+   * - Convert all event emitters and listeners to empty objects
+   */
+  async up(db) {
+    const queryInterface = db.getQueryInterface();
+    logger.info('Adding new table AppOAuth2Secret');
+    await queryInterface.createTable('AppOAuth2Secret', {
+      id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+      authorizationUrl: { type: DataTypes.STRING, allowNull: false },
+      tokenUrl: { type: DataTypes.STRING, allowNull: false },
+      userInfoUrl: { type: DataTypes.STRING },
+      clientId: { type: DataTypes.STRING, allowNull: false },
+      clientSecret: { type: DataTypes.STRING, allowNull: false },
+      icon: { type: DataTypes.STRING, allowNull: false },
+      name: { type: DataTypes.STRING, allowNull: false },
+      scope: { type: DataTypes.STRING, allowNull: false },
+      created: { type: DataTypes.DATE, allowNull: false },
+      updated: { type: DataTypes.DATE, allowNull: false },
+      AppId: { type: DataTypes.NUMBER, allowNull: false },
+    });
+    await queryInterface.addConstraint('AppOAuth2Secret', ['AppId'], {
+      type: 'foreign key',
+      name: 'AppOAuth2Secret_AppId_fkey',
+      onDelete: 'cascade',
+      onUpdate: 'cascade',
+      references: { table: 'App', field: 'id' },
+    });
+
+    logger.info('Adding new table AppOAuth2Authorization');
+    await queryInterface.createTable('AppOAuth2Authorization', {
+      sub: { type: DataTypes.STRING, primaryKey: true },
+      AppOAuth2SecretId: { type: DataTypes.INTEGER, primaryKey: true },
+      accessToken: { type: DataTypes.STRING, allowNull: false },
+      expires: { type: DataTypes.DATE },
+      refreshToken: { type: DataTypes.STRING },
+      created: { type: DataTypes.DATE, allowNull: false },
+      updated: { type: DataTypes.DATE, allowNull: false },
+      UserId: { type: DataTypes.UUID, allowNull: false },
+    });
+    await queryInterface.addConstraint('AppOAuth2Authorization', ['AppId'], {
+      type: 'foreign key',
+      name: 'AppOAuth2Authorization_AppId_fkey',
+      onDelete: 'cascade',
+      onUpdate: 'cascade',
+      references: { table: 'App', field: 'id' },
+    });
+    await queryInterface.addConstraint('AppOAuth2Authorization', ['UserId'], {
+      type: 'foreign key',
+      name: 'AppOAuth2Authorization_UserId_fkey',
+      onDelete: 'cascade',
+      onUpdate: 'cascade',
+      references: { table: 'User', field: 'id' },
+    });
+  },
+
+  async down(db) {
+    const queryInterface = db.getQueryInterface();
+    logger.warn('Dropping table AppOAuth2Authorization');
+    await queryInterface.dropTable('AppOAuth2Authorization');
+
+    logger.warn('Dropping table AppOAuth2Secret');
+    await queryInterface.dropTable('AppOAuth2Secret');
+  },
+} as Migration;
