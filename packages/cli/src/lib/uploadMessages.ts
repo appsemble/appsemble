ce3a7a28549a6aed13410f1d26898db56e3caa0e
Wessel Kuipers <wessel@d-centralize.nl>
2020-07-13 15:23:05 +0200

    add uploading messages to update/create app commands

---
+++ packages/cli/src/lib/uploadMessages.ts
@@ -0,0 +1,33 @@
+import { logger } from '@appsemble/node-utils';
+import axios from 'axios';
+import fs from 'fs-extra';
+import yaml from 'js-yaml';
+import { join } from 'path';
+
+export default async function uploadMessages(path: string, appId: string): Promise<void> {
+  if (!fs.existsSync(join(path, 'messages'))) {
+    return;
+  }
+
+  const messageDir = await fs.readdir(join(path, 'messages'));
+
+  if (messageDir.length === 0) {
+    return;
+  }
+
+  logger.info(`Traversing app messages for ${messageDir.length} languages 🕵`);
+  const result: { language: string; content: object }[] = [];
+
+  for (const messageFile of messageDir) {
+    logger.verbose(`Processing ${join(path, 'messages', messageFile)} ⚙️`);
+    const [language] = messageFile.split('.');
+    const file = await fs.readFile(join(path, 'messages', messageFile), 'utf8');
+    const content = yaml.safeLoad(file);
+    result.push({ language, content });
+  }
+
+  for (const language of result) {
+    await axios.post(`/api/apps/${appId}/translations`, language);
+    logger.info(`Successfully uploaded messages for language “${language.language}” 🎉`);
+  }
+}
