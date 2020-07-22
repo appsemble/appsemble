ee84a582ae912b5839b9215dfe30a91bffbe827b
Remco Haszing <remcohaszing@gmail.com>
2020-07-22 15:57:41 +0200

    Fix some routing and spacing issues in app
    The standard Bulma class `has-navbar-fixed-top` is used.
    
    Redirecting the user is now handled using the `<Redirect />` component.
    Previously this was done by calling `history.replace()` in the render
    method, which caused several issues.

--- packages/app/src/components/Page/index.tsx
+++ packages/app/src/components/Page/index.tsx
@@ -1,12 +1,13 @@
-import { useLocationString, useMessages } from '@appsemble/react-components';
+import { Button, Content, Message, useLocationString } from '@appsemble/react-components';
 import type { PageDefinition } from '@appsemble/types';
 import { checkAppRole, normalize } from '@appsemble/utils';
 import { EventEmitter } from 'events';
 import React, { ReactElement, useCallback, useEffect, useRef, useState } from 'react';
-import { useIntl } from 'react-intl';
-import { Redirect, Route, Switch, useHistory, useRouteMatch } from 'react-router-dom';
+import { FormattedMessage } from 'react-intl';
+import { Redirect, Route, Switch, useRouteMatch } from 'react-router-dom';
 
 import type { ShowDialogParams } from '../../types';
+import settings from '../../utils/settings';
 import { useAppDefinition } from '../AppDefinitionProvider';
 import BlockList from '../BlockList';
 import FlowPage from '../FlowPage';
@@ -19,11 +20,8 @@ import messages from './messages';
 
 export default function Page(): ReactElement {
   const { definition } = useAppDefinition();
-  const history = useHistory();
-  const { formatMessage } = useIntl();
-  const push = useMessages();
   const redirect = useLocationString();
-  const { isLoggedIn, logout, role } = useUser();
+  const { isLoggedIn, role } = useUser();
   const {
     params: { pageId },
     path,
@@ -40,51 +38,6 @@ export default function Page(): ReactElement {
   const index = definition.pages.findIndex((p) => normalize(p.name) === pageId);
   const page = index === -1 ? null : definition.pages[index];
   const prefix = index === -1 ? null : `pages.${index}`;
-
-  const handlePagePermissions = useCallback(() => {
-    const checkPagePermissions = (p: PageDefinition): boolean => {
-      const roles = p.roles || definition.roles || [];
-      return roles.length === 0 || roles.some((r) => checkAppRole(definition.security, r, role));
-    };
-
-    const permission = checkPagePermissions(page);
-    if (permission) {
-      return;
-    }
-
-    if (!isLoggedIn) {
-      history.replace(`/Login?${new URLSearchParams({ redirect })}`);
-      return;
-    }
-
-    // User is logged in but doesn’t have the right permissions
-    // Attempt to find a default page to redirect to
-    const defaultPagePermission = checkPagePermissions(
-      definition.pages.find((p) => p.name === definition.defaultPage),
-    );
-
-    if (defaultPagePermission) {
-      history.replace('/');
-      return;
-    }
-
-    // Redirect to the first page that doesn’t have parameters.
-    const redirectPage = definition.pages.find(
-      (p) => p.parameters === undefined && checkPagePermissions(p),
-    );
-
-    // Show message that explains the app is inaccessible with the current permissions.
-    if (!redirectPage) {
-      push({
-        body: formatMessage(messages.permissionLogout),
-        color: 'danger',
-        dismissable: true,
-      });
-      logout();
-    } else {
-      history.replace(`/${normalize(redirectPage.name)}`);
-    }
-  }, [definition, formatMessage, history, isLoggedIn, logout, page, push, redirect, role]);
 
   const showDialog = useCallback((d: ShowDialogParams) => {
     setDialog(d);
@@ -120,52 +73,98 @@ export default function Page(): ReactElement {
     };
   }, [page]);
 
-  if (definition.security) {
-    handlePagePermissions();
+  const checkPagePermissions = (p: PageDefinition): boolean => {
+    const roles = p.roles || definition.roles || [];
+    return roles.length === 0 || roles.some((r) => checkAppRole(definition.security, r, role));
+  };
+
+  // If the user is on an existing page and is allowed to view it, render it.
+  if (page && checkPagePermissions(page)) {
+    return (
+      <main className={styles.root} data-path={prefix}>
+        <TitleBar>{page.name}</TitleBar>
+        {page.type === 'tabs' ? (
+          <TabsPage
+            ee={ee.current}
+            page={page}
+            prefix={prefix}
+            showDialog={showDialog}
+            subPages={page.subPages}
+          />
+        ) : (
+          // The switch is used to enforce an exact path.
+          <Switch>
+            <Route exact path={path}>
+              {page.type === 'flow' ? (
+                <FlowPage
+                  definition={definition}
+                  ee={ee.current}
+                  page={page}
+                  prefix={prefix}
+                  showDialog={showDialog}
+                />
+              ) : (
+                <BlockList
+                  blocks={page.blocks}
+                  ee={ee.current}
+                  page={page}
+                  prefix={`${prefix}.blocks`}
+                  showDialog={showDialog}
+                />
+              )}
+            </Route>
+            {/* Redirect from a matching sub URL to the actual URL */}
+            <Redirect to={url} />
+          </Switch>
+        )}
+        <PageDialog dialog={dialog} ee={ee.current} page={page} showDialog={showDialog} />
+      </main>
+    );
   }
 
-  if (!page) {
-    return <Redirect to={`/${normalize(definition.defaultPage)}`} />;
+  // If the user isn’t allowed to view the page, because they aren’t loffed in, redirect to the
+  // login page.
+  if (!isLoggedIn) {
+    return <Redirect to={`/Login?${new URLSearchParams({ redirect })}`} />;
   }
 
+  // If the user is logged in, but isn’t allowed to view the current page, redirect to the default
+  // page.
+  const defaultPage = definition.pages.find((p) => p.name === definition.defaultPage);
+  if (checkPagePermissions(defaultPage)) {
+    return <Redirect to={`/${normalize(defaultPage.name)}`} />;
+  }
+
+  // If the user isn’t allowed to view the default page either, find a page to redirect the user to.
+  const redirectPage = definition.pages.find(checkPagePermissions);
+  if (redirectPage) {
+    return <Redirect to={`/${normalize(redirectPage.name)}`} />;
+  }
+
+  // If the user isn’t allowed to view any pages, show an error message.
   return (
-    <main className={styles.root} data-path={prefix}>
-      <TitleBar>{page.name}</TitleBar>
-      {page.type === 'tabs' ? (
-        <TabsPage
-          ee={ee.current}
-          page={page}
-          prefix={prefix}
-          showDialog={showDialog}
-          subPages={page.subPages}
-        />
-      ) : (
-        // The switch is used to enforce an exact path.
-        <Switch>
-          <Route exact path={path}>
-            {page.type === 'flow' ? (
-              <FlowPage
-                definition={definition}
-                ee={ee.current}
-                page={page}
-                prefix={prefix}
-                showDialog={showDialog}
-              />
-            ) : (
-              <BlockList
-                blocks={page.blocks}
-                ee={ee.current}
-                page={page}
-                prefix={`${prefix}.blocks`}
-                showDialog={showDialog}
-              />
-            )}
-          </Route>
-          {/* Redirect from a matching sub URL to the actual URL */}
-          <Redirect to={url} />
-        </Switch>
-      )}
-      <PageDialog dialog={dialog} ee={ee.current} page={page} showDialog={showDialog} />
-    </main>
+    <Content padding>
+      <Message color="danger">
+        <p>
+          <FormattedMessage
+            {...messages.permissionError}
+            values={{
+              a: (text: string) => (
+                <a
+                  href={`${settings.apiUrl}/apps/${settings.id}`}
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  {text}
+                </a>
+              ),
+            }}
+          />
+        </p>
+        <Button className="mt-4" color="danger">
+          <FormattedMessage {...messages.logout} />
+        </Button>
+      </Message>
+    </Content>
   );
 }
