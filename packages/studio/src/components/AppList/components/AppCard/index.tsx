b87b58cb4ba2808e0a7db7ffdc15cb1b82076ecf
Wessel Kuipers <wessel@d-centralize.nl>
2020-07-31 15:25:31 +0200

    rework app cards

--- packages/studio/src/components/AppList/components/AppCard/index.tsx
+++ packages/studio/src/components/AppList/components/AppCard/index.tsx
@@ -1,10 +1,10 @@
+import { Subtitle, Title } from '@appsemble/react-components/src';
 import type { App } from '@appsemble/types';
 import classNames from 'classnames';
-import React, { ReactElement } from 'react';
-import { FormattedMessage, useIntl } from 'react-intl';
-import { Link, useRouteMatch } from 'react-router-dom';
+import React, { ReactElement, useCallback } from 'react';
+import { useIntl } from 'react-intl';
+import { Link, useHistory, useRouteMatch } from 'react-router-dom';
 
-import getAppUrl from '../../../../utils/getAppUrl';
 import Rating from '../../../Rating';
 import styles from './index.css';
 import messages from './messages';
@@ -16,42 +16,45 @@ interface AppCardProps {
 export default function AppCard({ app }: AppCardProps): ReactElement {
   const { formatMessage } = useIntl();
   const { url } = useRouteMatch();
+  const history = useHistory();
+  const onClick = useCallback(() => {
+    history.push(`${url}/${app.id}`);
+  }, [app, history, url]);
+
+  const onKeyDown = useCallback(
+    (event: React.KeyboardEvent<HTMLDivElement>) => {
+      if (event.key === 'Enter') {
+        onClick();
+      }
+    },
+    [onClick],
+  );
 
   return (
-    <div className={classNames('card', styles.appCard)}>
-      <header className="card-header">
-        <p className="card-header-title">{app.definition.name}</p>
-      </header>
+    <div
+      className={classNames('card', styles.appCard)}
+      onClick={onClick}
+      onKeyDown={onKeyDown}
+      role="button"
+      tabIndex={0}
+    >
       <div className={classNames('card-content', styles.appCardContent)}>
         <div className="media">
-          <figure className={classNames('image', 'is-64x64', styles.image)}>
+          <figure className={classNames('image', 'is-128x128', styles.image)}>
             <img alt={formatMessage(messages.icon)} src={`/api/apps/${app.id}/icon`} />
           </figure>
         </div>
-        {app.definition.description && (
-          <div className={classNames('content', styles.appDescription)}>
-            {app.definition.description}
-          </div>
-        )}
-        <Rating
-          className={styles.rating}
-          count={(app.rating && app.rating.count) || 0}
-          value={(app.rating && app.rating.average) || 0}
-        />
+        <Title level={4}>
+          <Link className="has-text-dark" to={`${url}/${app.id}`}>
+            {app.definition.name}
+          </Link>
+        </Title>
+        {/* XXX Make this a link to the organization page */}
+        <Subtitle className="mb-0" level={6}>
+          @{app.OrganizationId}
+        </Subtitle>
+        <Rating className={styles.rating} value={(app.rating && app.rating.average) || 0} />
       </div>
-      <footer className={classNames('card-footer', styles.appCardFooter)}>
-        <a
-          className="card-footer-item"
-          href={getAppUrl(app.OrganizationId, app.path, app.domain)}
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          <FormattedMessage {...messages.view} />
-        </a>
-        <Link className="card-footer-item" to={`${url}/${app.id}`}>
-          <FormattedMessage {...messages.details} />
-        </Link>
-      </footer>
     </div>
   );
 }
--- packages/studio/src/components/AppList/components/AppCard/messages.tsx
+++ packages/studio/src/components/AppList/components/AppCard/messages.tsx
@@ -3,5 +3,4 @@ import { defineMessages } from 'react-intl';
 export default defineMessages({
   details: 'Details',
   icon: 'Icon',
-  view: 'View',
 });
