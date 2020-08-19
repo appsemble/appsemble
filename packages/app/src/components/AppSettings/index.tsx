bda4d7e2362b32df4911518ad7e0dcc3a5bed4c1
Wessel Kuipers <wessel@d-centralize.nl>
2020-08-19 19:13:13 +0200

    extract AppSubscriptions component

--- packages/app/src/components/AppSettings/index.tsx
+++ packages/app/src/components/AppSettings/index.tsx
@@ -1,221 +1,24 @@
-import { Checkbox, Content, FormComponent, Loader, useMessages } from '@appsemble/react-components';
-import type { ResourceHooks, SubscriptionResponse } from '@appsemble/types';
-import axios from 'axios';
-import React, { ChangeEvent, ReactElement, useEffect, useState } from 'react';
-import { FormattedMessage, useIntl } from 'react-intl';
+import { Content, Title } from '@appsemble/react-components';
+import React, { ReactElement } from 'react';
+import { FormattedMessage } from 'react-intl';
 
-import { apiUrl, appId } from '../../utils/settings';
-import { useAppDefinition } from '../AppDefinitionProvider';
-import { useServiceWorkerRegistration } from '../ServiceWorkerRegistrationProvider';
 import { TitleBar } from '../TitleBar';
-import styles from './index.css';
+import { AppSubscriptions } from './AppSubscriptions';
 import { messages } from './messages';
 
-interface ResourceState {
-  [resourceType: string]: SubscriptionState;
-}
-
-type ExtendedResourceHooks = ResourceHooks & { subscribed: boolean };
-
-interface SubscriptionState {
-  create?: ExtendedResourceHooks;
-  update?: ExtendedResourceHooks;
-  delete?: ExtendedResourceHooks;
-}
-
 /**
- * The main entry point of the React app.
- *
- * This configures all providers and sets up the global app structure.
+ * Page containing all the settings configurable for an app
  */
 export function AppSettings(): ReactElement {
-  const { formatMessage } = useIntl();
-  const push = useMessages();
-  const [subscriptions, setSubscriptions] = useState<ResourceState>();
-  const { definition } = useAppDefinition();
-  const {
-    requestPermission,
-    subscribe,
-    subscription,
-    unsubscribe,
-  } = useServiceWorkerRegistration();
-
-  useEffect(() => {
-    const subs = Object.entries(definition.resources).reduce<ResourceState>(
-      (acc, [resourceType, resource]) => {
-        Object.keys(resource)
-          .filter((key) => ['create', 'update', 'delete'].includes(key))
-          .forEach((key: keyof SubscriptionState) => {
-            if (resource[key].hooks?.notification?.subscribe) {
-              if (!Object.hasOwnProperty.call(acc, resourceType)) {
-                acc[resourceType] = {};
-              }
-              acc[resourceType][key] = { ...resource[key].hooks, subscribed: false };
-            }
-          });
-
-        return acc;
-      },
-      {},
-    );
-
-    if (subscription) {
-      const { endpoint } = subscription;
-      axios
-        .get<SubscriptionResponse>(`${apiUrl}/api/apps/${appId}/subscriptions`, {
-          params: { endpoint },
-        })
-        .then(({ data }) => {
-          Object.entries(data).forEach(([key, resource]) => {
-            if (!Object.hasOwnProperty.call(subs, key)) {
-              return;
-            }
-
-            Object.entries(resource).forEach(([action, value]) => {
-              if (!Object.hasOwnProperty.call(subs[key], action)) {
-                return;
-              }
-
-              subs[key][action as keyof SubscriptionState].subscribed = value;
-            });
-          });
-          setSubscriptions(subs);
-        })
-        .catch(() => {
-          push('Something went wrong when trying to fetch your subscription settings');
-          setSubscriptions(subs);
-        });
-    } else {
-      setSubscriptions(subs);
-    }
-  }, [definition, push, subscription]);
-
-  const onSubscribeClick = async (event: ChangeEvent<HTMLInputElement>): Promise<void> => {
-    event.preventDefault();
-
-    if (subscription) {
-      await unsubscribe();
-      push({ body: formatMessage(messages.unsubscribeSuccess), color: 'info' });
-      return;
-    }
-
-    if (window.Notification?.permission === 'denied') {
-      push({ body: formatMessage(messages.blocked), color: 'warning' });
-      return;
-    }
-
-    const result = await requestPermission();
-
-    if (result !== 'granted') {
-      push({ body: formatMessage(messages.permissionDenied), color: 'danger' });
-      return;
-    }
-
-    try {
-      await subscribe();
-      push({ body: formatMessage(messages.subscribeSuccessful), color: 'success' });
-    } catch {
-      push({ body: formatMessage(messages.subscribeError), color: 'danger' });
-    }
-  };
-
-  const onSubscriptionChange = async (
-    resource: string,
-    action: keyof SubscriptionState,
-    value: boolean,
-  ): Promise<void> => {
-    try {
-      const { endpoint } = subscription;
-      await axios.patch(`${apiUrl}/api/apps/${appId}/subscriptions`, {
-        endpoint,
-        resource,
-        action,
-        value,
-      });
-
-      setSubscriptions({
-        ...subscriptions,
-        [resource]: {
-          ...subscriptions[resource],
-          [action]: { ...subscriptions[resource][action], subscribed: value },
-        },
-      });
-    } catch {
-      push({ body: formatMessage(messages.subscribeError), color: 'danger' });
-    }
-  };
-
-  if (subscriptions == null) {
-    return <Loader />;
-  }
-
   return (
     <Content main padding>
       <TitleBar>
         <FormattedMessage {...messages.settings} />
       </TitleBar>
-      {(definition.notifications !== undefined || Object.keys(subscriptions).length > 0) && (
-        <>
-          <FormComponent label={<FormattedMessage {...messages.notifications} />} required>
-            <div className={`${styles.setting} is-flex`}>
-              <p className={styles.settingDescription}>
-                <FormattedMessage {...messages.suscribeDescription} />
-              </p>
-              <Checkbox
-                className={styles.checkbox}
-                help={<FormattedMessage {...messages.subscribe} />}
-                name="subscribe"
-                onChange={onSubscribeClick}
-                switch
-                value={Boolean(subscription)}
-                wrapperClassName="is-flex"
-              />
-            </div>
-          </FormComponent>
-
-          <FormComponent label={<FormattedMessage {...messages.subscriptions} />} required>
-            {Object.entries(subscriptions).map(([resourceType, resource]) => (
-              <FormComponent
-                key={resourceType}
-                label={
-                  <FormattedMessage
-                    {...messages.resourceSubscriptions}
-                    values={{ resource: resourceType }}
-                  />
-                }
-                required
-              >
-                {(Object.keys(resource) as (keyof SubscriptionState)[])
-                  .filter(
-                    (key) =>
-                      resource[key].notification.subscribe === 'all' ||
-                      resource[key].notification.subscribe === 'both',
-                  )
-                  .map((key) => (
-                    <Checkbox
-                      className={styles.subscribeCheckbox}
-                      disabled={!subscription || !resource.create}
-                      help={
-                        <FormattedMessage
-                          {...messages.subscriptionLabel}
-                          values={{
-                            resource: resourceType,
-                            actionVerb: <FormattedMessage {...messages[key]} />,
-                          }}
-                        />
-                      }
-                      key={key}
-                      name={`${resourceType}.${key}`}
-                      onChange={(_, value) => onSubscriptionChange(resourceType, key, value)}
-                      switch
-                      value={subscriptions[resourceType][key].subscribed}
-                    />
-                  ))}
-              </FormComponent>
-            ))}
-          </FormComponent>
-        </>
-      )}
+      <Title level={2}>
+        <FormattedMessage {...messages.settings} />
+      </Title>
+      <AppSubscriptions />
     </Content>
   );
 }
--- packages/app/src/components/AppSettings/messages.tsx
+++ packages/app/src/components/AppSettings/messages.tsx
@@ -1,24 +1,5 @@
 import { defineMessages } from 'react-intl';
 
 export const messages = defineMessages({
-  blocked: 'Notifications have been blocked. Please update your site and try again.',
   settings: 'Settings',
-  subscribe: 'Subscribe',
-  suscribeDescription:
-    'Subscribing to an app allows for the app creators to connect with their users by sending push notifications.',
-  notifications: 'Notifications',
-  subscribed: 'Subscribed',
-  notSubscribed: 'Not subscribed',
-  subscribeSuccessful: 'Subscribed successfully!',
-  subscribeError: 'Something went wrong when trying to subscribe.',
-  unsubscribe: 'Unsubscribe',
-  unsubscribeSuccess: 'Successfully unsubscribed from notifications.',
-  permissionDenied:
-    'Unable to subscribe. Either no permission has been granted or your device does not support notifications.',
-  subscriptions: 'Subscriptions',
-  resourceSubscriptions: '{resource} subscriptions',
-  subscriptionLabel: 'Get notified when “{resource}” {actionVerb}',
-  create: 'is created',
-  update: 'is updated',
-  delete: 'is deleted',
 });
