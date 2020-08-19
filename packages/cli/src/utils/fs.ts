48105c3263ebc15be78340cdfdfc0b538ec7e012
Remco Haszing <remcohaszing@gmail.com>
2020-08-19 17:09:24 +0200

    Add CLI function to simplify traversing directory
    This is used to traverse apps and themes directories.

---
+++ packages/cli/src/utils/fs.ts
@@ -0,0 +1,39 @@
+import { Dirent, promises as fs, Stats } from 'fs';
+import { join } from 'path';
+
+import { AppsembleError } from '@appsemble/node-utils/src';
+import type { Promisable } from 'type-fest';
+
+interface OpenDirSafeOptions {
+  allowMissing?: boolean;
+}
+
+/**
+ * Read the contents of a directory.
+ *
+ * @param directory - The path of the directory to open.
+ * @param onFile - A callback which will get called for every file. This will be called with the
+ * full file path as its first argument and its `Dirent` object as the second argument.
+ * @param options - Additional options.
+ */
+export async function opendirSafe(
+  directory: string,
+  onFile: (fullpath: string, stat: Dirent) => Promisable<void>,
+  { allowMissing }: OpenDirSafeOptions = {},
+): Promise<void> {
+  let stats: Stats;
+  try {
+    stats = await fs.stat(directory);
+  } catch (err) {
+    if (!(allowMissing && err.code === 'ENOENT')) {
+      throw new AppsembleError(`Expected ${directory} to be a directory`);
+    }
+  }
+  if (!stats.isDirectory()) {
+    throw new AppsembleError(`Expected ${directory} to be a directory`);
+  }
+  const dir = await fs.opendir(directory);
+  for await (const file of dir) {
+    await onFile(join(directory, file.name), file);
+  }
+}
