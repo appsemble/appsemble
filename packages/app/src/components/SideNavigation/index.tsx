9cafcb96e9f49f6a060a0faf0a114c4283cd3bc6
Wessel Kuipers <wessel@d-centralize.nl>
2020-08-20 14:33:36 +0200

    use matched route for menu redirects

--- packages/app/src/components/SideNavigation/index.tsx
+++ packages/app/src/components/SideNavigation/index.tsx
@@ -2,7 +2,7 @@ import { Icon } from '@appsemble/react-components';
 import type { PageDefinition } from '@appsemble/types';
 import { normalize } from '@appsemble/utils';
 import React, { ReactElement } from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useRouteMatch } from 'react-router-dom';
 
 import { SideMenu } from '../SideMenu';
 import styles from './index.css';
@@ -15,13 +15,15 @@ interface SideNavigationProps {
  * The app navigation that is displayed in the side menu.
  */
 export function SideNavigation({ pages }: SideNavigationProps): ReactElement {
+  const route = useRouteMatch();
+
   return (
     <SideMenu>
       <nav>
         <ul className={`menu-list ${styles.menuList}`}>
           {pages.map((page) => (
             <li key={page.name}>
-              <NavLink activeClassName={styles.active} to={`/${normalize(page.name)}`}>
+              <NavLink activeClassName={styles.active} to={`${route.url}/${normalize(page.name)}`}>
                 {page.icon ? <Icon className={styles.icon} icon={page.icon} /> : null}
                 <span>{page.name}</span>
               </NavLink>
