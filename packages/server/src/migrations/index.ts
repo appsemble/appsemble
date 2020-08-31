c1bcf8fd699242a4f1e863f4aae14ed75fcac935
Wessel Kuipers <wessel@d-centralize.nl>
2020-08-31 16:19:39 +0200

    autosort migration imports

--- packages/server/src/migrations/index.ts
+++ packages/server/src/migrations/index.ts
@@ -1,9 +1,4 @@
-/* eslint eslint-comments/no-use: off */
-/* eslint-disable import/order */
 import type { Migration } from '../utils/migrate';
-import * as m1 from './0.9.0';
-import * as m2 from './0.9.3';
-import * as m3 from './0.9.4';
 import * as m4 from './0.10.0';
 import * as m5 from './0.11.0';
 import * as m6 from './0.11.3';
@@ -11,14 +6,16 @@ import * as m7 from './0.12.4';
 import * as m8 from './0.12.6';
 import * as m9 from './0.13.0';
 import * as m10 from './0.13.1';
+import * as m16 from './0.13.11';
+import * as m17 from './0.13.12';
 import * as m11 from './0.13.2';
 import * as m12 from './0.13.3';
 import * as m13 from './0.13.5';
 import * as m14 from './0.13.6';
 import * as m15 from './0.13.9';
-import * as m16 from './0.13.11';
-import * as m17 from './0.13.12';
-/* eslint-enable import/order */
+import * as m1 from './0.9.0';
+import * as m2 from './0.9.3';
+import * as m3 from './0.9.4';
 
 export const migrations: Migration[] = [
   m1,
