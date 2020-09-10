7e68702d5017b5d236f73c3803f67d6468e8fbbf
Remco Haszing <remcohaszing@gmail.com>
2020-09-10 13:17:27 +0200

    Reset error state when user navigates from render error
    This resets the error state when a user tries to resolve uncaught errors
    by navigating. This doesn’t resolve all issues that might arise when an
    uncaught error occurs, but it provides an escape in certain situations.

--- packages/react-components/src/ErrorHandler/index.tsx
+++ packages/react-components/src/ErrorHandler/index.tsx
@@ -1,8 +1,16 @@
 import { captureException, withScope } from '@sentry/browser';
 import React, { Component, ElementType, ErrorInfo, ReactNode } from 'react';
+import { RouteComponentProps, withRouter } from 'react-router-dom';
 
-interface ErrorHandlerProps {
+interface ErrorHandlerProps extends RouteComponentProps<any> {
+  /**
+   * Children to render.
+   */
   children: ReactNode;
+
+  /**
+   * The fallback to render in case an error occurs rendering children.
+   */
   fallback: ElementType;
 }
 
@@ -11,27 +19,39 @@ interface ErrorHandlerState {
 }
 
 /**
- * Capture renderer errors using Sentry.
+ * Capture React render errors.
+ *
+ * The are captured errors using Sentry.
  */
-export class ErrorHandler extends Component<ErrorHandlerProps, ErrorHandlerState> {
-  state: ErrorHandlerState = {
-    error: false,
-  };
+export const ErrorHandler = withRouter(
+  class extends Component<ErrorHandlerProps, ErrorHandlerState> {
+    state: ErrorHandlerState = {
+      error: false,
+    };
 
-  componentDidCatch(error: Error, info: ErrorInfo): void {
-    this.setState({ error: true });
-    withScope((scope) => {
-      Object.entries(info).forEach(([key, value]) => {
-        scope.setExtra(key, value);
+    componentDidMount(): void {
+      const { history } = this.props;
+
+      history.listen(() => {
+        this.setState({ error: false });
       });
-      captureException(error);
-    });
-  }
+    }
 
-  render(): ReactNode {
-    const { children, fallback: Fallback } = this.props;
-    const { error } = this.state;
+    componentDidCatch(error: Error, info: ErrorInfo): void {
+      this.setState({ error: true });
+      withScope((scope) => {
+        Object.entries(info).forEach(([key, value]) => {
+          scope.setExtra(key, value);
+        });
+        captureException(error);
+      });
+    }
 
-    return error ? <Fallback /> : children;
-  }
-}
+    render(): ReactNode {
+      const { children, fallback: Fallback } = this.props;
+      const { error } = this.state;
+
+      return error ? <Fallback /> : children;
+    }
+  },
+);
