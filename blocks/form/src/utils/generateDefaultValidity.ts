ef3fe1bb912ffe6c465b0bc7c6ec3c81c4644f89
Remco Haszing <remcohaszing@gmail.com>
2020-09-14 11:59:59 +0200

    Make form validators optional
    This is a cleaner solution than defining empty functions.

--- blocks/form/src/utils/generateDefaultValidity.ts
+++ blocks/form/src/utils/generateDefaultValidity.ts
@@ -7,7 +7,9 @@ export function generateDefaultValidity(
   data: any,
 ): { [field: string]: boolean } {
   return parameters.fields.reduce<{ [field: string]: boolean }>((acc, field) => {
-    const valid = validators[field.type](field, data[field.name]);
+    const valid = Object.hasOwnProperty.call(validators, field.type)
+      ? validators[field.type](field, data[field.name])
+      : undefined;
     acc[field.name] = valid === undefined;
     return acc;
   }, {});
--- blocks/form/src/utils/validators/index.ts
+++ blocks/form/src/utils/validators/index.ts
@@ -7,17 +7,9 @@ export const validators: { [name: string]: Validator } = {
   file: validateFile,
   geocoordinates: (_, value: { longitude: number; latitude: number }) =>
     value?.latitude && value?.longitude ? undefined : {},
-  hidden: () => {},
   string: validateString,
   number: validateNumber,
   integer: validateNumber,
-  boolean: () => {},
-  radio: () => {},
-  enum: () => {},
 };
 
-type Validator = (
-  field: Field,
-  value: unknown,
-  // eslint-disable-next-line @typescript-eslint/no-invalid-void-type
-) => BaseRequirement | void;
+type Validator = (field: Field, value: unknown) => BaseRequirement;
