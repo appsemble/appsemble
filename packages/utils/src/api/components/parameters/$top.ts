fccaa953a335760105183c10850c47dc39f66ff2
Remco Haszing <remcohaszing@gmail.com>
2020-09-21 11:45:06 +0200

    Fix OpenAPI document

--- packages/utils/src/api/components/parameters/$top.ts
+++ packages/utils/src/api/components/parameters/$top.ts
@@ -4,6 +4,6 @@ export const $top: OpenAPIV3.ParameterObject = {
   name: '$top',
   in: 'query',
   description: 'Limit the number of entities returned.',
-  // XXX This doesn’t work due to an upstream bug in Koas.
-  // schema: { type: 'integer' },
+  // XXX The type should be integer. This doesn’t work due to an upstream bug in Koas.
+  schema: { type: 'string' },
 };
