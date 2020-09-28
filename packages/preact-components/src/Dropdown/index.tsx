dc37107698bc9f54d7441c5bcad911337c9b2204
Wessel Kuipers <wesselkuipers@gmail.com>
2020-09-28 17:30:17 +0200

    add dropdown to preact components

---
+++ packages/preact-components/src/Dropdown/index.tsx
@@ -0,0 +1,68 @@
+import type { IconName } from '@fortawesome/fontawesome-common-types';
+import classNames from 'classnames';
+import { ComponentChildren, h, VNode } from 'preact';
+import { useCallback, useRef } from 'preact/hooks';
+
+import { Button, useClickOutside, useToggle } from '..';
+
+interface DropdownProps {
+  /**
+   * The children to render as menu items.
+   *
+   * Typically these are nodes that have the `dropdown-item` or `dropdown-divicer` class.
+   */
+  children: ComponentChildren;
+
+  /**
+   * An optional class name to add to the root element.
+   */
+  className?: string;
+
+  /**
+   * The label to render on the menu toggle button.
+   */
+  label: VNode;
+
+  /**
+   * The icon to display next to the label.
+   */
+  icon?: IconName;
+}
+
+/**
+ * Render an aria compliant Bulma dropdown menu.
+ */
+export function Dropdown({ children, className, icon, label }: DropdownProps): VNode {
+  const ref = useRef<HTMLDivElement>();
+  const { disable, enabled, toggle } = useToggle();
+
+  const onKeyDown = useCallback(
+    (event: KeyboardEvent) => {
+      if (event.key === 'Escape') {
+        disable();
+      }
+    },
+    [disable],
+  );
+
+  useClickOutside(ref, disable);
+
+  return (
+    <div className={classNames('dropdown', className, { 'is-active': enabled })} ref={ref}>
+      <div className="dropdown-trigger">
+        <Button aria-haspopup icon={icon} onClick={toggle} onKeyDown={onKeyDown}>
+          {label}
+        </Button>
+      </div>
+      <div
+        className="dropdown-menu"
+        onClick={toggle}
+        onKeyDown={onKeyDown}
+        role="menu"
+        tabIndex={0}
+      >
+        <div className="dropdown-content">{children}</div>
+      </div>
+    </div>
+  );
+}
---
+++ packages/preact-components/src/useEventListener.tsx
@@ -0,0 +1,42 @@
+import { useEffect } from 'preact/hooks';
+
+type EventListener<E extends Event = Event> = (event: E) => void;
+type Options = boolean | AddEventListenerOptions;
+
+/**
+ * Attach an event listener to an event target.
+ *
+ * @param target - The target to add the event listener to.
+ * @param type - The type of the event listener to add
+ * @param listener - The event listener callback function.
+ * @param options - Additional event listener options.
+ */
+export function useEventListener<K extends keyof DocumentEventMap>(
+  target: Document,
+  type: K,
+  listener: EventListener<DocumentEventMap[K]>,
+  options?: Options,
+): void;
+export function useEventListener<T extends HTMLElement, K extends keyof HTMLElementEventMap>(
+  target: T,
+  type: K,
+  listener: EventListener<HTMLElementEventMap[K]>,
+  options?: Options,
+): void;
+export function useEventListener(
+  target: EventTarget,
+  type: string,
+  listener: EventListener,
+  options?: Options,
+): void;
+export function useEventListener(
+  target: EventTarget,
+  type: string,
+  listener: EventListener,
+  options?: Options,
+): void {
+  useEffect(() => {
+    target.addEventListener(type, listener, options);
+    return () => target.removeEventListener(type, listener, options);
+  }, [listener, options, target, type]);
+}
