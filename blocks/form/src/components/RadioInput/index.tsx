3c2d7b903a46d1b93ec58c92b42a302c11c2984a
Wessel Kuipers <wesselkuipers@gmail.com>
2020-10-14 10:59:04 +0200

    use index instead of value for form radio button

--- blocks/form/src/components/RadioInput/index.tsx
+++ blocks/form/src/components/RadioInput/index.tsx
@@ -1,6 +1,7 @@
 import { useBlock } from '@appsemble/preact';
 import { RadioButton, RadioGroup } from '@appsemble/preact-components';
 import { h, VNode } from 'preact';
+import { useCallback } from 'preact/hooks';
 
 import type { InputProps, RadioField } from '../../../block';
 import { isRequired } from '../../utils/requirements';
@@ -27,6 +28,13 @@ export function RadioInput({
   const { label, options, tag } = field;
   const required = isRequired(field);
 
+  const handleOnChange = useCallback(
+    (event: Event, index: any) => {
+      onChange(event, options[index].value);
+    },
+    [onChange, options],
+  );
+
   return (
     <RadioGroup
       className="appsemble-radio"
@@ -34,18 +42,14 @@ export function RadioInput({
       error={dirty && error && utils.remap(invalidLabel, value)}
       label={utils.remap(label, value)}
       name={name}
-      onChange={onChange}
+      onChange={handleOnChange}
       optionalLabel={utils.remap(optionalLabel, value)}
       required={required}
       tag={utils.remap(tag, value)}
-      value={value}
+      value={options.findIndex((o) => o.value === value)}
     >
-      {options.map((option) => (
-        <RadioButton
-          key={String(option.value)}
-          value={option.value}
-          wrapperClassName={styles.choice}
-        >
+      {options.map((option, index) => (
+        <RadioButton key={option.value} value={index} wrapperClassName={styles.choice}>
           {utils.remap(option.label, {}) ?? option.value}
         </RadioButton>
       ))}
