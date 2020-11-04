8759ca1c76424a249930fd9803b89070ed3cd98e
Wessel Kuipers <wessel@d-centralize.nl>
2020-11-04 09:25:06 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- packages/app/src/utils/constants.test.ts
+++ packages/app/src/utils/constants.test.ts
@@ -4,7 +4,7 @@ import { OpenAPIV3 } from 'openapi-types';
 import { oauth2Scope } from './constants';
 
 describe('oauth2Scope', () => {
-  it('should match the alowed scopes in the API', () => {
+  it('should match the allowed scopes in the API', () => {
     const openApiDocument = api('');
     const scheme = openApiDocument.components.securitySchemes.app as OpenAPIV3.OAuth2SecurityScheme;
     const allowedScopes = Object.keys(scheme.flows.authorizationCode.scopes).sort().join(' ');
