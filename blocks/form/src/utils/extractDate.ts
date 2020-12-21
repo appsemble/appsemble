2348cdbfbed17302877dba5265422a69fc321ea2
Remco Haszing <remcohaszing@gmail.com>
2020-12-21 10:35:49 +0100

    Don’t crash form block if a date-time field doesn’t have a max and min

--- blocks/form/src/utils/extractDate.ts
+++ blocks/form/src/utils/extractDate.ts
@@ -1,5 +1,10 @@
+function pad(value: number, padding = 2): string {
+  return String(value).padStart(padding, '0');
+}
+
 export function extractDate(date: Date): string {
-  return `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, '0')}-${String(
-    date.getDate(),
-  ).padStart(2, '0')}`;
+  if (!date) {
+    return;
+  }
+  return `${pad(date.getFullYear(), 4)}-${pad(date.getMonth() + 1)}-${pad(date.getDate())}`;
 }
