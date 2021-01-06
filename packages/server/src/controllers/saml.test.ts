bdbf2ed8a43483dc714f341aada02876d4208a5f
Remco Haszing <remcohaszing@gmail.com>
2021-01-06 16:34:15 +0100

    Convert inline PEM content to fixtures

--- packages/server/src/controllers/saml.test.ts
+++ packages/server/src/controllers/saml.test.ts
@@ -2,6 +2,7 @@ import { URL, URLSearchParams } from 'url';
 import { promisify } from 'util';
 import { inflateRaw } from 'zlib';
 
+import { readFixture } from '@appsemble/node-utils';
 import { install, InstalledClock } from '@sinonjs/fake-timers';
 import { request, setTestApp } from 'axios-test-instance';
 import toXml from 'xast-util-to-xml';
@@ -19,79 +20,6 @@ let secret: AppSamlSecret;
 let user: User;
 
 const inflate = promisify(inflateRaw);
-
-const idpCertificate = `-----BEGIN CERTIFICATE-----
-MIICKzCCAdWgAwIBAgIJAM8DxRNtPj90MA0GCSqGSIb3DQEBBQUAMEUxCzAJBgNV
-BAYTAkFVMRMwEQYDVQQIEwpTb21lLVN0YXRlMSEwHwYDVQQKExhJbnRlcm5ldCBX
-aWRnaXRzIFB0eSBMdGQwHhcNMTEwODEyMjA1MTIzWhcNMTIwODExMjA1MTIzWjBF
-MQswCQYDVQQGEwJBVTETMBEGA1UECBMKU29tZS1TdGF0ZTEhMB8GA1UEChMYSW50
-ZXJuZXQgV2lkZ2l0cyBQdHkgTHRkMFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBANcN
-mgm4YlSUAr2xdWei5aRU/DbWtsQ47gjkv28Ekje3ob+6q0M+D5phwYDcv9ygYmuJ
-5wOi1cPprsWdFWmvSusCAwEAAaOBpzCBpDAdBgNVHQ4EFgQUzyBR9+vE8bygqvD6
-CZ/w6aQPikMwdQYDVR0jBG4wbIAUzyBR9+vE8bygqvD6CZ/w6aQPikOhSaRHMEUx
-CzAJBgNVBAYTAkFVMRMwEQYDVQQIEwpTb21lLVN0YXRlMSEwHwYDVQQKExhJbnRl
-cm5ldCBXaWRnaXRzIFB0eSBMdGSCCQDPA8UTbT4/dDAMBgNVHRMEBTADAQH/MA0G
-CSqGSIb3DQEBBQUAA0EAIQuPLA/mlMJAMF680kL7reX5WgyRwAtRzJK6FgNjE7kR
-aLZQ79UKYVYa0VAyrRdoNEyVhG4tJFEiQJzaLWsl/A==
------END CERTIFICATE-----
-`;
-const spCertificate = `-----BEGIN CERTIFICATE-----
-MIIC4TCCAcmgAwIBAgIBADANBgkqhkiG9w0BAQUFADA0MR4wHAYDVQQDExVodHRw
-Oi8vbG9jYWxob3N0Ojk5OTkxEjAQBgNVBAoTCUFwcHNlbWJsZTAeFw0yMDExMDYx
-MTIxMjNaFw0zMDExMDYxMTIxMjNaMDQxHjAcBgNVBAMTFWh0dHA6Ly9sb2NhbGhv
-c3Q6OTk5OTESMBAGA1UEChMJQXBwc2VtYmxlMIIBIjANBgkqhkiG9w0BAQEFAAOC
-AQ8AMIIBCgKCAQEAvoJXvGK6c3aWhA5qOqQdDozsSe8Ii01wtd3+joZs8X7rg2Vx
-ETZM47OUgw4gbH6Ar104aGxdvfp0w5sP8Dj18s3U8S3dpdEzz6cVqfobzt85oHtm
-d0GqbzDrNlgG27Uk4PgK1AZEUtOJd8+kK0mKa84B4RDHS62fm18s3MPN6r4YHqLP
-5WBuf2qjqXP9Glsro9SpwGF9a7Ufc1smtbDOg+zWLmVyXDfVOC1zfEek+TA+3PRQ
-3XVdXZKhiw49Afjc5MGUD08n3jESlc++XfAMgKuW6OUwMuE3Tvq1qqkdVZgYubA6
-v48ZAywfCl3GQ6qcXdF6nGGb0X3QYSR7GFUonwIDAQABMA0GCSqGSIb3DQEBBQUA
-A4IBAQAFBSSUwb8TRSTun2ZKAI4bovsZ9yBd4Nx7DpBB1Lfiag1+vdrFOUZOUYXF
-cjoS/0Dpr8BSGWnSRB9ea9RiIOBOVIshtTPsapNmNLsR5NmkSCFGC2euAWybqScS
-J9OH+HYXykXlY8p2AN6Y2ib5IeJYB03lepL32geDFsfuXoYZrEWxxcAChgy5CCtd
-69yO8GL8uW78Fr5X136m12+zacrQ+hmi7KBIGaqraSolpCUTJplc5f41AXjkJFzk
-x73C1Dw1O0acaz41CyyE7D0BXm7vWsXg7rHfHVoT3D4HjcRujIi1Oi5GqdyLAAFw
-MDm9VevTHL9rsmXufHriUcUP3z0I
------END CERTIFICATE-----
-`;
-const spPrivateKey = `-----BEGIN RSA PRIVATE KEY-----
-MIIEpAIBAAKCAQEAvoJXvGK6c3aWhA5qOqQdDozsSe8Ii01wtd3+joZs8X7rg2Vx
-ETZM47OUgw4gbH6Ar104aGxdvfp0w5sP8Dj18s3U8S3dpdEzz6cVqfobzt85oHtm
-d0GqbzDrNlgG27Uk4PgK1AZEUtOJd8+kK0mKa84B4RDHS62fm18s3MPN6r4YHqLP
-5WBuf2qjqXP9Glsro9SpwGF9a7Ufc1smtbDOg+zWLmVyXDfVOC1zfEek+TA+3PRQ
-3XVdXZKhiw49Afjc5MGUD08n3jESlc++XfAMgKuW6OUwMuE3Tvq1qqkdVZgYubA6
-v48ZAywfCl3GQ6qcXdF6nGGb0X3QYSR7GFUonwIDAQABAoIBADrfkvd/1zjpc/hZ
-cBCxON2PRVJP0WcjRmqdsRYgirhSzbtsdPWpvzc6/vFFQ1Xj/iaze0QiARI9vSnZ
-2KIXKEKhw2XJE88ziHxqmTJPPZq/t1JV8EN4gpAcIQRJTc1F/KCdZtNBIIosHzZs
-b8KE1occQc5o3f4QKNveHMIYrKEXFyEBLsviC0d1N7NHXFuj4fpiIevmW/VKYgwQ
-k8Q1RJaAUGMKFgniZWuot82LVnEii07hWlC7CQuHy1SLopdW/wa1yJIsIwN71uTV
-jgmdIQ05AtxdAKyNE9HBQea0vmnoh2hgv+WDBAqHeY3EAxxL0y/Asmxbwi2flKIy
-4W47b0kCgYEA6vshL7k1gqALlvRMrCOuv2DdlLP8fHA77eBBAx7zuSRLHl7KgguF
-opWO723+zu5I7K33I/hZ/7shlFJVcLEtzCeQc1Evp8PKGdvyrMaYgmNqPEMODgna
-0Mb0rJkKHea5XiKmAk6rB0bcEI6LtbFx4d8brguEM6pi8oPbP8EanEMCgYEAz4zY
-U5+MbNIOEzx9ifupz6wxT7K1n8wH4i0tVC4aeJ0qO4dsnJzil8fXsZ673HhuJbxP
-QfpDEjduukXeNgmo8fOScMSJB7+2G0NObUFMdVeWrHmgg9x6zGziYc2FTIeLU1JQ
-Tm5z1LCNCAjAJINrvOOnzeRzmjciub3k6XgoanUCgYAZBPg5npzF9ciGICjxwzLe
-CbBOHwPpNAINh+0kKe2FbwnYh/tD8XofH5GCiNGysshs+vDvkYfzpLcklcenIhZg
-QG4u+Rv73urr6fFi2Bpw96jeFb2vXPlBdY2juv/Kw0BFd6b5Y+rt8WuJD8RsaiJY
-DGtkErHW1nOGoud7WFI/BQKBgQCSmLcLibwT+OI8MBOz7G/q4YMK+f1TWzeOR76m
-oNHNs6Tq2yKaFC3waxUsviOO+/lA6htJTM0nBsRNCaCEppoQG6ypvgfT01N7OjUg
-8mhg0Xb5aAc6W/bYgWf9EL9AaNS+C3vxig6Cghl7PUMKB1GH33zc7qnAmYULkNcp
-GNfjKQKBgQCI8uACJIeRSI+mk2clhX8Gip3Q9v9N7Xkqyzj3ooM+ZDesEzI/8iEj
-6/KmfeI1Lkti0kno0ZjqHhzZ41VfMWsur7YrYSkK0+BX0ZbM2MPGedG+pkhfJ5J/
-OlIQYsEyg3M1BRkKXgOm1oPvA/4zXGoIZ6xQMRE0BiO3uc0VCo0NNg==
------END RSA PRIVATE KEY-----
-`;
-const spPublicKey = `-----BEGIN PUBLIC KEY-----
-MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAvoJXvGK6c3aWhA5qOqQd
-DozsSe8Ii01wtd3+joZs8X7rg2VxETZM47OUgw4gbH6Ar104aGxdvfp0w5sP8Dj1
-8s3U8S3dpdEzz6cVqfobzt85oHtmd0GqbzDrNlgG27Uk4PgK1AZEUtOJd8+kK0mK
-a84B4RDHS62fm18s3MPN6r4YHqLP5WBuf2qjqXP9Glsro9SpwGF9a7Ufc1smtbDO
-g+zWLmVyXDfVOC1zfEek+TA+3PRQ3XVdXZKhiw49Afjc5MGUD08n3jESlc++XfAM
-gKuW6OUwMuE3Tvq1qqkdVZgYubA6v48ZAywfCl3GQ6qcXdF6nGGb0X3QYSR7GFUo
-nwIDAQAB
------END PUBLIC KEY-----
-`;
 
 interface CreateSamlResponseOptions {
   statusCode?: string;
@@ -310,12 +238,12 @@ beforeEach(async () => {
     AppId: app.id,
     entityId: 'https://example.com/saml/metadata.xml',
     ssoUrl: 'https://example.com/saml/login',
-    idpCertificate,
+    idpCertificate: await readFixture('saml/idp-certificate.pem', 'utf8'),
     icon: '',
     name: '',
-    spCertificate,
-    spPrivateKey,
-    spPublicKey,
+    spCertificate: await readFixture('saml/sp-certificate.pem', 'utf8'),
+    spPrivateKey: await readFixture('saml/sp-private-key.pem', 'utf8'),
+    spPublicKey: await readFixture('saml/sp-public-key.pem', 'utf8'),
   });
 });
 
