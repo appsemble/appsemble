5cc0960650ba6c36567e7be092b131e98f3c976e
Remco Haszing <remcohaszing@gmail.com>
2021-01-14 13:01:13 +0100

    Document createSettings

--- packages/server/src/utils/render.ts
+++ packages/server/src/utils/render.ts
@@ -1,6 +1,13 @@
 import { createHash } from 'crypto';
 
-export function createSettings(settings: unknown): [string, string] {
+/**
+ * Render settings as an HTML script tag.
+ *
+ * @param settings - The settings to render. This must be a JSON serializable object.
+ * @returns A tuple of the digest and the HTML script tag. The digest should be added to the CSP
+ * `script-src`.
+ */
+export function createSettings(settings: unknown): [digest: string, script: string] {
   const script = `window.settings=${JSON.stringify(settings)}`;
   const hash = createHash('sha256').update(script, 'utf8').digest('base64');
   return [`'sha256-${hash}'`, `<script>${script}</script>`];
