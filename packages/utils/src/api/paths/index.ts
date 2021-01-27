7aa2e3c156315842b623a4be4067b993b7ea1cda
Wessel Kuipers <wesselkuipers@gmail.com>
2021-01-27 18:16:45 +0100

    merge translations into one file

--- packages/utils/src/api/paths/index.ts
+++ packages/utils/src/api/paths/index.ts
@@ -1,4 +1,5 @@
 import { paths as action } from './action';
+import { paths as translations } from './appMessages';
 import { paths as appOAuth2Secrets } from './appOAuth2Secrets';
 import { paths as apps } from './apps';
 import { paths as appSamlSecrets } from './appSamlSecrets';
@@ -7,7 +8,6 @@ import { paths as blocks } from './blocks';
 import { paths as emails } from './emails';
 import { paths as health } from './health';
 import { paths as invite } from './invite';
-import { paths as translations } from './messages';
 import { paths as oauth2ClientCredentials } from './oauth2ClientCredentials';
 import { paths as oauth2Login } from './oauth2Login';
 import { paths as oauth2Provider } from './oauth2Provider';
