b7614531568eace2df842bcb907085c1c55cbeb1
Wessel Kuipers <wesselkuipers@gmail.com>
2021-02-24 19:05:26 +0100

    translate data-notifier block

--- blocks/data-notifier/src/index.tsx
+++ blocks/data-notifier/src/index.tsx
@@ -1,4 +1,4 @@
-import { attach, Remapper } from '@appsemble/sdk';
+import { attach, Messages } from '@appsemble/sdk';
 import equal from 'fast-deep-equal';
 // XXX Update mini-jsx to use jsxImportSource
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -10,103 +10,91 @@ type Item = Record<string, unknown>;
 
 type Data = Item | Item[];
 
-attach(
-  ({
-    events,
-    parameters: {
-      buttonLabel = 'Refresh',
-      color = 'dark',
-      id = 'id',
-      newMessage = 'New data is available',
-      updatedMessage = 'Data has been changed',
-    },
-    utils,
-  }) => {
-    let oldData: Data;
-    let pendingData: Data;
-    let message: HTMLElement;
-    let messageText: HTMLElement;
-    let messageBody: HTMLElement;
+attach(({ events, parameters: { color = 'dark', id = 'id' }, utils }) => {
+  let oldData: Data;
+  let pendingData: Data;
+  let message: HTMLElement;
+  let messageText: HTMLElement;
+  let messageBody: HTMLElement;
 
-    const setPending = (newData: Data, msg: Remapper, count: number): void => {
-      pendingData = newData;
-      messageText.textContent = utils.remap(msg, { count });
-      messageBody.classList.remove(styles.hidden, 'py-0');
-      message.classList.add('my-3');
-    };
+  const setPending = (newData: Data, msg: keyof Messages, count: number): void => {
+    pendingData = newData;
+    messageText.textContent = utils.formatMessage(msg, { count });
+    messageBody.classList.remove(styles.hidden, 'py-0');
+    message.classList.add('my-3');
+  };
 
-    const onClick = (): void => {
-      messageBody.classList.add(styles.hidden, 'py-0');
-      message.classList.remove('my-3');
-      oldData = pendingData;
-      events.emit.data(pendingData);
-    };
+  const onClick = (): void => {
+    messageBody.classList.add(styles.hidden, 'py-0');
+    message.classList.remove('my-3');
+    oldData = pendingData;
+    events.emit.data(pendingData);
+  };
 
-    events.on.seed((newData: Data) => {
-      pendingData = newData;
-      onClick();
+  events.on.seed((newData: Data) => {
+    pendingData = newData;
+    onClick();
+  });
+
+  events.on.data((newData: Data) => {
+    // Just emit the data if there is nothing to compara against.
+    if (oldData === undefined) {
+      oldData = newData;
+      events.emit.data(newData);
+      return;
+    }
+
+    // If we are comparing non-array items, just check for equality.
+    if (!Array.isArray(oldData) || !Array.isArray(newData)) {
+      if (!equal(oldData, newData)) {
+        setPending(newData, 'updatedMessage', 1);
+      }
+      return;
+    }
+
+    // Zip old and new items for easy comparison. Use the new data as the source of truth. The id
+    // parameter is used to make them represent the same entity.
+    const zipped = newData.map<[Item, Item]>((newItem) => {
+      const oldItem = (oldData as Item[]).find((o) => o[id] === newItem[id]);
+      return [oldItem, newItem];
     });
 
-    events.on.data((newData: Data) => {
-      // Just emit the data if there is nothing to compara against.
-      if (oldData === undefined) {
-        oldData = newData;
-        events.emit.data(newData);
-        return;
-      }
+    // Count new items where nothing matches the id of an old one.
+    const newCount = zipped.filter(([oldItem]) => !oldItem).length;
+    if (newCount) {
+      setPending(newData, 'newMessage', newCount);
+      return;
+    }
 
-      // If we are comparing non-array items, just check for equality.
-      if (!Array.isArray(oldData) || !Array.isArray(newData)) {
-        if (!equal(oldData, newData)) {
-          setPending(newData, updatedMessage, 1);
-        }
-        return;
-      }
+    // If there are no new items, check is any items have been updated using deep equality..
+    const diffCount = zipped.filter(([oldItem, newItem]) => !equal(oldItem, newItem)).length;
+    if (diffCount) {
+      setPending(newData, 'updatedMessage', diffCount);
+    }
+  });
 
-      // Zip old and new items for easy comparison. Use the new data as the source of truth. The id
-      // parameter is used to make them represent the same entity.
-      const zipped = newData.map<[Item, Item]>((newItem) => {
-        const oldItem = (oldData as Item[]).find((o) => o[id] === newItem[id]);
-        return [oldItem, newItem];
-      });
-
-      // Count new items where nothing matches the id of an old one.
-      const newCount = zipped.filter(([oldItem]) => !oldItem).length;
-      if (newCount) {
-        setPending(newData, newMessage, newCount);
-        return;
-      }
-
-      // If there are no new items, check is any items have been updated using deep equality..
-      const diffCount = zipped.filter(([oldItem, newItem]) => !equal(oldItem, newItem)).length;
-      if (diffCount) {
-        setPending(newData, updatedMessage, diffCount);
-      }
-    });
-
-    return (
+  return (
+    <div
+      className={`message mx-3 is-${color} ${styles.message}`}
+      ref={(node) => {
+        message = node;
+      }}
+    >
       <div
-        className={`message mx-3 is-${color} ${styles.message}`}
+        className={`message-body is-clipped is-flex py-0 ${styles.messageBody} ${styles.hidden}`}
         ref={(node) => {
-          message = node;
+          messageBody = node;
         }}
       >
-        <div
-          className={`message-body is-clipped is-flex py-0 ${styles.messageBody} ${styles.hidden}`}
+        <span
           ref={(node) => {
-            messageBody = node;
+            messageText = node;
           }}
-        >
-          <span
-            ref={(node) => {
-              messageText = node;
-            }}
-          />
-          <button className={`button is-${color}`} onclick={onClick} type="button">
-            {utils.remap(buttonLabel, null)}
-          </button>
-        </div>
+        />
+        <button className={`button is-${color}`} onclick={onClick} type="button">
+          {utils.formatMessage('buttonLabel')}
+        </button>
       </div>
-    );
-  },
-);
+    </div>
+  );
+});
