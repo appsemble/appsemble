d1414e381fb21f5c0710691b9219c8c7b2f289ea
Wessel Kuipers <wesselkuipers@gmail.com>
2021-02-24 18:44:36 +0100

    translate list block

---
+++ blocks/list/changed/removed/labels.md
@@ -0,0 +1,1 @@
+Remove labels `error`, `noData`. These have been replaced by the message translation support.
--- blocks/list/src/index.tsx
+++ blocks/list/src/index.tsx
@@ -5,79 +5,71 @@ import { useCallback, useEffect, useState } from 'preact/hooks';
 import { Item } from '../block';
 import { ListItem } from './components/ListItem';
 
-bootstrap(
-  ({
-    data: blockData,
-    events,
-    parameters: {
-      base,
-      error: errorText = 'An error occurred when fetching the data.',
-      noData = 'There is no data available.',
-    },
-    ready,
-    utils,
-  }) => {
-    const [data, setData] = useState<Item[]>([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(false);
+bootstrap(({ data: blockData, events, parameters: { base }, ready, utils }) => {
+  const [data, setData] = useState<Item[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
 
-    useEffect(() => {
-      if (blockData != null) {
-        const newData = base == null ? blockData : blockData[base];
+  useEffect(() => {
+    if (blockData != null) {
+      const newData = base == null ? blockData : blockData[base];
 
-        if (Array.isArray(newData)) {
-          setData(newData);
-          setLoading(false);
-        }
-      }
-    }, [base, blockData]);
-
-    const loadData = useCallback(
-      (d: any, err: string): void => {
-        if (err) {
-          setError(true);
-        } else {
-          if (base == null) {
-            setData(d);
-          } else {
-            setData(d[base]);
-          }
-          setError(false);
-        }
+      if (Array.isArray(newData)) {
+        setData(newData);
         setLoading(false);
-      },
-      [base],
-    );
-
-    useEffect(() => {
-      events.on.data(loadData);
-      ready();
-    }, [events, loadData, ready, utils]);
-
-    if (loading) {
-      return <Loader />;
+      }
     }
+  }, [base, blockData]);
 
-    if (error) {
-      return (
-        <Message className="mt-4 mr-6 mb-4 ml-5" color="danger">
-          {utils.remap(errorText, {})}
-        </Message>
-      );
-    }
+  const loadData = useCallback(
+    (d: any, err: string): void => {
+      if (err) {
+        setError(true);
+      } else {
+        if (base == null) {
+          setData(d);
+        } else {
+          setData(d[base]);
+        }
+        setError(false);
+      }
+      setLoading(false);
+    },
+    [base],
+  );
 
-    if (!data.length) {
-      return <Message className="mt-4 mr-6 mb-4 ml-5">{utils.remap(noData, {})}</Message>;
-    }
+  useEffect(() => {
+    events.on.data(loadData);
+    ready();
+  }, [events, loadData, ready, utils]);
 
+  if (loading) {
+    return <Loader />;
+  }
+
+  if (error) {
     return (
-      <ul className="py-4 px-5">
-        {data.map((item, index) => (
-          <li key={item.id ?? index}>
-            <ListItem item={item} />
-          </li>
-        ))}
-      </ul>
+      <Message className="mt-4 mr-6 mb-4 ml-5" color="danger">
+        <span>{utils.formatMessage('error')}</span>
+      </Message>
     );
-  },
-);
+  }
+
+  if (!data.length) {
+    return (
+      <Message className="mt-4 mr-6 mb-4 ml-5">
+        <span>{utils.formatMessage('noData')}</span>
+      </Message>
+    );
+  }
+
+  return (
+    <ul className="py-4 px-5">
+      {data.map((item, index) => (
+        <li key={item.id ?? index}>
+          <ListItem item={item} />
+        </li>
+      ))}
+    </ul>
+  );
+});
