70f796593b775603c034f4ef194869a96943bbd2
Remco Haszing <remcohaszing@gmail.com>
2021-03-01 12:11:11 +0100

    Ignore TypeScript packages that aren’t built

--- packages/server/src/middleware/frontend.ts
+++ packages/server/src/middleware/frontend.ts
@@ -6,6 +6,10 @@ export async function frontend(webpackConfigs: Configuration[]): Promise<KoaMidd
   // eslint-disable-next-line node/no-unpublished-import
   const { default: koaWebpack } = await import('koa-webpack');
   const { default: webpack } = await import('webpack');
+  // eslint-disable-next-line max-len
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment, @typescript-eslint/prefer-ts-expect-error
+  // @ts-ignore Because the webpack core config isn’t built when building the server, an error is
+  // expected here at build time, but while type checking.
   const { createAppConfig, createStudioConfig } = await import('@appsemble/webpack-core');
   const configApp = createAppConfig({ mode: 'development' });
   const configStudio = createStudioConfig({ mode: 'development' });
