4692f2664cfe777eb5fdec72a679ac8c72bfde98
Remco Haszing <remcohaszing@gmail.com>
2021-03-04 13:01:28 +0100

    Remove trailing spaces in translations
    Weblate marks this as warnings. Also they can be safely removed where
    they’re used.

--- packages/studio/src/components/GUIEditor/GUIEditorNavBar/messages.ts
+++ packages/studio/src/components/GUIEditor/GUIEditorNavBar/messages.ts
@@ -3,5 +3,5 @@ import { defineMessages } from 'react-intl';
 export const messages = defineMessages({
   recipe: 'Recipe',
   addBlock: 'Add Block',
-  editBlock: 'Edit ',
+  editBlock: 'Edit',
 });
