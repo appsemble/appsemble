aea6d7efaf51b88f953795534103fcd1e422c9b7
Remco Haszing <remcohaszing@gmail.com>
2021-03-15 14:16:10 +0100

    Restore codecov

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,5 @@ module.exports = {
   projects: ['<rootDir>/blocks/*', '<rootDir>/packages/*'],
   collectCoverageFrom: ['**/src/**/*.{ts,tsx}'],
   reporters: ['default', 'jest-junit', 'jest-image-snapshot/src/outdated-snapshot-reporter.js'],
-  coverageReporters: ['lcov', 'text', 'cobertura'],
+  coverageReporters: ['cobertura', 'json', 'lcov', 'text'],
 };
