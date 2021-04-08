aa5736ed1ee3afcfeee353a5b25a386d3e97474d
Remco Haszing <remcohaszing@gmail.com>
2021-04-08 17:23:37 +0200

    Extract, enhance and test findMessageIds
    It’s going to be used by `@appsemble/cli` as well.

---
+++ packages/utils/src/appMessages.ts
@@ -0,0 +1,23 @@
+/**
+ * Recursively find `string.format` remapper message IDs.
+ *
+ * @param obj - THe object to search.
+ * @returns All message IDs found
+ */
+export function findMessageIds(obj: unknown): string[] {
+  if (!obj || typeof obj !== 'object') {
+    return [];
+  }
+  if (Array.isArray(obj)) {
+    return obj.flatMap((item) => findMessageIds(item));
+  }
+  const entries = Object.entries(obj);
+  // Remappers throw if multiple keys are defined, so this means it’s not a remapper.
+  if (entries.length === 1) {
+    const [[key, value]] = entries;
+    if (key === 'string.format' && typeof value?.messageId === 'string') {
+      return [value.messageId];
+    }
+  }
+  return entries.flatMap(([, value]) => findMessageIds(value));
+}
