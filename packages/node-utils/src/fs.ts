49d03350e1cc72acd6b5f3bd9f498058768d51b2
Remco Haszing <remcohaszing@gmail.com>
2021-04-14 11:09:11 +0200

    Add some well tested fs utilities in node-utils
    - `isErrno` checks if a value is a NodeJS IO error in a type-safe
      manner.
    - `readYaml` reads a YAML file.
    - Tests have been added for `opendirSafe`.
    - All of the above have been implemented where applicable.

--- packages/node-utils/src/fs.ts
+++ packages/node-utils/src/fs.ts
@@ -1,9 +1,51 @@
 import { Dirent, promises as fs, Stats } from 'fs';
 import { join } from 'path';
 
+import yaml from 'js-yaml';
 import { Promisable } from 'type-fest';
 
 import { AppsembleError } from '.';
+
+/**
+ * Test if the error is a NodeJS errno exception.
+ *
+ * @param error - The value to check
+ * @param code - If specified, theck if the code matches
+ * @returns Whether or not the error is a NodeJS errno exception.
+ */
+export function isErrno(error: unknown, code?: string): error is NodeJS.ErrnoException {
+  if (!error || typeof error !== 'object') {
+    return false;
+  }
+  const err = error as NodeJS.ErrnoException;
+  if (code) {
+    return err.code === code;
+  }
+  return typeof err.code === 'string';
+}
+
+/**
+ * Read and parse a YAML file.
+ *
+ * @param path - The path to the file to read.
+ * @returns A tuple of the parsed YAML content and the YAML content as a string.
+ */
+export async function readYaml<R>(path: string): Promise<[R, string]> {
+  let content: string;
+  try {
+    content = await fs.readFile(path, 'utf8');
+  } catch {
+    throw new AppsembleError(`Error reading file ${path}`);
+  }
+  try {
+    return [(yaml.safeLoad(content) as unknown) as R, content];
+  } catch (error: unknown) {
+    if (error instanceof yaml.YAMLException) {
+      throw new AppsembleError(`Error parsing ${path}\n${error.message}`);
+    }
+    throw error;
+  }
+}
 
 interface OpenDirSafeOptions {
   allowMissing?: boolean;
@@ -27,7 +69,7 @@ export async function opendirSafe(
   try {
     stats = await fs.stat(directory);
   } catch (err: unknown) {
-    if (options.allowMissing && (err as NodeJS.ErrnoException).code === 'ENOENT') {
+    if (options.allowMissing && isErrno(err, 'ENOENT')) {
       return;
     }
     throw new AppsembleError(`Expected ${directory} to be a directory`);
