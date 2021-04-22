5d03c76b8a35edc32963c605cd1ee2429d1eb115
Remco Haszing <remcohaszing@gmail.com>
2021-04-22 14:47:59 +0200

    Cleanup stopped review environments
    Once a review environment is stopped, is still exists in GitLab. This
    clutters the environment viewer. This change makes sure stopped review
    environments are deleted nightly.

---
+++ packages/scripts/src/lib/gitlab.ts
@@ -0,0 +1,33 @@
+import axios from 'axios';
+
+export interface Environment {
+  id: number;
+  name: string;
+  slug: string;
+  state: 'available' | 'stopped';
+}
+
+export interface AssetLink {
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  link_type?: 'image' | 'other' | 'package';
+  name: string;
+  url: string;
+}
+
+export interface Release {
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  tag_name: string;
+  description: string;
+  assets: { links: AssetLink[] };
+}
+
+const {
+  CI_JOB_TOKEN,
+  CI_PROJECT_ID,
+  CI_API_V4_URL = `https://gitlab.com/api/v4/projects/${CI_PROJECT_ID}`,
+} = process.env;
+
+export const gitlab = axios.create({
+  baseURL: CI_API_V4_URL,
+  headers: { authorization: `Bearer ${CI_JOB_TOKEN}` },
+});
