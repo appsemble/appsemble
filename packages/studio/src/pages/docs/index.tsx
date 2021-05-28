f4bdaf2c26fc49fe6155ae5edd05f04aebfa5795
Remco Haszing <remcohaszing@gmail.com>
2021-05-28 10:50:49 +0200

    Remove base url from Studio
    This makes hash anchors simpler to handle

--- packages/studio/src/pages/docs/index.tsx
+++ packages/studio/src/pages/docs/index.tsx
@@ -14,41 +14,39 @@ const docs = context
     return {
       Component,
       icon,
-      path: key
+      p: key
         .replace(/^\.\//, '')
         .replace(/\.mdx?$/, '')
         .replace(/(^|\/)index$/, '/'),
       title,
     };
   })
-  .sort((a, b) => a.path.localeCompare(b.path));
+  .sort((a, b) => a.p.localeCompare(b.p));
+
+function getUrl(p: string, base: string): string {
+  return p === '/' ? base : `${base}/${p.replace(/\/$/, '')}`;
+}
 
 /**
  * Render the documentation in the root of the Apsemble repository.
  */
 export function DocsRoutes(): ReactElement {
-  const { url } = useRouteMatch();
-
-  function getUrl(path: string): string {
-    return path === '/' ? url : `${url}/${path.replace(/\/$/, '')}`;
-  }
+  const { path, url } = useRouteMatch();
 
   useSideMenu(
     <MenuSection label={<FormattedMessage {...messages.title} />}>
       {docs
-        .filter(({ path }) => path.endsWith('/'))
-        .map(({ icon, path, title }) => {
-          const subRoutes = docs.filter(
-            (subRoute) => subRoute.path !== path && subRoute.path.startsWith(path),
-          );
+        .filter(({ p }) => p.endsWith('/'))
+        .map(({ icon, p, title }) => {
+          const subRoutes = docs.filter((subRoute) => subRoute.p !== p && subRoute.p.startsWith(p));
           return [
-            <MenuItem exact icon={icon} key={`${path}-title`} to={getUrl(path)}>
+            <MenuItem exact icon={icon} key={`${path}-title`} to={getUrl(p, url)}>
               {title}
             </MenuItem>,
             subRoutes.length ? (
               <MenuSection key={`${path}-section`}>
                 {subRoutes.map((subRoute) => (
-                  <MenuItem key={subRoute.path} to={getUrl(subRoute.path)}>
+                  <MenuItem key={subRoute.p} to={getUrl(subRoute.p, url)}>
                     {subRoute.title}
                   </MenuItem>
                 ))}
@@ -61,13 +59,13 @@ export function DocsRoutes(): ReactElement {
 
   return (
     <MetaSwitch title={messages.title}>
-      {docs.map(({ Component, path, title }) => (
-        <Route exact key={path} path={getUrl(path)} strict>
+      {docs.map(({ Component, p, title }) => (
+        <Route exact key={p} path={getUrl(p, path)} strict>
           <Doc component={Component} title={title} />
         </Route>
       ))}
-      {docs.map(({ path }) => (
-        <Redirect exact from={`${getUrl(path)}/`} key={path} to={getUrl(path)} />
+      {docs.map(({ p }) => (
+        <Redirect exact from={`${getUrl(p, path)}/`} key={p} to={getUrl(p, path)} />
       ))}
       <Redirect to={url} />
     </MetaSwitch>
