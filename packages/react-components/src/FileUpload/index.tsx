dc1abd5af3f5588fcbffce04cfefc49da7ffae6f
Remco Haszing <remcohaszing@gmail.com>
2021-06-15 17:27:14 +0200

    Fix organization logo selection

--- packages/react-components/src/FileUpload/index.tsx
+++ packages/react-components/src/FileUpload/index.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, ComponentPropsWithoutRef, forwardRef, ReactNode } from 'react';
+import { ChangeEvent, ComponentPropsWithoutRef, forwardRef, ReactNode, useCallback } from 'react';
 
 import { FormComponent, Icon, Input, SharedFormComponentProps } from '..';
 
@@ -10,7 +10,7 @@ type FileUploadProps = Omit<
     fileButtonLabel?: ReactNode;
     fileLabel?: ReactNode;
     preview?: ReactNode;
-    onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+    onChange: (event: ChangeEvent<HTMLInputElement>, value: File) => void;
     formComponentClassName?: string;
   };
 
@@ -32,28 +32,35 @@ export const FileUpload = forwardRef<HTMLInputElement, FileUploadProps>(
       required = false,
     },
     ref,
-  ) => (
-    <FormComponent className={formComponentClassName} id={id} label={label} required={required}>
-      {preview}
-      <div className="file has-name">
-        <label className={`file-label ${className}`} htmlFor={id}>
-          <input
-            accept={accept}
-            className="file-input"
-            id={id}
-            name={name}
-            onChange={onChange}
-            ref={ref}
-            type="file"
-          />
-          <span className="file-cta">
-            <Icon icon={icon} />
-            {fileButtonLabel && <span className="file-label">{fileButtonLabel}</span>}
-          </span>
-          {fileLabel && <span className="file-name">{fileLabel}</span>}
-        </label>
-      </div>
-      {help && <p className="help">{help}</p>}
-    </FormComponent>
-  ),
+  ) => {
+    const handleChange = useCallback(
+      (event: ChangeEvent<HTMLInputElement>) => onChange(event, event.currentTarget.files[0]),
+      [onChange],
+    );
+
+    return (
+      <FormComponent className={formComponentClassName} id={id} label={label} required={required}>
+        {preview}
+        <div className="file has-name">
+          <label className={`file-label ${className}`} htmlFor={id}>
+            <input
+              accept={accept}
+              className="file-input"
+              id={id}
+              name={name}
+              onChange={handleChange}
+              ref={ref}
+              type="file"
+            />
+            <span className="file-cta">
+              <Icon icon={icon} />
+              {fileButtonLabel && <span className="file-label">{fileButtonLabel}</span>}
+            </span>
+            {fileLabel && <span className="file-name">{fileLabel}</span>}
+          </label>
+        </div>
+        {help && <p className="help">{help}</p>}
+      </FormComponent>
+    );
+  },
 );
