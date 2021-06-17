510b59f9e68e27164dcfcd45d912a7715714386f
Remco Haszing <remcohaszing@gmail.com>
2021-06-17 11:56:30 +0200

    Fix schema for sub pages

--- packages/utils/src/api/components/schemas/SubPage.ts
+++ packages/utils/src/api/components/schemas/SubPage.ts
@@ -3,8 +3,14 @@ import { OpenAPIV3 } from 'openapi-types';
 export const SubPage: OpenAPIV3.NonArraySchemaObject = {
   type: 'object',
   description: 'This describes what a sub page will look like in the app.',
-  required: ['name', 'blocks'],
+  required: ['blocks'],
   properties: {
+    name: {
+      type: 'string',
+      description: 'The name of the sub page.',
+      minLength: 1,
+      maxLength: 50,
+    },
     blocks: {
       type: 'array',
       minItems: 1,
