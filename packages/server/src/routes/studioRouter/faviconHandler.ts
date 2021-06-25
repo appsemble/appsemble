4bdb09d0654cd69e339c85a4fae4b1ea6dc3bd62
Remco Haszing <remcohaszing@gmail.com>
2021-06-25 15:46:46 +0200

    Remove to-ico dependency
    This is an outdated and unmaintained dependency with a big dependency
    tree which was only used to generate a `favicon.ico` asset for Studio.
    Instead, it has been replaced with a static file.

--- packages/server/src/routes/studioRouter/faviconHandler.ts
+++ packages/server/src/routes/studioRouter/faviconHandler.ts
@@ -1,17 +1,7 @@
-import sharp from 'sharp';
-import toIco from 'to-ico';
-
 import { KoaContext } from '../../types';
 import { readAsset } from '../../utils/readAsset';
 
-const sizes = [16, 32, 48, 64, 128, 256];
-
 export async function faviconHandler(ctx: KoaContext): Promise<void> {
-  const icon = await readAsset('appsemble.svg');
-  // 2400 is the maximum density, meaning the SVG icon will still look good.
-  const resize = (size: number): Promise<Buffer> =>
-    sharp(icon.slice(), { density: 2400 }).resize(size).png().toBuffer();
-  const pngs = await Promise.all(sizes.map((size) => resize(size)));
-  ctx.body = await toIco(pngs, { resize: false });
+  ctx.body = await readAsset('favicon.ico');
   ctx.type = 'image/x-icon';
 }
