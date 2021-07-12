28a5766b9eb5828756e98006e3c985cd00bd2990
Wessel Kuipers <wesselkuipers@gmail.com>
2021-07-12 11:06:32 +0200

    update translations

--- packages/app/src/components/Page/ShareDialog/messages.ts
+++ packages/app/src/components/Page/ShareDialog/messages.ts
@@ -1,10 +1,7 @@
 import { defineMessages } from 'react-intl';
 
 export const messages = defineMessages({
-  permissionError:
-    'Due to misconfigured permissions, you’re not able to view the app. If you think this is a mistake, please leave a message in the <link>Appsemble store page</link>.',
-  logout: 'Logout',
-  share: 'Share',
   email: 'Email',
+  share: 'Share',
   shareOn: 'Share on {name}',
 });
