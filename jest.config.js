a7a4f12a58b06ceedae00730ad258710466c5f5e
Wessel Kuipers <wesselkuipers@gmail.com>
2021-07-19 20:50:10 +0200

    fix ci

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   projects: ['<rootDir>/blocks/*', '<rootDir>/packages/*'],
-  modulePathIgnorePatterns: ['<rootDir>/packages/e2e'],
   collectCoverageFrom: ['**/src/**/*.{ts,tsx}'],
   reporters: ['default', 'jest-junit', 'jest-image-snapshot/src/outdated-snapshot-reporter.js'],
   coverageReporters: ['cobertura', 'json', 'lcov', 'text'],
