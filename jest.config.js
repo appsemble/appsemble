2ca115ffcdedd51b6025b60b962b31a28452bb92
Wessel Kuipers <wesselkuipers@gmail.com>
2021-07-19 16:46:22 +0200

    ignore e2e in jest config

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  projects: ['<rootDir>/blocks/*', '<rootDir>/packages/*', '!<rootDir/packages/e2e>'],
+  projects: ['<rootDir>/blocks/*', '<rootDir>/packages/*'],
+  modulePathIgnorePatterns: ['<rootDir>/packages/e2e'],
   collectCoverageFrom: ['**/src/**/*.{ts,tsx}'],
   reporters: ['default', 'jest-junit', 'jest-image-snapshot/src/outdated-snapshot-reporter.js'],
   coverageReporters: ['cobertura', 'json', 'lcov', 'text'],
