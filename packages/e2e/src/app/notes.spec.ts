c1ede8c2c230c015ea14d2ed459f9467288c5b1b
Wessel Kuipers <wesselkuipers@gmail.com>
2021-07-21 18:41:08 +0200

    add notes tests

---
+++ packages/e2e/src/app/notes.spec.ts
@@ -0,0 +1,49 @@
+describe('Notes app', () => {
+  const { host, protocol } = new URL(Cypress.config().baseUrl);
+  const url = `${protocol}//notes.appsemble.${host}`;
+
+  function login(): void {
+    cy.get('.appsemble-loader').should('not.exist');
+    cy.get('.appsemble-login > button').click();
+    cy.get('#email').type(Cypress.env('BOT_ACCOUNT_EMAIL'));
+    cy.get('#password').type(Cypress.env('BOT_ACCOUNT_PASSWORD'));
+    cy.get('button[type="submit"]').click();
+    cy.get('body').then((body) => {
+      if (body.find('.has-text-centered > .button.is-primary').length) {
+        cy.get('.button.is-primary').click();
+      }
+    });
+    cy.get('[data-block]').should('exist');
+    cy.get('.appsemble-loader', { includeShadowDom: true }).should('not.exist');
+  }
+
+  it('should match a screenshot in desktop mode', () => {
+    cy.intercept({ url: '/api/apps/*/resources/note', method: 'GET' }, { body: [] });
+    cy.visit(url);
+    login();
+    cy.matchImageSnapshot();
+  });
+
+  it('should match a screenshot in mobile mode', () => {
+    cy.intercept({ url: '/api/apps/*/resources/note', method: 'GET' }, { body: [] });
+    cy.viewport('iphone-x');
+    cy.visit(url);
+    login();
+    cy.matchImageSnapshot();
+  });
+
+  it('should create a new note and view it', () => {
+    const date = Date.now();
+
+    cy.visit(url);
+    login();
+    cy.get('.button.is-rounded', { includeShadowDom: true }).click();
+    cy.get('#title', { includeShadowDom: true }).type(`Title ${date}`);
+    cy.get('#body', { includeShadowDom: true }).type(`Body ${date}`, { force: true });
+    cy.get('button[type="submit"]', { includeShadowDom: true }).click();
+    cy.contains(`Title ${date}`, { includeShadowDom: true }).as('hr').should('exist');
+    cy.get('@hr').click();
+    cy.get('h6.title', { includeShadowDom: true }).should('contain.text', `Title ${date}`);
+    cy.get('div.content', { includeShadowDom: true }).should('contain.text', `Body ${date}`);
+  });
+});
