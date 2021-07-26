8297676b22d142582d34ae79843a3dedc6088eb5
Wessel Kuipers <wesselkuipers@gmail.com>
2021-07-26 14:49:02 +0200

    attempt snapshot tests at 720p instead

--- packages/e2e/cypress/support/index.ts
+++ packages/e2e/cypress/support/index.ts
@@ -1,3 +1,8 @@
 import { addMatchImageSnapshotCommand } from 'cypress-image-snapshot/command';
 
-addMatchImageSnapshotCommand({ capture: 'viewport' });
+addMatchImageSnapshotCommand({
+  capture: 'viewport',
+  failureThreshold: 0,
+  failureThresholdType: 'percent',
+  customDiffConfig: { threshold: 0 },
+});
