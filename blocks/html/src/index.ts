4a334ec391d087e2f098e504976ccfc2a1b8a6a6
Wessel Kuipers <wesselkuipers@gmail.com>
2021-08-11 14:00:52 +0200

    resolve feedback

--- blocks/html/src/index.ts
+++ blocks/html/src/index.ts
@@ -1,32 +1,57 @@
 import { bootstrap } from '@appsemble/sdk';
+import { has } from '@appsemble/utils';
 
-bootstrap(({ actions, data, parameters: { content, placeholders }, utils: { asset, remap } }) => {
-  const parser = new DOMParser();
-  const node = parser.parseFromString(content, 'text/html');
+const parser = new DOMParser();
 
-  const contentNodes = node.querySelectorAll('[data-content]');
-  const assetNodes = node.querySelectorAll(
-    'img[data-asset], source[data-asset], video[data-asset], audio[data-asset]',
-  );
-  const clickNodes = node.querySelectorAll('a[data-click], input[data-click], button[data-click]');
+bootstrap(
+  ({
+    actions,
+    data,
+    parameters: { content, placeholders },
+    shadowRoot,
+    utils: { asset, remap },
+  }) => {
+    const { body, head } = parser.parseFromString(content, 'text/html');
 
-  for (const contentNode of contentNodes) {
-    const placeholderName = contentNode.getAttribute('data-content');
-    contentNode.textContent = remap(placeholders[placeholderName], data);
-  }
+    const contentNodes = body.querySelectorAll<HTMLElement>('[data-content]');
+    const assetNodes = body.querySelectorAll<HTMLElement>(
+      '[data-asset], source[data-asset], video[data-asset], audio[data-asset]',
+    );
+    const clickNodes = body.querySelectorAll<HTMLElement>('[data-click]');
 
-  for (const assetNode of assetNodes) {
-    const assetId = assetNode.getAttribute('data-asset');
-    assetNode.setAttribute('src', asset(assetId));
-  }
+    for (const contentNode of contentNodes) {
+      const placeholderName = contentNode.dataset.asset;
+      if (has(placeholders, placeholderName)) {
+        contentNode.textContent = remap(placeholders[placeholderName], data);
+      }
+    }
 
-  for (const clickNode of clickNodes) {
-    const action = actions[clickNode.getAttribute('data-click')];
-    (clickNode as HTMLButtonElement).onclick = () => action(data);
-  }
+    for (const assetNode of assetNodes) {
+      const assetId = assetNode.dataset.asset;
+      assetNode.setAttribute('src', asset(assetId));
+    }
 
-  const result = document.createElement('div');
-  // Returns <html> -> <body> -> children
-  result.append(...node.firstElementChild.lastElementChild.children);
-  return result;
-});
+    for (const clickNode of clickNodes) {
+      const { click } = clickNode.dataset;
+      if (!has(actions, click)) {
+        continue;
+      }
+
+      const action = actions[click];
+      if (clickNode.tagName === 'A' && action.type === 'link') {
+        (clickNode as HTMLAnchorElement).href = action.href(data);
+      }
+
+      clickNode.addEventListener(
+        'click',
+        (event) => {
+          event.preventDefault();
+          action(data);
+        },
+        false,
+      );
+    }
+
+    shadowRoot.append(...head.getElementsByTagName('style'), ...body.children);
+  },
+);
