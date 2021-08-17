03a47b93ca0c0a93e3b3e6aea3a31604edd39fa1
Remco Haszing <remcohaszing@gmail.com>
2021-08-17 14:44:23 +0200

    Replace theme schema default with property defaults
    The way this is rendered looks better.

--- packages/utils/src/api/components/schemas/Theme.ts
+++ packages/utils/src/api/components/schemas/Theme.ts
@@ -6,12 +6,12 @@ export const Theme: OpenAPIV3.NonArraySchemaObject = {
   type: 'object',
   description: 'A generic theme definition.',
   additionalProperties: false,
-  default: baseTheme,
   minProperties: 1,
   properties: {
     themeColor: {
       type: 'string',
       pattern: hexColor.source,
+      default: baseTheme.themeColor,
       description: `The generic theme color of the app.
 
 This is used for example in the URL bar on Android.
@@ -20,6 +20,7 @@ This is used for example in the URL bar on Android.
     splashColor: {
       type: 'string',
       pattern: hexColor.source,
+      default: baseTheme.splashColor,
       description: `The background color of the PWA splash screen.
 
 This defaults to the theme color.
@@ -28,6 +29,7 @@ This defaults to the theme color.
     primaryColor: {
       type: 'string',
       pattern: hexColor.source,
+      default: baseTheme.primaryColor,
       description: `The primary color used within the app.
 
 This is used in various elements like the navbar.
@@ -36,31 +38,37 @@ This is used in various elements like the navbar.
     linkColor: {
       type: 'string',
       pattern: hexColor.source,
+      default: baseTheme.linkColor,
       description: 'The color used for links.',
     },
     infoColor: {
       type: 'string',
       pattern: hexColor.source,
+      default: baseTheme.infoColor,
       description: 'The color used for informational messages.',
     },
     successColor: {
       type: 'string',
       pattern: hexColor.source,
+      default: baseTheme.successColor,
       description: 'The color used for successful actions.',
     },
     warningColor: {
       type: 'string',
       pattern: hexColor.source,
+      default: baseTheme.warningColor,
       description: 'The color used for warning messages.',
     },
     dangerColor: {
       type: 'string',
       pattern: hexColor.source,
+      default: baseTheme.dangerColor,
       description: 'The color used for error messages.',
     },
     tileLayer: {
       type: 'string',
       format: 'uri',
+      default: baseTheme.tileLayer,
       description: 'The tileLayer to use for Leaflet maps.',
     },
   },
