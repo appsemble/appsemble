59473e7a63d484a4478792875cd4f59b35ce48fa
Remco Haszing <remcohaszing@gmail.com>
2021-10-13 11:50:27 +0200

    Use official mdx types

--- packages/studio/src/components/MDX/MDXWrapper/index.tsx
+++ packages/studio/src/components/MDX/MDXWrapper/index.tsx
@@ -1,16 +1,15 @@
 import { defaultLocale } from '@appsemble/utils';
+import { MDXProps } from 'mdx/types';
 import { ReactElement } from 'react';
 
 import styles from './index.module.css';
-
-import { MDXWrapperProps } from '*.md';
 
 /**
  * A wrapper for MDX content.
  *
  * This renders a `<main />` element.
  */
-export function MDXWrapper({ children, main = true }: MDXWrapperProps): ReactElement {
+export function MDXWrapper({ children, main = true }: MDXProps): ReactElement {
   return main ? (
     <main className={`content pl-6 ${styles.root}`} lang={defaultLocale}>
       {children}
