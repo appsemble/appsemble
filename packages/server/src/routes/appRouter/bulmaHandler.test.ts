73d5650f5ce6b12d1bcdf8cbfe7cabafe6e9675e
Wessel Kuipers <wesselkuipers@gmail.com>
2021-11-12 15:16:15 +0100

    Slightly expand bulma handler test

--- packages/server/src/routes/appRouter/bulmaHandler.test.ts
+++ packages/server/src/routes/appRouter/bulmaHandler.test.ts
@@ -3,6 +3,7 @@ import { request, setTestApp } from 'axios-test-instance';
 import bulma from 'bulma/package.json';
 import Koa from 'koa';
 import { omit } from 'lodash';
+import sass from 'sass';
 
 import { appRouter } from '.';
 import { boomMiddleware } from '../../middleware/boom';
@@ -29,8 +30,14 @@ afterEach(truncate);
 
 afterAll(closeTestSchema);
 
-it('should cache the Bulma styles', async () => {
+it('should generate and save cache the Bulma styles', async () => {
+  const spy = jest.spyOn(sass, 'renderSync');
   const { data } = await request.get<string>(`/bulma/${bulma.version}/bulma.min.css`, {
+    params: { primaryColor: '#ffffff' },
+  });
+
+  // Second request to check if sass is only called once.
+  await request.get<string>(`/bulma/${bulma.version}/bulma.min.css`, {
     params: { primaryColor: '#ffffff' },
   });
 
@@ -38,6 +45,7 @@ it('should cache the Bulma styles', async () => {
   const cachedTheme = await Theme.findOne({ where: { primaryColor: '#ffffff' } });
 
   expect(count).toBe(1);
+  expect(spy).toHaveBeenCalledTimes(1);
   expect(cachedTheme).toMatchObject({
     ...omit(baseTheme, ['font', 'tileLayer']),
     id: expect.any(Number),
