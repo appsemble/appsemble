85c7eec0936e3a0cc2b785e5ae84f8a1c7da2636
Wessel Kuipers <wesselkuipers@gmail.com>
2021-11-12 16:36:30 +0100

    Set AllowNull

--- packages/server/src/models/Theme.ts
+++ packages/server/src/models/Theme.ts
@@ -3,36 +3,47 @@ import { AllowNull, Column, CreatedAt, DataType, Model, Table } from 'sequelize-
 
 @Table({ tableName: 'Theme', paranoid: false, updatedAt: false })
 export class Theme extends Model implements Omit<ThemeType, 'font' | 'tileLayer'> {
+  @AllowNull(false)
   @Column
   bulmaVersion: string;
 
+  @AllowNull(false)
   @Column
   primaryColor: string;
 
+  @AllowNull(false)
   @Column
   linkColor: string;
 
+  @AllowNull(false)
   @Column
   successColor: string;
 
+  @AllowNull(false)
   @Column
   infoColor: string;
 
+  @AllowNull(false)
   @Column
   warningColor: string;
 
+  @AllowNull(false)
   @Column
   dangerColor: string;
 
+  @AllowNull(false)
   @Column
   themeColor: string;
 
+  @AllowNull(false)
   @Column
   splashColor: string;
 
+  @AllowNull(false)
   @Column
   fontFamily: string;
 
+  @AllowNull(false)
   @Column
   fontSource: string;
 
@@ -40,6 +51,7 @@ export class Theme extends Model implements Omit<ThemeType, 'font' | 'tileLayer'
   @Column(DataType.TEXT)
   css: string;
 
+  @AllowNull(false)
   @CreatedAt
   created: Date;
 }
