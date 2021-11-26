abdd7fce2bae5e71a704b7948475f29d879714bc
Vincent <a>
2021-11-26 12:52:51 +0100

    Camera capture test

--- blocks/qr-scan/block.ts
+++ blocks/qr-scan/block.ts
@@ -1,19 +1,3 @@
-import { IconName } from '@appsemble/sdk';
-
 export {};
 
-declare module '@appsemble/sdk' {
-  interface Actions {
-    /**
-     * What happens if the button is clicked.
-     */
-    onClick: {};
-  }
-
-  interface Parameters {
-    /**
-     * A [Font Awesome icon](https://fontawesome.com/icons?m=free) name to render on the button.
-     */
-    icon: IconName;
-  }
-}
+declare module '@appsemble/sdk' {}
--- blocks/qr-scan/src/index.ts
+++ blocks/qr-scan/src/index.ts
@@ -2,27 +2,15 @@ import './index.css';
 
 import { bootstrap } from '@appsemble/sdk';
 
-bootstrap(({ actions, data, parameters: { icon }, utils: { fa } }) => {
-  let node;
-  const iconNode = document.createElement('i');
-  iconNode.className = fa(icon);
-  if (actions.onClick.type === 'link') {
-    node = document.createElement('a');
-    node.href = actions.onClick.href(data);
-  } else {
-    node = document.createElement('button');
-    node.type = 'button';
-  }
-  node.classList.add('button', 'is-paddingless', 'is-primary', 'is-rounded');
-  node.addEventListener(
-    'click',
-    (event) => {
-      event.preventDefault();
-      actions.onClick(data);
-    },
-    true,
-  );
+bootstrap(() => {
+  const div = document.createElement('div');
+  const video = document.createElement('video');
 
-  node.append(iconNode);
-  return node;
+  navigator.mediaDevices.getUserMedia().then((stream) => {
+    video.srcObject = stream;
+  });
+
+  div.append(video);
+
+  return div;
 });
