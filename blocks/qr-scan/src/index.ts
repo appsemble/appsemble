2df8c0283dbd7cf45cc582923e1ca85d03ae70c1
Vincent <a>
2021-11-29 15:52:24 +0100

    QrScanner implemented with VideoStream

--- blocks/qr-scan/src/index.ts
+++ blocks/qr-scan/src/index.ts
@@ -1,18 +1,57 @@
-import './index.css';
-
 import { bootstrap } from '@appsemble/sdk';
+import './index.css';
+import jsQR from 'jsqr';
 
 bootstrap(() => {
-  const video = document.createElement('video');
   const div = document.createElement('div');
 
-  video.autoplay = true;
-  window.onload = function capture() {
-    navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
-      video.srcObject = stream;
-    });
-  };
+  const video = document.createElement('video');
+  const canvasElement = document.createElement('canvas');
+  const canvas = canvasElement.getContext('2d');
+  const outputData = document.createElement('h1');
 
-  div.append(video);
+  function drawLine(begin: { x: any; y: any }, end: { x: any; y: any }, color: string): void {
+    canvas.beginPath();
+    canvas.moveTo(begin.x, begin.y);
+    canvas.lineTo(end.x, end.y);
+    canvas.lineWidth = 4;
+    canvas.strokeStyle = color;
+    canvas.stroke();
+  }
+
+  function tick(): void {
+    if (video.readyState === video.HAVE_ENOUGH_DATA) {
+      canvasElement.hidden = false;
+
+      canvasElement.height = video.videoHeight;
+      canvasElement.width = video.videoWidth;
+      canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
+      const imageData = canvas.getImageData(0, 0, canvasElement.width, canvasElement.height);
+      const code = jsQR(imageData.data, imageData.width, imageData.height, {
+        inversionAttempts: 'dontInvert',
+      });
+      if (code) {
+        drawLine(code.location.topLeftCorner, code.location.topRightCorner, '#FF3B58');
+        drawLine(code.location.topRightCorner, code.location.bottomRightCorner, '#FF3B58');
+        drawLine(code.location.bottomRightCorner, code.location.bottomLeftCorner, '#FF3B58');
+        drawLine(code.location.bottomLeftCorner, code.location.topLeftCorner, '#FF3B58');
+
+        outputData.textContent = code.data;
+      }
+    }
+    requestAnimationFrame(tick);
+  }
+
+  // Use facingMode: environment to attemt to get the front camera on phones
+  navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment' } }).then((stream) => {
+    video.srcObject = stream;
+    // Required to tell iOS safari we don't want fullscreen
+    // Commented -> video.setAttribute("playsinline", true);
+    video.play();
+    requestAnimationFrame(tick);
+  });
+
+  div.append(outputData);
+  div.append(canvasElement);
   return div;
 });
