7f214a4bc382f55d14f5f76b359467fbf14185fe
Vincent <a>
2021-12-06 21:49:25 +0100

    QR-Scanner block

--- blocks/qr-scan/block.ts
+++ blocks/qr-scan/block.ts
@@ -1,3 +1,30 @@
 export {};
 
-declare module '@appsemble/sdk' {}
+declare module '@appsemble/sdk' {
+  interface Parameters {
+    /**
+     * Draws rectangle on the QR found.
+     * By drawing on the canvas in which the video-stream is loaded.
+     */
+    drawQr?: boolean;
+
+    /**
+     * Video Element Height
+     */
+    videoHeight?: number;
+
+    /**
+     * Video Element Width
+     */
+    videoWidth?: number;
+  }
+
+  interface EventEmitters {
+    /**
+     * Event that gets emitted once the qr scanner has identified and resolved a QR code.
+     *
+     * foundQr event only holds the string of the QR.
+     */
+    foundQr: never;
+  }
+}
--- blocks/qr-scan/src/index.ts
+++ blocks/qr-scan/src/index.ts
@@ -2,13 +2,12 @@ import { bootstrap } from '@appsemble/sdk';
 import './index.css';
 import jsQR from 'jsqr';
 
-bootstrap(() => {
+bootstrap(({ events, parameters: { drawQr = false, videoHeight = 0, videoWidth = 0 } }) => {
   const div = document.createElement('div');
 
   const video = document.createElement('video');
   const canvasElement = document.createElement('canvas');
   const canvas = canvasElement.getContext('2d');
-  const outputData = document.createElement('h1');
 
   function drawLine(begin: { x: any; y: any }, end: { x: any; y: any }, color: string): void {
     canvas.beginPath();
@@ -23,26 +22,29 @@ bootstrap(() => {
     if (video.readyState === video.HAVE_ENOUGH_DATA) {
       canvasElement.hidden = false;
 
-      canvasElement.height = video.videoHeight;
-      canvasElement.width = video.videoWidth;
+      canvasElement.height = videoHeight === 0 ? video.videoHeight : videoHeight;
+      canvasElement.width = videoWidth === 0 ? video.videoWidth : videoWidth;
       canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
       const imageData = canvas.getImageData(0, 0, canvasElement.width, canvasElement.height);
       const code = jsQR(imageData.data, imageData.width, imageData.height, {
         inversionAttempts: 'dontInvert',
       });
       if (code) {
-        drawLine(code.location.topLeftCorner, code.location.topRightCorner, '#FF3B58');
-        drawLine(code.location.topRightCorner, code.location.bottomRightCorner, '#FF3B58');
-        drawLine(code.location.bottomRightCorner, code.location.bottomLeftCorner, '#FF3B58');
-        drawLine(code.location.bottomLeftCorner, code.location.topLeftCorner, '#FF3B58');
+        if (drawQr) {
+          drawLine(code.location.topLeftCorner, code.location.topRightCorner, '#FF3B58');
+          drawLine(code.location.topRightCorner, code.location.bottomRightCorner, '#FF3B58');
+          drawLine(code.location.bottomRightCorner, code.location.bottomLeftCorner, '#FF3B58');
+          drawLine(code.location.bottomLeftCorner, code.location.topLeftCorner, '#FF3B58');
+        }
 
-        outputData.textContent = code.data;
+        // Emit event with QR URL (code.data)
+        events.emit.foundQr(code.data);
       }
     }
     requestAnimationFrame(tick);
   }
 
-  // Use facingMode: environment to attemt to get the front camera on phones
+  // Use facingMode: environment to attempt to get the front camera on phones
   navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment' } }).then((stream) => {
     video.srcObject = stream;
     // Required to tell iOS safari we don't want fullscreen
@@ -51,7 +53,6 @@ bootstrap(() => {
     requestAnimationFrame(tick);
   });
 
-  div.append(outputData);
   div.append(canvasElement);
   return div;
 });
