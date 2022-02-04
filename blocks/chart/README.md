3e7ef56f54e9fcaeef8ea7067865e9672f01e343
Remco Haszing <remcohaszing@gmail.com>
2022-02-04 15:10:32 +0100

    Add chart block

---
+++ blocks/chart/README.md
@@ -0,0 +1,10 @@
+This block can render bar and line charts. The chart always starts empty. Data can be added using
+events.
+
+To add data, an event may emit either a single data object, or an array of data objects. The data is
+expected to adhere to a certain interface.
+
+- `type`: This is should be a string containing either `bar` or `line`. If this is unspecified, the
+  `type` defined by the block parameters is used.
+- `label`: This should be a textual description of the dataset.
+- `data`: The data should be an array of numbers. If a value is null,it’s skipped from the chart.
---
+++ blocks/chart/block.ts
@@ -0,0 +1,46 @@
+import { Remapper } from '@appsemble/sdk';
+
+declare module '@appsemble/sdk' {
+  interface EventListeners {
+    /**
+     * Add new data to the chart.
+     *
+     * The the block description for more information on the supported data format.
+     */
+    add: never;
+
+    /**
+     * Replace all current data with the new data.
+     *
+     * The the block description for more information on the supported data format.
+     */
+    replace: never;
+  }
+
+  interface Parameters {
+    /**
+     * The chart type to render by default.
+     *
+     * @default 'line'
+     */
+    type: 'bar' | 'line';
+
+    /**
+     * The labels to display at the bottom of the chart.
+     *
+     * @minItems 1
+     */
+    labels: Remapper[];
+
+    /**
+     * The background colors to use for each dataset by default.
+     *
+     * If the length of this array exceeds the number of labels, the pattern will be repeated.
+     *
+     * By default the primary color of the theme will be used.
+     *
+     * @minItems 1
+     */
+    backgroundColors?: Remapper[];
+  }
+}
