77562e07f791feef7a5d94e47cd6bac70444600f
Wessel Kuipers <wesselkuipers@gmail.com>
2022-03-28 12:01:06 +0200

    Fix various migration issues

--- packages/server/src/migrations/0.20.3.ts
+++ packages/server/src/migrations/0.20.3.ts
@@ -28,20 +28,21 @@ function processAppsAndMessages(
 
   for (const app of apps) {
     const appMessages = messages.filter((m) => m.AppId === app.id);
-    const replacementMap = new Map(
-      app.definition.pages.map((page, index) =>
+    const replacementMap = app.definition.pages
+      .map((page, index) =>
         downMigration
           ? [normalize(page.name), String(index)]
           : [String(index), normalize(page.name)],
-      ),
-    );
+      )
+      // Reversed in order to prevent pages.10 being replaced by pages.1
+      .reverse();
+
     logger.info(`Processing app ${app.id}`);
-    // To prevent index 0 replacing index 10’s name, iterate through the map in reverse.
-    for (const [oldKey, newKey] of [...replacementMap.entries()].reverse()) {
+    for (const [oldKey, newKey] of replacementMap) {
       for (const appMessage of appMessages) {
-        for (const name in appMessage.messages.app) {
-          if (name.startsWith(`pages.${oldKey}`)) {
-            appMessage.messages.app[`pages.${newKey}`] = appMessage.messages.app[name];
+        for (const name of Object.keys(appMessage.messages.app)) {
+          if (name === `pages.${oldKey}` || name.startsWith(`pages.${oldKey}.`)) {
+            appMessage.messages.app[name.replace(oldKey, newKey)] = appMessage.messages.app[name];
             delete appMessage.messages.app[name];
           }
         }
@@ -74,28 +75,38 @@ export async function up(db: Sequelize): Promise<void> {
     },
   );
 
-  const appsInput = await db.query<AppQuery>(
-    'SELECT id, "coreStyle", "sharedStyle", definition FROM "App" WHERE "coreStyle" ~ \'pages\\.\\d+\' OR "sharedStyle" ~ \'pages\\.\\d+\' OR id IN (?)',
-    { type: QueryTypes.SELECT, replacements: messagesInput.map((m) => m.AppId) },
-  );
+  const appsInput = messagesInput.length
+    ? await db.query<AppQuery>(
+        'SELECT id, "coreStyle", "sharedStyle", definition FROM "App" WHERE "coreStyle" ~ \'pages\\.\\d+\' OR "sharedStyle" ~ \'pages\\.\\d+\' OR id IN (?)',
+        { type: QueryTypes.SELECT, replacements: [messagesInput.map((m) => m.AppId)] },
+      )
+    : await db.query<AppQuery>(
+        'SELECT id, "coreStyle", "sharedStyle", definition FROM "App" WHERE "coreStyle" ~ \'pages\\.\\d+\' OR "sharedStyle" ~ \'pages\\.\\d+\'',
+        { type: QueryTypes.SELECT },
+      );
 
   const { apps, messages } = processAppsAndMessages(appsInput, messagesInput);
 
   logger.info('Applying all changes to database.');
+
   await Promise.all([
-    ...apps.map((app) =>
-      db.query('UPDATE "App" SET "coreStyle" = ? AND "sharedStyle" = ? WHERE id = ?', {
-        type: QueryTypes.UPDATE,
-        replacements: [app.coreStyle, app.sharedStyle, app.id],
-      }),
-    ),
+    ...apps
+      .filter((app) => app.coreStyle && app.sharedStyle)
+      .map((app) =>
+        db.query('UPDATE "App" SET "coreStyle" = ?, "sharedStyle" = ? WHERE id = ?', {
+          type: QueryTypes.UPDATE,
+          replacements: [app.coreStyle, app.sharedStyle, app.id],
+        }),
+      ),
     ...messages.map((m) =>
       db.query('UPDATE "AppMessages" SET messages = ? WHERE "AppId" = ? AND language = ?', {
         type: QueryTypes.UPDATE,
-        replacements: [m.messages, m.AppId, m.language],
+        replacements: [JSON.stringify(m.messages), m.AppId, m.language],
       }),
     ),
   ]);
+
+  throw new Error('don’t update meta please');
 }
 
 /**
@@ -112,21 +123,29 @@ export async function down(db: Sequelize): Promise<void> {
     },
   );
 
-  const appsInput = await db.query<AppQuery>(
-    `SELECT id, "coreStyle", "sharedStyle", definition
-    FROM "App"
-    WHERE "coreStyle" ~ 'pages\\.${partialNormalized.source}'
-    OR "sharedStyle" ~ 'pages\\.${partialNormalized.source}'
-    OR id IN (?)`,
-    { type: QueryTypes.SELECT, replacements: messagesInput.map((m) => m.AppId) },
-  );
+  const appsInput = messagesInput.length
+    ? await db.query<AppQuery>(
+        `SELECT id, "coreStyle", "sharedStyle", definition
+         FROM "App"
+         WHERE "coreStyle" ~ 'pages\\.${partialNormalized.source}'
+         OR "sharedStyle" ~ 'pages\\.${partialNormalized.source}'
+         OR id IN (?)`,
+        { type: QueryTypes.SELECT, replacements: [messagesInput.map((m) => m.AppId)] },
+      )
+    : await db.query<AppQuery>(
+        `SELECT id, "coreStyle", "sharedStyle", definition
+         FROM "App"
+         WHERE "coreStyle" ~ 'pages\\.${partialNormalized.source}'
+         OR "sharedStyle" ~ 'pages\\.${partialNormalized.source}'`,
+        { type: QueryTypes.SELECT },
+      );
 
   const { apps, messages } = processAppsAndMessages(appsInput, messagesInput, true);
 
   logger.info('Applying all changes to database.');
   await Promise.all([
     ...apps.map((app) =>
-      db.query('UPDATE "App" SET "coreStyle" = ? AND "sharedStyle" = ? WHERE id = ?', {
+      db.query('UPDATE "App" SET "coreStyle" = ?, "sharedStyle" = ? WHERE id = ?', {
         type: QueryTypes.UPDATE,
         replacements: [app.coreStyle, app.sharedStyle, app.id],
       }),
@@ -134,7 +153,7 @@ export async function down(db: Sequelize): Promise<void> {
     ...messages.map((m) =>
       db.query('UPDATE "AppMessages" SET messages = ? WHERE "AppId" = ? AND language = ?', {
         type: QueryTypes.UPDATE,
-        replacements: [m.messages, m.AppId, m.language],
+        replacements: [JSON.stringify(m.messages), m.AppId, m.language],
       }),
     ),
   ]);
