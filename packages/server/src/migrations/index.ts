70415a342739d9abf15d6a26d5141972c86d7be0
Remco Haszing <remcohaszing@gmail.com>
2022-05-30 12:15:23 +0200

    Use more semantic naming for migrations

--- packages/server/src/migrations/index.ts
+++ packages/server/src/migrations/index.ts
@@ -1,106 +1,106 @@
 import { Migration } from '../utils/migrate';
-import * as m4 from './0.10.0';
-import * as m5 from './0.11.0';
-import * as m6 from './0.11.3';
-import * as m7 from './0.12.4';
-import * as m8 from './0.12.6';
-import * as m9 from './0.13.0';
-import * as m10 from './0.13.1';
-import * as m16 from './0.13.11';
-import * as m17 from './0.13.12';
-import * as m18 from './0.13.14';
-import * as m11 from './0.13.2';
-import * as m12 from './0.13.3';
-import * as m13 from './0.13.5';
-import * as m14 from './0.13.6';
-import * as m15 from './0.13.9';
-import * as m23 from './0.15.10';
-import * as m24 from './0.15.12';
-import * as m19 from './0.15.2';
-import * as m20 from './0.15.3';
-import * as m21 from './0.15.4';
-import * as m22 from './0.15.8';
-import * as m25 from './0.16.0';
-import * as m26 from './0.16.2';
-import * as m27 from './0.17.0';
-import * as m28 from './0.17.2';
-import * as m29 from './0.17.3';
-import * as m30 from './0.17.6';
-import * as m31 from './0.17.7';
-import * as m32 from './0.18.0';
-import * as m33 from './0.18.1';
-import * as m35 from './0.18.11';
-import * as m36 from './0.18.19';
-import * as m34 from './0.18.2';
-import * as m37 from './0.18.25';
-import * as m38 from './0.18.28';
-import * as m39 from './0.18.29';
-import * as m40 from './0.19.0';
-import * as m41 from './0.19.1';
-import * as m45 from './0.19.11';
-import * as m46 from './0.19.12';
-import * as m47 from './0.19.13';
-import * as m48 from './0.19.14';
-import * as m49 from './0.19.15';
-import * as m42 from './0.19.4';
-import * as m43 from './0.19.6';
-import * as m44 from './0.19.8';
-import * as m50 from './0.20.2';
-import * as m51 from './0.20.3';
-import * as m1 from './0.9.0';
-import * as m2 from './0.9.3';
-import * as m3 from './0.9.4';
+import * as m0_10_0 from './0.10.0';
+import * as m0_11_0 from './0.11.0';
+import * as m0_11_3 from './0.11.3';
+import * as m0_12_4 from './0.12.4';
+import * as m0_12_6 from './0.12.6';
+import * as m0_13_0 from './0.13.0';
+import * as m0_13_1 from './0.13.1';
+import * as m0_13_11 from './0.13.11';
+import * as m0_13_12 from './0.13.12';
+import * as m0_13_14 from './0.13.14';
+import * as m0_13_2 from './0.13.2';
+import * as m0_13_3 from './0.13.3';
+import * as m0_13_5 from './0.13.5';
+import * as m0_13_6 from './0.13.6';
+import * as m0_13_9 from './0.13.9';
+import * as m0_15_10 from './0.15.10';
+import * as m0_15_12 from './0.15.12';
+import * as m0_15_2 from './0.15.2';
+import * as m0_15_3 from './0.15.3';
+import * as m0_15_4 from './0.15.4';
+import * as m0_15_8 from './0.15.8';
+import * as m0_16_0 from './0.16.0';
+import * as m0_16_2 from './0.16.2';
+import * as m0_17_0 from './0.17.0';
+import * as m0_17_2 from './0.17.2';
+import * as m0_17_3 from './0.17.3';
+import * as m0_17_6 from './0.17.6';
+import * as m0_17_7 from './0.17.7';
+import * as m0_18_0 from './0.18.0';
+import * as m0_18_1 from './0.18.1';
+import * as m0_18_11 from './0.18.11';
+import * as m0_18_19 from './0.18.19';
+import * as m0_18_2 from './0.18.2';
+import * as m0_18_25 from './0.18.25';
+import * as m0_18_28 from './0.18.28';
+import * as m0_18_29 from './0.18.29';
+import * as m0_19_0 from './0.19.0';
+import * as m0_19_1 from './0.19.1';
+import * as m0_19_11 from './0.19.11';
+import * as m0_19_12 from './0.19.12';
+import * as m0_19_13 from './0.19.13';
+import * as m0_19_14 from './0.19.14';
+import * as m0_19_15 from './0.19.15';
+import * as m0_19_4 from './0.19.4';
+import * as m0_19_6 from './0.19.6';
+import * as m0_19_8 from './0.19.8';
+import * as m0_20_2 from './0.20.2';
+import * as m0_20_3 from './0.20.3';
+import * as m0_9_0 from './0.9.0';
+import * as m0_9_3 from './0.9.3';
+import * as m0_9_4 from './0.9.4';
 
 export const migrations: Migration[] = [
-  m1,
-  m2,
-  m3,
-  m4,
-  m5,
-  m6,
-  m7,
-  m8,
-  m9,
-  m10,
-  m11,
-  m12,
-  m13,
-  m14,
-  m15,
-  m16,
-  m17,
-  m18,
-  m19,
-  m20,
-  m21,
-  m22,
-  m23,
-  m24,
-  m25,
-  m26,
-  m27,
-  m28,
-  m29,
-  m30,
-  m31,
-  m32,
-  m33,
-  m34,
-  m35,
-  m36,
-  m37,
-  m38,
-  m39,
-  m40,
-  m41,
-  m42,
-  m43,
-  m44,
-  m45,
-  m46,
-  m47,
-  m48,
-  m49,
-  m50,
-  m51,
+  m0_9_0,
+  m0_9_3,
+  m0_9_4,
+  m0_10_0,
+  m0_11_0,
+  m0_11_3,
+  m0_12_4,
+  m0_12_6,
+  m0_13_0,
+  m0_13_1,
+  m0_13_11,
+  m0_13_12,
+  m0_13_14,
+  m0_13_2,
+  m0_13_3,
+  m0_13_5,
+  m0_13_6,
+  m0_13_9,
+  m0_15_10,
+  m0_15_12,
+  m0_15_2,
+  m0_15_3,
+  m0_15_4,
+  m0_15_8,
+  m0_16_0,
+  m0_16_2,
+  m0_17_0,
+  m0_17_2,
+  m0_17_3,
+  m0_17_6,
+  m0_17_7,
+  m0_18_0,
+  m0_18_1,
+  m0_18_11,
+  m0_18_19,
+  m0_18_2,
+  m0_18_25,
+  m0_18_28,
+  m0_18_29,
+  m0_19_0,
+  m0_19_1,
+  m0_19_11,
+  m0_19_12,
+  m0_19_13,
+  m0_19_14,
+  m0_19_15,
+  m0_19_4,
+  m0_19_6,
+  m0_19_8,
+  m0_20_2,
+  m0_20_3,
 ];
