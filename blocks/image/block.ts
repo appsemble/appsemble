2792de2447c780011732202c79543d1cdd277027
AinarsK <ainars@d-centralize.nl>
2022-06-02 09:46:18 +0200

    New image block

---
+++ blocks/image/block.ts
@@ -0,0 +1,45 @@
+import { Remapper } from '@appsemble/sdk';
+
+declare module '@appsemble/sdk' {
+  interface Actions {
+    /**
+     * What happens if the button is clicked.
+     */
+    onClick: never;
+  }
+  interface Parameters {
+    /**
+     * The URL of the image.
+     *
+     * Note that this is ignored if the onImage event listener is set.
+     */
+    url?: Remapper;
+
+    /**
+     * The ALT text of the image.
+     *
+     */
+    alt?: Remapper;
+
+    /**
+     * Is image rounded.
+     *
+     */
+    rounded?: boolean;
+
+    /**
+     * The alignment of the text content.
+     *
+     * @default 'left'
+     */
+    alignment?: 'center' | 'left' | 'right';
+
+    /**
+     * Is image rounded.
+     *
+     */
+    ratio?: string;
+  }
+}
+
+export {};
---
+++ blocks/image/src/index.ts
@@ -0,0 +1,36 @@
+import { bootstrap } from '@appsemble/sdk';
+
+/**
+ * @param {Object} actions - Prepared actions the block can dispatch.
+ * @param {Object} data - The data that was somehow passed into the block. I.e. data passed in from
+ * another block.
+ * @param {Object} events - Event related functions and constants.
+ * @param {Object} pageParameters - Parameters that the app creator defined in the app definition.
+ * @param {Object} utils - Some utility functions provided by the Appsemble framework.
+ */
+
+bootstrap(({ data, parameters: { alignment, alt, rounded, url }, utils }) => {
+  const node = document.createElement('figure');
+  const image = document.createElement('img');
+  node.append(image);
+
+  node.classList.add('image');
+
+  if (rounded) {
+    node.classList.add('is-rounded');
+  }
+  image.alt = utils.remap(alt, data) as string;
+  image.src = utils.remap(url, data) as string;
+
+  const wrapper = document.createElement('div');
+  wrapper.append(node);
+  wrapper.classList.add('is-flex');
+
+  if (alignment) {
+    wrapper.classList.add(`is-justify-content-${alignment}`);
+  } else {
+    wrapper.classList.add('is-justify-content-left');
+  }
+
+  return wrapper;
+});
