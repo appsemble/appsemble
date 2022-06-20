ff66b7b82c668488db2af597c420352049908757
Remco Haszing <remcohaszing@gmail.com>
2022-06-20 09:14:08 +0000

    Apply 3 suggestion(s) to 3 file(s)

--- packages/studio/src/pages/apps/app/edit/messages.ts
+++ packages/studio/src/pages/apps/app/edit/messages.ts
@@ -14,5 +14,5 @@ export const messages = defineMessages({
   coreStyle: 'Core style',
   sharedStyle: 'Shared style',
   cancel: 'Cancel',
-  notification: 'You have unsaved changes, do you wish to continue?',
+  notification: 'You have unsaved changes. Do you wish to continue?',
 });
