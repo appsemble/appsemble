63b5ae197c04dc06bfab0ff93defd45bb93f16ed
Jean-Marc <jeanmarc@d-centralize.nl>
2022-08-30 10:16:20 +0200

    Added e2e tests for deleting invites in organizations members

--- packages/e2e/studio/organizations.cy.ts
+++ packages/e2e/studio/organizations.cy.ts
@@ -17,4 +17,32 @@ describe('/organizations', () => {
     cy.contains('Unlittered').should('exist');
     cy.contains('action-button').should('exist');
   });
+
+  it('should cancel the deletion of invite', () => {
+    cy.get('[href="/en/organizations/appsemble/members]').click();
+    cy.contains('Organization Members').should('exist');
+    cy.get('button[class="button"]').should('exist').click();
+    cy.contains('Add new members').should('exist');
+    cy.get('input[class="input"]').should('exist').type('foo@bar.com');
+    cy.get('button[class="button"]').should('exist').click();
+    cy.get('table').contains('foo@bar.com').should('exist');
+    cy.get('button[class="button is-danger", title="Delete invite"]').should('exist').click();
+    cy.contains('Delete invite').should('exist');
+    cy.get('button[class="button is-white"]').contains('Cancel').should('exist').click();
+    cy.get('table').contains('foo@bar.com').should('exist');
+  });
+
+  it('should delete invite', () => {
+    cy.get('[href="/en/organizations/appsemble/members]').click();
+    cy.contains('Organization Members').should('exist');
+    cy.get('button[class="button"]').should('exist').click();
+    cy.contains('Add new members').should('exist');
+    cy.get('input[class="input"]').should('exist').type('foodelete@bar.com');
+    cy.get('button[class="button"]').should('exist').click();
+    cy.get('table').contains('foodelete@bar.com').should('exist');
+    cy.get('button[class="button is-danger", title="Delete invite"]').should('exist').click();
+    cy.contains('Delete invite').should('exist');
+    cy.get('button[class="button is-danger"]').contains('Delete invite').should('exist').click();
+    cy.get('table').contains('foodelete@bar.com').should('not.exist');
+  });
 });
