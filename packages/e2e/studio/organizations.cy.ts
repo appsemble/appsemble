0fb9564a463cad932f45f3afbfb756cd059577a4
Jean-Marc <jeanmarc@d-centralize.nl>
2022-08-30 14:56:08 +0200

    Fix broken organization e2e tests

--- packages/e2e/studio/organizations.cy.ts
+++ packages/e2e/studio/organizations.cy.ts
@@ -19,30 +19,40 @@ describe('/organizations', () => {
   });
 
   it('should cancel the deletion of invite', () => {
-    cy.get('[href="/en/organizations/appsemble/members]').click();
+    cy.login('/en/organizations/appsemble/members');
     cy.contains('Organization Members').should('exist');
     cy.get('button[class="button"]').should('exist').click();
     cy.contains('Add new members').should('exist');
-    cy.get('input[class="input"]').should('exist').type('foo@bar.com');
-    cy.get('button[class="button"]').should('exist').click();
-    cy.get('table').contains('foo@bar.com').should('exist');
-    cy.get('button[class="button is-danger", title="Delete invite"]').should('exist').click();
+    cy.get('input[name="email"]').should('exist').type('foo@bar.com');
+    cy.get('button[type="submit"]').should('contain.text', 'Add members').click();
+    cy.get('table')
+      .contains('foo@bar.com')
+      .should('exist')
+      .parent('tr')
+      .within(() => {
+        cy.get('button[title="Delete invite"]').should('exist').click();
+      });
     cy.contains('Delete invite').should('exist');
-    cy.get('button[class="button is-white"]').contains('Cancel').should('exist').click();
-    cy.get('table').contains('foo@bar.com').should('exist');
+    cy.get('button[type=button]')
+      .contains('Cancel')
+      .should('exist')
+      .then(($button) => {
+        $button.click();
+      });
   });
 
   it('should delete invite', () => {
-    cy.get('[href="/en/organizations/appsemble/members]').click();
+    cy.login('/en/organizations/appsemble/members');
     cy.contains('Organization Members').should('exist');
-    cy.get('button[class="button"]').should('exist').click();
-    cy.contains('Add new members').should('exist');
-    cy.get('input[class="input"]').should('exist').type('foodelete@bar.com');
-    cy.get('button[class="button"]').should('exist').click();
-    cy.get('table').contains('foodelete@bar.com').should('exist');
-    cy.get('button[class="button is-danger", title="Delete invite"]').should('exist').click();
+    cy.get('table')
+      .contains('foo@bar.com')
+      .should('exist')
+      .parent('tr')
+      .within(() => {
+        cy.get('button[title="Delete invite"]').should('exist').click();
+      });
     cy.contains('Delete invite').should('exist');
-    cy.get('button[class="button is-danger"]').contains('Delete invite').should('exist').click();
-    cy.get('table').contains('foodelete@bar.com').should('not.exist');
+    cy.get('button[type="button"]').contains('Delete invite').should('exist').click();
+    cy.get('table').contains('foo@bar.com').should('not.exist');
   });
 });
