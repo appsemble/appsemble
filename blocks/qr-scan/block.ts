b496ca9400b193dded80119ed3479f8ed8f6dbba
Veronika P-CB02 Student 4173287 <v.tsvietkova@student.fontys.nl>
2022-08-31 11:31:11 +0200

    QR scanner with implemented feedback on clear branch

---
+++ blocks/qr-scan/block.ts
@@ -0,0 +1,26 @@
+export {};
+
+declare module '@appsemble/sdk' {
+  interface Parameters {
+    /**
+     * Draws rectangle on the QR found.
+     * If true, the block will draw a rectangle around the QR code when it’s found.
+     *
+     * @default false
+     */
+    drawQr?: boolean;
+    // Video Element Height
+    height?: number;
+    // Video Element Width
+    width?: number;
+  }
+
+  interface EventEmitters {
+    /**
+     * Event that gets emitted once the qr scanner has identified and resolved a QR code.
+     *
+     * foundQr event only holds the string of the QR.
+     */
+    foundQr: never;
+  }
+}
