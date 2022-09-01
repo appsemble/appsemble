714ac2b89e3e86b0b6511c164b56493813040638
Veronika P-CB02 Student 4173287 <v.tsvietkova@student.fontys.nl>
2022-09-01 16:34:21 +0200

    no drawline function & config to prev v

--- blocks/qr-scan/src/index.ts
+++ blocks/qr-scan/src/index.ts
@@ -1,6 +1,5 @@
 import { bootstrap } from '@appsemble/sdk';
 import jsQR from 'jsqr';
-import { Point } from 'jsqr/dist/locator';
 
 bootstrap(({ events, parameters: { drawQr = false, height = 0, width = 0 }, theme, utils }) => {
   const div = document.createElement('div');
@@ -9,22 +8,6 @@ bootstrap(({ events, parameters: { drawQr = false, height = 0, width = 0 }, them
   const video = document.createElement('video');
   const canvasElement = document.createElement('canvas');
   const canvas = canvasElement.getContext('2d');
-
-  /**
-   * Draw a line on the canvas.
-   *
-   * @param begin  The position of the beginning of the line.
-   * @param end  The position of the end of the line.
-   * @param color  The color of the line as a hexadecimal color.
-   */
-  function drawLine(begin: Point, end: Point, color: string): void {
-    canvas.beginPath();
-    canvas.moveTo(begin.x, begin.y);
-    canvas.lineTo(end.x, end.y);
-    canvas.lineWidth = 4;
-    canvas.strokeStyle = color;
-    canvas.stroke();
-  }
 
   let stopped = false;
   utils.addCleanup(() => {
