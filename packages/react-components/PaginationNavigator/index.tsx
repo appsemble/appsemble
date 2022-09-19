0beff0b51461484b18e0d3c016572cd1e959b7b7
Jean-Marc <jeanmarc@d-centralize.nl>
2022-09-19 11:59:15 +0200

    Add documentation for react-component pagination

--- packages/react-components/PaginationNavigator/index.tsx
+++ packages/react-components/PaginationNavigator/index.tsx
@@ -6,14 +6,42 @@ import { PaginationNumbers } from '../PaginationNumbers/index.js';
 import { messages } from './messages.js';
 
 export interface PaginationNavigatorProps {
+  /**
+   * An array of amounts of rows to display per page. Infinity represents all rows.
+   */
   rowsPerPageOptions: number[];
+  /**
+   * The amount of rows to display per page.
+   */
   rowsPerPage: number;
+  /**
+   * The maximum number of entries.
+   */
   count: number;
+  /**
+   * The current page to display.
+   */
   page: number;
+  /**
+   * The callback to invoke when the page changes.
+   *
+   * @param page
+   */
   onPageChange: (page: number) => void;
+  /**
+   * The callback to invoke when the amount of rows per page changes.
+   *
+   * @param rowsPerPage
+   */
   onRowsPerPageChange: (rowsPerPage: number) => void;
 }
 
+/**
+ * A component to render a pagination control. Contains a dropdown to select the amount of rows to
+ * display per page, and a set of buttons to navigate to the previous and next page.
+ *
+ * @param props The properties of the component.
+ */
 export function PaginationNavigator({
   count,
   onPageChange,
