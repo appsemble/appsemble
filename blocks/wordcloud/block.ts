bb45decb0f9d0714bf1709540610117c3233b69d
SamPhilipsen <sam.philipsen@hotmail.nl>
2022-09-20 15:23:13 +0200

    Changing wordcloud library and adding a few options

--- blocks/wordcloud/block.ts
+++ blocks/wordcloud/block.ts
@@ -8,9 +8,57 @@ declare module '@appsemble/sdk' {
 
   interface Parameters {
     /**
+      Shape of the wordcloud.
+      @default 'circle'
+    */
+    shape: 'circle' 
+          | 'cardioid' 
+          | 'diamond' 
+          | 'square' 
+          | 'triangle-forward' 
+          | 'triangle' 
+          | 'pentagon' 
+          | 'star'
+    /**
      * A list of fields to render out in a table.
      */
-    fields: string[];
+    fields: string[];  
+
+    /**
+     *  List of options for rendering the wordcloud.
+     */
+    options: Options;
+  }
+
+  
+  export interface Options {
+
+    /**
+     *  The font the wordcloud words use.
+     */
+    font?: string | undefined
+
+    /**
+      The color of the wordcloud words.
+      @default '#000000'
+    */
+    color?: string | undefined
+
+    /**
+     *  The background color for the wordcloud.
+     */
+    backgroundColor?: string | undefined
+
+    /**
+      Probability for the word to rotate. Set the number to 1 to always rotate.
+      @default 0
+    */
+    rotateRatio?: number | undefined
+
+    /**
+      Number to multiply the word size by.
+    */
+    weightFactor?: number | undefined
   }
 }
 
--- blocks/wordcloud/src/index.tsx
+++ blocks/wordcloud/src/index.tsx
@@ -1,13 +1,13 @@
 import { bootstrap } from '@appsemble/preact';
 import { useEffect, useState } from 'preact/hooks';
-import Wordcloud from 'react-wordcloud';
+import WordcloudLogic from './WordcloudLogic';
 
-bootstrap(({ events, parameters: { fields } }) => {
-  const [data, setData] = useState<any[]>(fields);
+bootstrap(({ events, parameters: { shape, fields, options}}) => {
+  const [data, setData] = useState<string[]>(fields);
   const [error, setError] = useState(false);
 
   useEffect(() => {
-    const onData = (newData: unknown[], newError: unknown): void => {
+    const onData = (newData: string[], newError: unknown): void => {
       if (newError) {
         setError(true);
         setData(null);
@@ -29,62 +29,20 @@ bootstrap(({ events, parameters: { fields } }) => {
   }
 
   if (!data) {
-    //Set mock data
-    const dataset = [
-      "text",
-      "text",
-      "text",
-      "text",
-      "text",
-      "text",
-      "text",
-      "text",
-      "text",
-      "banana",
-      "banana",
-      "banana",
-      "banana",
-      "banana",
-      "car",
-      "car",
-      "car",
-      "car",
-      "car",
-      "car",
-    ]
-    setData(dataset)
+    return <p>No data</p>
   }
 
-  interface WordcloudItem {
-    text: string;
-    value: number
+  if(!data.length) {
+    return <p>Data is empty</p>
   }
   
-  const wordList: WordcloudItem[] = [];
-  
-  const mapObjectToWordcloudItem = (obj: string[]) => 
-  {
-    const kvPair: any = {}
-  
-    obj.forEach((word) => {
-      kvPair[word] = (kvPair[word] || 0) + 1
-    })
-  
-    const keys = Object.keys(kvPair)
-    
-    for(let i = 0; i < keys.length; i++) {
-      wordList.push({
-        'text': keys[i],
-        'value': kvPair[keys[i]]
-      })
-    }
-  }
-  
-  mapObjectToWordcloudItem(data)
-
   return (
     <div>
-      <Wordcloud words={wordList} />
+      <WordcloudLogic 
+        shape = {shape}
+        words = {fields}
+        options = {options}
+      />
     </div>
   );
 });
