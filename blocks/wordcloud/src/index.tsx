8a304b8f334f11743c0f9083b6cc630e61dfdd19
SamPhilipsen <sam@d-centralize.nl>
2022-09-22 10:55:25 +0200

    Adding support for objects by extracting strings from object values

--- blocks/wordcloud/src/index.tsx
+++ blocks/wordcloud/src/index.tsx
@@ -2,8 +2,8 @@ import { bootstrap } from '@appsemble/preact';
 import { useEffect, useState } from 'preact/hooks';
 import WordcloudLogic from './WordcloudLogic';
 
-bootstrap(({ events, parameters: { shape, fields, options}}) => {
-  const [data, setData] = useState<string[]>(fields);
+bootstrap(({ events, parameters: { shape, fields, options } }) => {
+  const [data, setData] = useState<any>(fields);
   const [error, setError] = useState(false);
 
   useEffect(() => {
@@ -24,25 +24,41 @@ bootstrap(({ events, parameters: { shape, fields, options}}) => {
     };
   }, [events]);
 
+  useEffect(() => {
+    setData(sortData(data));
+  }, []);
+
+  //This sets the data to an array of strings if needed/possible to feed to the wordcloud logic layer
+  const sortData = (data: any) => {
+    if (Array.isArray(data)) {
+      return data;
+    } else if (typeof data === 'object') {
+      const filteredList: string[] = Object.values(data as object).filter(
+        (item) => typeof item === 'string',
+      );
+      return filteredList;
+    } else if (typeof data === 'string') {
+      return [data];
+    } else {
+      throw new Error('Data type is not supported!');
+    }
+  };
+
   if (error) {
     return <p>Error loading data.</p>;
   }
 
   if (!data) {
-    return <p>No data</p>
+    return <p>No data</p>;
   }
 
-  if(!data.length) {
-    return <p>Data is empty</p>
+  if (!data.length) {
+    return <p>Data is empty</p>;
   }
-  
+
   return (
     <div>
-      <WordcloudLogic 
-        shape = {shape}
-        words = {fields}
-        options = {options}
-      />
+      <WordcloudLogic shape={shape} words={data} options={options} />
     </div>
   );
 });
