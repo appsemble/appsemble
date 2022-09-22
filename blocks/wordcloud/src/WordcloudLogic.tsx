5767180a0de339b9eaf0baced04e546908cbbf66
SamPhilipsen <sam@d-centralize.nl>
2022-09-22 10:55:25 +0200

    Adding support for objects by extracting strings from object values

--- blocks/wordcloud/src/WordcloudLogic.tsx
+++ blocks/wordcloud/src/WordcloudLogic.tsx
@@ -1,64 +1,64 @@
 import { useEffect, useRef, useState } from 'preact/hooks';
-import WordCloud from 'wordcloud'
+import WordCloud from 'wordcloud';
 
-const WordcloudLogic = (props: any) => { 
-    const [canvasWidth] = useState(1170)
-    const [canvasHeight] = useState(760)
-    const canvasRef: any = useRef(null)
-    const wordList: any[] = []
+const WordcloudLogic = (props: any) => {
+  const [canvasWidth] = useState(1170);
+  const [canvasHeight] = useState(760);
+  const canvasRef: any = useRef(null);
+  const [wordList, setWordList] = useState([]);
+  const [optionsList, setOptions] = useState({});
 
-    if(!WordCloud.isSupported)
-        return (<p>Your browser does not support the Wordcloud block!</p>)
+  useEffect(() => {
+    canvasRef.current.focus();
+    mapArrayToWordcloudObject(props.words);
+    setOptions({
+      ...props.options,
+      ...{ shape: props.shape },
+    });
+  }, []);
 
-    useEffect(() => {
-        canvasRef.current.focus();
-
-        const optionsList = {
-            ...props.options, 
-            ...{list: wordList}, 
-            ...{shape: props.shape}
-        }
-        console.log(optionsList)
-
-        if(canvasRef.current != null) {
-            WordCloud(canvasRef.current, optionsList)
-        }
-        mapObjectToWordcloudItem(props.words)
-    }, [])
-    
-    const mapObjectToWordcloudItem = (obj: string[]) => 
-    {
-        const kvPair: any = {}
-        if(wordList.length > 0) return;
-
-        obj.forEach((word) => {
-            kvPair[word] = (kvPair[word] || 0) + 1
-        })
-
-        const keys = Object.keys(kvPair)
-        
-        for(let i = 0; i < keys.length; i++) {
-            wordList.push(
-                [
-                    keys[i], kvPair[keys[i]]
-                ]
-            )
-        }
+  useEffect(() => {
+    if (canvasRef.current != null) {
+      WordCloud(canvasRef.current, {
+        ...optionsList, 
+        ...{ list: wordList }
+      });
     }
+  }, [wordList]);
 
-    return (
-        <div className='canvasContainer'>
-            <canvas 
-                ref={canvasRef} 
-                width={canvasWidth}
-                height={canvasHeight}
-                style={{
-                    margin: 'auto',
-                    padding: 0,
-                    display: 'flex'
-                }}
-            />
-        </div>
-    )
-}
-export default WordcloudLogic
+  //This turns the given string of words into an array of arrays in the format of [word, size]
+  const mapArrayToWordcloudObject = (obj: string[]) => {
+    const kvPair: any = {};
+    const tempWordlist = [];
+    if (wordList.length > 0) return;
+
+    obj.forEach((word) => {
+      kvPair[word] = (kvPair[word] || 0) + 1;
+    });
+
+    const keys = Object.keys(kvPair);
+
+    for (let i = 0; i < keys.length; i++) {
+      tempWordlist.push([keys[i], kvPair[keys[i]]]);
+    }
+    setWordList(tempWordlist);
+  };
+
+  if (!WordCloud.isSupported) return <p>Your browser does not support the Wordcloud block!</p>;
+
+  return (
+    <div className="canvasContainer">
+      <canvas
+        ref={canvasRef}
+        width={canvasWidth}
+        height={canvasHeight}
+        style={{
+          margin: 'auto',
+          padding: 0,
+          display: 'flex',
+        }}
+      />
+    </div>
+  );
+};
+export default WordcloudLogic;
