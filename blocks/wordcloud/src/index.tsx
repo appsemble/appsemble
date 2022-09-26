42a9092b2e32085f7b1457b487a01f754cc4970d
SamPhilipsen <sam@d-centralize.nl>
2022-09-26 14:17:20 +0200

    Adding support for event data

--- blocks/wordcloud/src/index.tsx
+++ blocks/wordcloud/src/index.tsx
@@ -2,46 +2,45 @@ import { bootstrap } from '@appsemble/preact';
 import { useEffect, useState } from 'preact/hooks';
 import WordcloudLogic from './WordcloudLogic';
 
-bootstrap(({ events, parameters: { shape, fields, options } }) => {
+bootstrap(({ events, parameters: { shape, fields, options }, ready, utils }) => {
   const [data, setData] = useState<any>(fields);
   const [error, setError] = useState(false);
+  console.log('Begin of block')
 
   useEffect(() => {
-    const onData = (newData: string[], newError: unknown): void => {
-      if (newError) {
-        setError(true);
-        setData(null);
-      } else {
-        setError(false);
-        setData(newData);
-      }
-    };
-
-    events.on.data(onData);
-
-    return () => {
-      events.off.data(onData);
-    };
-  }, [events]);
-
-  useEffect(() => {
-    setData(sortData(data));
-  }, []);
-
-  //This sets the data to an array of strings if needed/possible to feed to the wordcloud logic layer
-  const sortData = (data: any) => {
-    if (Array.isArray(data)) {
-      return data;
-    } else if (typeof data === 'object') {
-      const filteredList: string[] = Object.values(data as object).filter(
-        (item) => typeof item === 'string',
+    const hasListener = events.on.data((newData: []) => {
+      const values = newData.map((element: object)  => 
+        utils.remap(fields, element)
       );
-      return filteredList;
-    } else if (typeof data === 'string') {
-      return [data];
-    } else {
-      throw new Error('Data type is not supported!');
+      setData(sortData(values))
+      setError(false);
+    })
+    setError(hasListener)
+    ready()
+  }, [events, ready])
+
+  //Sorts the data to be an array of strings to feed to the wordcloud logic layer
+  const sortData = (givenWordsList: any) => {
+    let filteredList: string[] = [];
+    getDataType(givenWordsList)
+
+    function getDataType(data: any) {
+      if (Array.isArray(data)) {
+        data.forEach((element) => {
+          getDataType(element);
+        });
+      } else if (typeof data === 'object') {
+        Object.values(data as object).forEach((item) => {
+          console.log('Item ', item)
+          getDataType(item)
+        });
+      } else if (typeof data === 'string') {
+        filteredList.push(data);
+      } else {
+        return;
+      } 
     }
+    return filteredList;
   };
 
   if (error) {
@@ -58,7 +57,7 @@ bootstrap(({ events, parameters: { shape, fields, options } }) => {
 
   return (
     <div>
-      <WordcloudLogic shape={shape} words={data} options={options} />
+      <WordcloudLogic shape={shape} words={data} options={options} fields={fields} />
     </div>
   );
 });
