bae0dc0ade1d35e7fa8d819176ca95d0494c4b73
Jean-Marc Dañe <jeanmarc@d-centralize.nl>
2022-10-18 14:47:06 +0200

    Add tabs and live preview

---
+++ packages/studio/pages/apps/app/GuiEditor/Components/Sidebar/index.tsx
@@ -0,0 +1,27 @@
+import { ReactElement } from 'react';
+
+import styles from './index.module.css';
+
+interface SidebarProps {
+  children: ReactElement;
+  type: 'left' | 'right';
+  isOpen: boolean;
+}
+
+export function Sidebar({ children, isOpen, type }: SidebarProps): ReactElement {
+  return (
+    <div
+      className={`${type === 'left' ? styles.leftBar : styles.rightBar}  ${
+        isOpen ? styles.isOpen : styles.isClosed
+      }`}
+    >
+      <div
+        className={`${styles.root} ${
+          type === 'left' ? styles.leftBarSlider : styles.rightBarSlider
+        }`}
+      >
+        {children}
+      </div>
+    </div>
+  );
+}
--- packages/studio/pages/apps/app/GuiEditor/PagesTab/index.tsx
+++ packages/studio/pages/apps/app/GuiEditor/PagesTab/index.tsx
@@ -1,14 +1,32 @@
-import { ReactElement } from 'react';
+import { ReactElement, useRef } from 'react';
 
+import { useApp } from '../../index.js';
+import { Preview } from '../Components/Preview/index.js';
+import { Sidebar } from '../Components/Sidebar/index.js';
 import { GuiEditorTabs } from '../index.js';
+import styles from './index.module.css';
 
 interface PagesTabProps {
   tab: GuiEditorTabs;
+  isOpenLeft: boolean;
+  isOpenRight: boolean;
 }
-export function PagesTab({ tab }: PagesTabProps): ReactElement {
+
+export function PagesTab({ isOpenLeft, isOpenRight, tab }: PagesTabProps): ReactElement {
+  const { app } = useApp();
+  const frame = useRef<HTMLIFrameElement>();
+
   return (
-    <div>
-      <h1>{tab.title}</h1>
-    </div>
+    <>
+      <Sidebar isOpen={isOpenLeft} type="left">
+        <span className="text-2xl font-bold">{tab.title}</span>
+      </Sidebar>
+      <div className={styles.root}>
+        <Preview app={app} iframeRef={frame} />
+      </div>
+      <Sidebar isOpen={isOpenRight} type="right">
+        <span className="text-2xl font-bold">{tab.title}</span>
+      </Sidebar>
+    </>
   );
 }
