58d2017e12c21a83cd1f04c67c0da40e4dadc176
Jean-Marc Dañe <jeanmarc@d-centralize.nl>
2022-10-25 10:40:53 +0200

    Remove unused files

--- packages/studio/pages/apps/app/GuiEditor/SecurityTab/index.tsx
+++ packages/studio/pages/apps/app/GuiEditor/SecurityTab/index.tsx
@@ -2,7 +2,6 @@ import { ReactElement } from 'react';
 
 import { Sidebar } from '../Components/Sidebar/index.js';
 import { GuiEditorTabs } from '../index.js';
-import styles from '../PagesTab/index.module.css';
 
 interface SecurityTabProps {
   tab: GuiEditorTabs;
@@ -15,7 +14,7 @@ export function SecurityTab({ isOpenLeft, isOpenRight, tab }: SecurityTabProps):
       <Sidebar isOpen={isOpenLeft} type="left">
         <span className="text-2xl font-bold">{tab.title}</span>
       </Sidebar>
-      <div className={styles.root}>{tab.title}</div>
+      <div>{tab.title}</div>
       <Sidebar isOpen={isOpenRight} type="right">
         <span className="text-2xl font-bold">{tab.title}</span>
       </Sidebar>
