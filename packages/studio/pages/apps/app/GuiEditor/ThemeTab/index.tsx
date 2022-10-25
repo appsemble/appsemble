06d076fba2cae2d34073bf632b45f47bd3a59aec
Jean-Marc Dañe <jeanmarc@d-centralize.nl>
2022-10-25 10:40:53 +0200

    Remove unused files

--- packages/studio/pages/apps/app/GuiEditor/ThemeTab/index.tsx
+++ packages/studio/pages/apps/app/GuiEditor/ThemeTab/index.tsx
@@ -2,7 +2,6 @@ import { ReactElement } from 'react';
 
 import { Sidebar } from '../Components/Sidebar/index.js';
 import { GuiEditorTabs } from '../index.js';
-import styles from '../PagesTab/index.module.css';
 
 interface ThemeTabProps {
   tab: GuiEditorTabs;
@@ -15,7 +14,7 @@ export function ThemeTab({ isOpenLeft, isOpenRight, tab }: ThemeTabProps): React
       <Sidebar isOpen={isOpenLeft} type="left">
         <span className="text-2xl font-bold">{tab.title}</span>
       </Sidebar>
-      <div className={styles.root}>{tab.title}</div>
+      <div>{tab.title}</div>
       <Sidebar isOpen={isOpenRight} type="right">
         <span className="text-2xl font-bold">{tab.title}</span>
       </Sidebar>
