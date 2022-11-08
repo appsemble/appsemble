e179e713747fdfcc94002f2dd387dc9de8d3fe39
Jean-Marc Dañe <jeanmarc@d-centralize.nl>
2022-11-08 10:01:46 +0100

    Add translation keys and rebase

--- packages/studio/components/Breadcrumbs/messages.ts
+++ packages/studio/components/Breadcrumbs/messages.ts
@@ -2,4 +2,6 @@ import { defineMessages } from 'react-intl';
 
 export const messages = defineMessages({
   breadcrumbs: 'Breadcrumbs',
+  switchToGuiEditor: 'Switch to GUI Editor',
+  switchToCodeEditor: 'Switch to Code Editor',
 });
