bca8a6fd1d7455106244004c01950804b4b0e8f4
Jean-Marc Dañe <jeanmarc@d-centralize.nl>
2022-11-08 16:13:25 +0100

    Add tree list view for roles

---
+++ packages/studio/pages/apps/app/GuiEditor/Components/TreeList/index.tsx
@@ -0,0 +1,44 @@
+import { Button } from '@appsemble/react-components';
+import { ReactElement } from 'react';
+
+import styles from './index.module.css';
+
+interface TreeListProps {
+  label: string;
+  options: readonly string[];
+  onChange: (index: number) => void;
+  isSelected: boolean;
+  onClick?: () => void;
+  value: string;
+}
+
+export function TreeList({
+  isSelected,
+  label,
+  onChange,
+  onClick,
+  options,
+  value,
+}: TreeListProps): ReactElement {
+  return (
+    <>
+      <Button
+        className={`${styles.treeListTop} ${isSelected ? 'is-link' : ''}`}
+        icon={options.length > 0 ? 'chevron-down' : undefined}
+        iconPosition="left"
+        onClick={onClick}
+      >
+        {label}
+      </Button>
+      {options.map((option, index) => (
+        <Button
+          className={`${styles.treeListItem} ${value === option && isSelected ? 'is-info' : ''}`}
+          key={option}
+          onClick={() => onChange(index)}
+        >
+          {option}
+        </Button>
+      ))}
+    </>
+  );
+}
