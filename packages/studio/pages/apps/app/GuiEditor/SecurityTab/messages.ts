ac29fcb33faad9b07515d0e7505180224442ca69
Jean-Marc Dañe <jeanmarc@d-centralize.nl>
2022-11-08 13:16:23 +0100

    Add security sidebar

---
+++ packages/studio/pages/apps/app/GuiEditor/SecurityTab/messages.ts
@@ -0,0 +1,11 @@
+import { defineMessages } from 'react-intl';
+
+export const messages = defineMessages({
+  defaultTab: 'Default',
+  teamsTab: 'Teams',
+  rolesTab: 'Roles',
+  defaultRoleLabel: 'Default role',
+  defaultPolicyLabel: 'Default policy',
+  noRoles: 'You need a role first to set to default!',
+  defaultCreateNewRole: 'Create new role',
+});
