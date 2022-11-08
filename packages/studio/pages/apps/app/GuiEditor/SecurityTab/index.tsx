1af3618feecb32fb932b9f59a9c0a6ba60219254
Jean-Marc Dañe <jeanmarc@d-centralize.nl>
2022-11-08 13:16:23 +0100

    Add security sidebar

--- packages/studio/pages/apps/app/GuiEditor/SecurityTab/index.tsx
+++ packages/studio/pages/apps/app/GuiEditor/SecurityTab/index.tsx
@@ -1,26 +1,135 @@
-import { ReactElement } from 'react';
+import { Button } from '@appsemble/react-components';
+import { ReactElement, useCallback, useRef, useState } from 'react';
 import { useIntl } from 'react-intl';
 
+import { useApp } from '../../index.js';
+import { InputList } from '../Components/InputList/index.js';
+import { Preview } from '../Components/Preview/index.js';
 import { Sidebar } from '../Components/Sidebar/index.js';
-import { GuiEditorTabs } from '../index.js';
 import styles from './index.module.css';
+import { messages } from './messages.js';
 
 interface SecurityTabProps {
-  tab: GuiEditorTabs;
   isOpenLeft: boolean;
   isOpenRight: boolean;
 }
-export function SecurityTab({ isOpenLeft, isOpenRight, tab }: SecurityTabProps): ReactElement {
+const defaultTab = {
+  tab: 'default',
+  title: messages.defaultTab,
+};
+const teamsTab = {
+  tab: 'teams',
+  title: messages.teamsTab,
+};
+const rolesTab = {
+  tab: 'roles',
+  title: messages.rolesTab,
+};
+const Tabs = [defaultTab, teamsTab, rolesTab] as const;
+type LeftSidebar = typeof Tabs[number];
+
+const policyOptions = ['everyone', 'organization', 'invite'] as const;
+
+export function SecurityTab({ isOpenLeft, isOpenRight }: SecurityTabProps): ReactElement {
   const { formatMessage } = useIntl();
+  const { app, setApp } = useApp();
+  const frame = useRef<HTMLIFrameElement>();
+  const [currentSideBar, setCurrentSideBar] = useState<LeftSidebar>(defaultTab);
+
+  const onChangeDefaultPolicy = useCallback(
+    (index: number) => {
+      if (!app.definition.security) {
+        setApp({
+          ...app,
+          definition: {
+            ...app.definition,
+            security: {
+              ...app.definition.security,
+              default: { ...app.definition.security.default, policy: policyOptions[index] },
+            },
+          },
+        });
+      }
+    },
+    [app, setApp],
+  );
+
+  const onChangeDefaultRole = useCallback(
+    (index: number) => {
+      if (!app.definition.security) {
+        setApp({
+          ...app,
+          definition: {
+            ...app.definition,
+            security: {
+              ...app.definition.security,
+              default: {
+                ...app.definition.security.default,
+                role: Object.entries(app.definition.security?.roles || []).map(([key]) => key)[
+                  index
+                ],
+              },
+            },
+          },
+        });
+      }
+    },
+    [app, setApp],
+  );
 
   return (
     <>
       <Sidebar isOpen={isOpenLeft} type="left">
-        <span className="text-2xl font-bold">{formatMessage(tab.title)}</span>
+        <>
+          {Tabs.map((sidebar) => (
+            <Button
+              className={`${styles.leftBarButton} ${currentSideBar === sidebar ? 'is-link' : ''}`}
+              key={sidebar.tab}
+              onClick={() => setCurrentSideBar(sidebar)}
+            >
+              {formatMessage(sidebar.title)}
+            </Button>
+          ))}
+        </>
       </Sidebar>
-      <div className={styles.root}>{formatMessage(tab.title)}</div>
+      <div className={styles.root}>
+        <Preview app={app} iframeRef={frame} />
+      </div>
       <Sidebar isOpen={isOpenRight} type="right">
-        <span className="text-2xl font-bold">{formatMessage(tab.title)}</span>
+        <>
+          {currentSideBar.tab === 'default' && (
+            <div className={styles.rightBar}>
+              <InputList
+                label={formatMessage(messages.defaultRoleLabel)}
+                labelPosition="top"
+                onChange={onChangeDefaultRole}
+                options={Object.entries(app.definition.security?.roles || []).map(([key]) => key)}
+                value={app.definition.security?.default.role || ''}
+              />
+              <InputList
+                label={formatMessage(messages.defaultPolicyLabel)}
+                labelPosition="top"
+                onChange={onChangeDefaultPolicy}
+                options={policyOptions}
+                value={app.definition.security?.default.policy || ''}
+              />
+              {!app.definition.security?.roles && (
+                <>
+                  <p className="help is-danger">{formatMessage(messages.noRoles)}</p>
+                  <Button
+                    className="is-primary"
+                    icon="add"
+                    onClick={() => setCurrentSideBar(rolesTab)}
+                  >
+                    {formatMessage(messages.defaultCreateNewRole)}
+                  </Button>
+                </>
+              )}
+            </div>
+          )}
+          {currentSideBar.tab === 'teams' && <div />}
+          {currentSideBar.tab === 'roles' && <div />}
+        </>
       </Sidebar>
     </>
   );
--- packages/studio/pages/apps/app/GuiEditor/index.tsx
+++ packages/studio/pages/apps/app/GuiEditor/index.tsx
@@ -153,7 +153,7 @@ export default function EditPage(): ReactElement {
           <ThemeTab isOpenLeft={leftPanelOpen} isOpenRight={rightPanelOpen} tab={currentTab} />
         )}
         {currentTab.tabName === 'security' && (
-          <SecurityTab isOpenLeft={leftPanelOpen} isOpenRight={rightPanelOpen} tab={currentTab} />
+          <SecurityTab isOpenLeft={leftPanelOpen} isOpenRight={rightPanelOpen} />
         )}
       </div>
     </div>
