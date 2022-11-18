9ce978f8538d2985d1f351d716fdd7bfc3983eeb
Jean-Marc Dañe <jeanmarc@d-centralize.nl>
2022-11-18 16:09:55 +0100

    Security inheritance cycling check

--- packages/studio/pages/apps/app/GuiEditor/Components/InputList/index.tsx
+++ packages/studio/pages/apps/app/GuiEditor/Components/InputList/index.tsx
@@ -5,11 +5,12 @@ import styles from './index.module.css';
 import { ListItem } from './ListItem/index.js';
 
 interface InputStringProps {
-  label: string;
+  label?: string;
   labelPosition?: 'left' | 'top';
   onChange: (index: number) => void;
   value: string;
   options: readonly string[];
+  size?: 'large' | 'medium' | 'normal' | 'small';
 }
 
 export function InputList({
@@ -17,6 +18,7 @@ export function InputList({
   labelPosition = 'top',
   onChange,
   options,
+  size = 'normal',
   value,
 }: InputStringProps): ReactElement {
   const onDropdownChange = useCallback(
@@ -26,6 +28,34 @@ export function InputList({
     [onChange],
   );
 
+  let valueString;
+  switch (size) {
+    case 'large':
+      valueString = value.length > 15 ? `${value.slice(0, 15)}...` : value;
+      break;
+    case 'medium':
+      valueString = value.length > 10 ? `${value.slice(0, 10)}...` : value;
+      break;
+    case 'small':
+      valueString = value.length > 7 ? `${value.slice(0, 7)}...` : value;
+      break;
+    default:
+      valueString = value;
+      break;
+  }
+
+  if (!label) {
+    return (
+      <div className={`${styles.root} field`}>
+        <Dropdown className={styles.dropDown} label={<span className="px-1">{valueString}</span>}>
+          {options.map((option, index) => (
+            <ListItem index={index} key={option} onChange={onDropdownChange} value={option} />
+          ))}
+        </Dropdown>
+      </div>
+    );
+  }
+
   return (
     <div
       className={`${styles.root} field ${
@@ -33,7 +63,7 @@ export function InputList({
       }`}
     >
       <label className={styles.label}>{label}</label>
-      <Dropdown className={styles.dropDown} label={<span className="px-1">{value}</span>}>
+      <Dropdown className={styles.dropDown} label={<span className="px-1">{valueString}</span>}>
         {options.map((option, index) => (
           <ListItem index={index} key={option} onChange={onDropdownChange} value={option} />
         ))}
