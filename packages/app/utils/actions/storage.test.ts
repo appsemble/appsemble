6d750081a42c447f0319a98cafa3f811012dc137
SamPhilipsen <sam@d-centralize.nl>
2022-11-18 15:02:57 +0100

    Parameterize storage tests

--- packages/app/utils/actions/storage.test.ts
+++ packages/app/utils/actions/storage.test.ts
@@ -1,7 +1,7 @@
 import { IDBPDatabase } from 'idb';
 
 import { createTestAction } from '../makeActions.js';
-import { getDB } from './storage.js';
+import { getDB, readStorage, writeStorage } from './storage.js';
 
 let db: IDBPDatabase;
 
@@ -19,12 +19,17 @@ beforeEach(async () => {
 });
 
 describe('storage.read', () => {
-  it('should read from the store', async () => {
+  it.each`
+    storageType         | expectedResult
+    ${'localStorage'}   | ${'This is default test data from localStorage!'}
+    ${'sessionStorage'} | ${'This is default test data from sessionStorage!'}
+    ${'indexedDB'}      | ${'This is default test data!'}
+  `('should read from $storageType', async ({ expectedResult, storageType }) => {
     const action = createTestAction({
-      definition: { type: 'storage.read', key: { prop: 'test' } },
+      definition: { type: 'storage.read', key: { prop: 'test' }, storage: storageType },
     });
     const result = await action({ test: 'data' });
-    expect(result).toBe('This is default test data!');
+    expect(result).toBe(expectedResult);
   });
 
   it('should return undefined for unknown keys in the store', async () => {
@@ -33,22 +38,6 @@ describe('storage.read', () => {
     });
     const result = await action({ test: 'bla' });
     expect(result).toBeUndefined();
-  });
-
-  it('should read from localStorage', async () => {
-    const action = createTestAction({
-      definition: { type: 'storage.read', key: { prop: 'test' }, storage: 'localStorage' },
-    });
-    const result = await action({ test: 'data' });
-    expect(result).toBe('This is default test data from localStorage!');
-  });
-
-  it('should read from sessionStorage', async () => {
-    const action = createTestAction({
-      definition: { type: 'storage.read', key: { prop: 'test' }, storage: 'sessionStorage' },
-    });
-    const result = await action({ test: 'data' });
-    expect(result).toBe('This is default test data from sessionStorage!');
   });
 
   it('should throw error when key is not valid', async () => {
@@ -68,190 +57,95 @@ describe('storage.read', () => {
 });
 
 describe('storage.write', () => {
-  it('should store data using idb', async () => {
-    const action = createTestAction({
-      definition: { type: 'storage.write', key: { prop: 'key' }, value: { prop: 'data' } },
-    });
-    const data = {
-      key: 'key',
-      data: { this: 'is', 0: 'some', arbitrary: { data: 'storage' } },
-      date: new Date(),
-    };
-    const result = await action({
-      key: 'key',
-      data,
-    });
-    expect(result).toStrictEqual({ key: 'key', data });
-    expect(await db.get('storage', 'key')).toStrictEqual(data);
-  });
-
-  it('should store data using localStorage', async () => {
+  it.each`
+    storageType
+    ${'localStorage'}
+    ${'sessionStorage'}
+    ${'indexedDB'}
+  `('should store data using $storageType', async ({ storageType }) => {
     const action = createTestAction({
       definition: {
         type: 'storage.write',
         key: { prop: 'key' },
         value: { prop: 'data' },
-        storage: 'localStorage',
+        storage: storageType,
       },
     });
     const data = {
       key: 'key',
       data: { this: 'is', 0: 'some', arbitrary: { data: 'storage' } },
-      date: new Date(),
+      date: '2022-11-18T13:08:03.128Z',
     };
     const result = await action({
       key: 'key',
       data,
     });
+    const storageData = await readStorage(storageType, 'key');
     expect(result).toStrictEqual({ key: 'key', data });
-    expect(JSON.parse(localStorage.getItem('appsemble-42-key'))).toStrictEqual({
-      ...data,
-      date: data.date.toISOString(),
-    });
-  });
-
-  it('should store data using sessionStorage', async () => {
-    const action = createTestAction({
-      definition: {
-        type: 'storage.write',
-        key: { prop: 'key' },
-        value: { prop: 'data' },
-        storage: 'sessionStorage',
-      },
-    });
-    const data = {
-      key: 'key',
-      data: { this: 'is', 0: 'some', arbitrary: { data: 'storage' } },
-      date: new Date(),
-    };
-    const result = await action({
-      key: 'key',
-      data,
-    });
-    expect(result).toStrictEqual({ key: 'key', data });
-    expect(JSON.parse(sessionStorage.getItem('appsemble-42-key'))).toStrictEqual({
-      ...data,
-      date: data.date.toISOString(),
-    });
+    expect(storageData).toStrictEqual(data);
   });
 });
 
 describe('storage.delete', () => {
-  it('should delete an existing item entirely using localStorage', async () => {
-    const action = createTestAction({
-      definition: {
-        type: 'storage.delete',
-        key: { prop: 'key' },
-        storage: 'localStorage',
-      },
-    });
-    await action({ key: 'data' });
-    const result = localStorage.getItem('appsemble-42-data');
-    expect(result).toBeNull();
-  });
+  it.each`
+    storageType         | expectedResult
+    ${'localStorage'}   | ${'Could not find data at this key.'}
+    ${'sessionStorage'} | ${'Could not find data at this key.'}
+    ${'indexedDB'}      | ${undefined}
+  `(
+    'should delete an existing item entirely using $storageType',
+    async ({ expectedResult, storageType }) => {
+      const action = createTestAction({
+        definition: {
+          type: 'storage.delete',
+          key: { prop: 'key' },
+          storage: storageType,
+        },
+      });
+      await action({ key: 'data' });
+      let result;
 
-  it('should delete an existing item entirely using sessionStorage', async () => {
-    const action = createTestAction({
-      definition: {
-        type: 'storage.delete',
-        key: { prop: 'key' },
-        storage: 'sessionStorage',
-      },
-    });
-    await action({ key: 'data' });
-    const result = sessionStorage.getItem('appsemble-42-data');
-    expect(result).toBeNull();
-  });
+      try {
+        result = await readStorage(storageType, 'data');
+      } catch (error) {
+        result = (error as Error).message;
+      }
 
-  it('should delete an existing item entirely using indexedDB', async () => {
-    const action = createTestAction({
-      definition: {
-        type: 'storage.delete',
-        key: { prop: 'key' },
-        storage: 'indexedDB',
-      },
-    });
-    await action({ key: 'data' });
-    const result = await db.get('storage', 'data');
-    expect(result).toBeUndefined();
-  });
+      expect(result).toBe(expectedResult);
+    },
+  );
 });
 
 describe('storage.append', () => {
-  it('should add a new item to an existing dataset using localStorage', async () => {
+  it.each`
+    storageType
+    ${'localStorage'}
+    ${'sessionStorage'}
+    ${'indexedDB'}
+  `('should add a new item to an existing dataset using $storageType', async ({ storageType }) => {
     const action = createTestAction({
       definition: {
         type: 'storage.append',
         key: { prop: 'key' },
         value: { prop: 'data' },
-        storage: 'localStorage',
+        storage: storageType,
       },
     });
     const data = {
       key: 'key',
       data: { text: 'test' },
     };
-    localStorage.setItem('appsemble-42-key', JSON.stringify([data, data]));
+    writeStorage(storageType, 'key', [data, data]);
 
     const result = await action({
       key: 'key',
       data,
     });
 
-    const newStorage = JSON.parse(localStorage.getItem('appsemble-42-key'));
+    const newStorage = await readStorage(storageType, 'key');
 
     expect(result).toStrictEqual({ key: 'key', data });
-    expect(newStorage[1]).toStrictEqual({ key: 'key', data: data.data });
-  });
-
-  it('should add a new item to an existing dataset using sessionStorage', async () => {
-    const action = createTestAction({
-      definition: {
-        type: 'storage.append',
-        key: { prop: 'key' },
-        value: { prop: 'data' },
-        storage: 'sessionStorage',
-      },
-    });
-    const data = {
-      key: 'key',
-      data: { text: 'test' },
-    };
-
-    const result = await action({
-      key: 'key',
-      data,
-    });
-
-    const newStorage = JSON.parse(sessionStorage.getItem('appsemble-42-key'));
-
-    expect(result).toStrictEqual({ key: 'key', data });
-    expect(newStorage[1]).toStrictEqual({ key: 'key', data: data.data });
-  });
-
-  it('should add a new item to an existing dataset using indexedDB', async () => {
-    const action = createTestAction({
-      definition: {
-        type: 'storage.append',
-        key: { prop: 'key' },
-        value: { prop: 'data' },
-        storage: 'indexedDB',
-      },
-    });
-    const data = {
-      key: 'key',
-      data: { text: 'test' },
-    };
-
-    const result = await action({
-      key: 'data',
-      data,
-    });
-
-    const newStorage = await db.get('storage', 'data');
-
-    expect(result).toStrictEqual({ key: 'data', data });
-    expect(newStorage[1]).toStrictEqual({ key: 'key', data: data.data });
+    expect((newStorage as Object[])[1]).toStrictEqual({ key: 'key', data: data.data });
   });
 
   it('should turn existing dataset with a single object into an array with new value', async () => {
@@ -273,7 +167,7 @@ describe('storage.append', () => {
       data,
     });
 
-    const newStorage = JSON.parse(localStorage.getItem('appsemble-42-key'));
+    const newStorage = await readStorage('localStorage', 'key');
 
     expect(result).toStrictEqual({ key: 'key', data });
     expect(Array.isArray(newStorage)).toBe(true);
@@ -281,77 +175,37 @@ describe('storage.append', () => {
 });
 
 describe('storage.subtract', () => {
-  it('should remove the last item from an existing dataset using localStorage', async () => {
-    const action = createTestAction({
-      definition: {
-        type: 'storage.subtract',
-        key: { prop: 'key' },
-        storage: 'localStorage',
-      },
-    });
-    const data = {
-      key: 'key',
-      data: { text: 'test' },
-    };
-    localStorage.setItem('appsemble-42-key', JSON.stringify([data, data, data]));
+  it.each`
+    storageType
+    ${'localStorage'}
+    ${'sessionStorage'}
+    ${'indexedDB'}
+  `(
+    'should remove the last item from an existing dataset using $storageType',
+    async ({ storageType }) => {
+      const action = createTestAction({
+        definition: {
+          type: 'storage.subtract',
+          key: { prop: 'key' },
+          storage: storageType,
+        },
+      });
+      const data = {
+        key: 'key',
+        data: { text: 'test' },
+      };
+      writeStorage(storageType, 'key', [data, data, data]);
 
-    const result = await action({
-      key: 'key',
-    });
+      const result = await action({
+        key: 'key',
+      });
 
-    const newStorage = JSON.parse(localStorage.getItem('appsemble-42-key'));
+      const newStorage = await readStorage(storageType, 'key');
 
-    expect(result).toStrictEqual({ key: 'key' });
-    expect(newStorage).toHaveLength(2);
-  });
-
-  it('should remove the last item from an existing dataset using sessionStorage', async () => {
-    const action = createTestAction({
-      definition: {
-        type: 'storage.subtract',
-        key: { prop: 'key' },
-        storage: 'sessionStorage',
-      },
-    });
-    const data = {
-      key: 'key',
-      data: { text: 'test' },
-    };
-    sessionStorage.setItem('appsemble-42-key', JSON.stringify([data, data, data]));
-
-    const result = await action({
-      key: 'key',
-    });
-
-    const newStorage = JSON.parse(sessionStorage.getItem('appsemble-42-key'));
-
-    expect(result).toStrictEqual({ key: 'key' });
-    expect(newStorage).toHaveLength(2);
-  });
-
-  it('should remove the last item from an existing dataset using indexedDB', async () => {
-    const action = createTestAction({
-      definition: {
-        type: 'storage.subtract',
-        key: { prop: 'key' },
-        storage: 'indexedDB',
-      },
-    });
-    const data = {
-      key: 'key',
-      data: { text: 'test' },
-    };
-    await db.put('storage', [data, data, data], 'data');
-
-    const result = await action({
-      key: 'data',
-    });
-
-    const newStorage = await db.get('storage', 'data');
-
-    expect(result).toStrictEqual({ key: 'data' });
-    expect(newStorage).toHaveLength(2);
-  });
+      expect(result).toStrictEqual({ key: 'key' });
+      expect(newStorage).toHaveLength(2);
+    },
+  );
 
   it('should convert array to single object when there is only one entry left', async () => {
     const action = createTestAction({
@@ -365,13 +219,13 @@ describe('storage.subtract', () => {
       key: 'key',
       data: { text: 'test' },
     };
-    localStorage.setItem('appsemble-42-key', JSON.stringify([data, data]));
+    writeStorage('localStorage', 'key', [data, data]);
 
     await action({
       key: 'key',
     });
 
-    const result = JSON.parse(localStorage.getItem('appsemble-42-key'));
+    const result = await readStorage('localStorage', 'key');
 
     expect(result).toStrictEqual(data);
   });
@@ -388,118 +242,64 @@ describe('storage.subtract', () => {
       key: 'key',
       data: { text: 'test' },
     };
-    localStorage.setItem('appsemble-42-key', JSON.stringify(data));
+    writeStorage('localStorage', 'key', data);
 
     await action({
       key: 'key',
     });
+    let result;
 
-    const result = JSON.parse(localStorage.getItem('appsemble-42-key'));
+    try {
+      result = await readStorage('localStorage', 'key');
+    } catch (error) {
+      result = (error as Error).message;
+    }
 
-    expect(result).toBeNull();
+    expect(result).toBe('Could not find data at this key.');
   });
 });
 
 describe('storage.update', () => {
-  it('should update the specified item in the dataset using localStorage', async () => {
-    const action = createTestAction({
-      definition: {
-        type: 'storage.update',
-        key: { prop: 'key' },
-        item: { prop: 'item' },
-        value: { prop: 'value' },
-        storage: 'localStorage',
-      },
-    });
-    const data = {
-      key: 'key',
-      data: { text: 'test' },
-    };
-    localStorage.setItem('appsemble-42-key', JSON.stringify([data, data]));
+  it.each`
+    storageType
+    ${'localStorage'}
+    ${'sessionStorage'}
+    ${'indexedDB'}
+  `(
+    'should update the specified item in the dataset using $storageType',
+    async ({ storageType }) => {
+      const action = createTestAction({
+        definition: {
+          type: 'storage.update',
+          key: { prop: 'key' },
+          item: { prop: 'item' },
+          value: { prop: 'value' },
+          storage: storageType,
+        },
+      });
+      const data = {
+        key: 'key',
+        data: { text: 'test' },
+      };
+      writeStorage(storageType, 'key', [data, data]);
 
-    await action({
-      key: 'key',
-      item: 1,
-      value: {
+      await action({
+        key: 'key',
+        item: 1,
+        value: {
+          key: 'key',
+          data: { text: 'test works' },
+        },
+      });
+
+      const result = await readStorage(storageType, 'key');
+
+      expect((result as Object[])[1]).toStrictEqual({
         key: 'key',
         data: { text: 'test works' },
-      },
-    });
-
-    const result = JSON.parse(localStorage.getItem('appsemble-42-key'));
-
-    expect(result[1]).toStrictEqual({
-      key: 'key',
-      data: { text: 'test works' },
-    });
-  });
-
-  it('should update the specified item in the dataset using sessionStorage', async () => {
-    const action = createTestAction({
-      definition: {
-        type: 'storage.update',
-        key: { prop: 'key' },
-        item: { prop: 'item' },
-        value: { prop: 'value' },
-        storage: 'sessionStorage',
-      },
-    });
-    const data = {
-      key: 'key',
-      data: { text: 'test' },
-    };
-    sessionStorage.setItem('appsemble-42-key', JSON.stringify([data, data]));
-
-    await action({
-      key: 'key',
-      item: 1,
-      value: {
-        key: 'key',
-        data: { text: 'test works' },
-      },
-    });
-
-    const result = JSON.parse(sessionStorage.getItem('appsemble-42-key'));
-
-    expect(result[1]).toStrictEqual({
-      key: 'key',
-      data: { text: 'test works' },
-    });
-  });
-
-  it('should update the specified item in the dataset using indexedDB', async () => {
-    const action = createTestAction({
-      definition: {
-        type: 'storage.update',
-        key: { prop: 'key' },
-        item: { prop: 'item' },
-        value: { prop: 'value' },
-        storage: 'indexedDB',
-      },
-    });
-    const data = {
-      key: 'key',
-      data: { text: 'test' },
-    };
-
-    await db.put('storage', [data, data], 'data');
-
-    await action({
-      key: 'data',
-      item: 1,
-      value: {
-        key: 'key',
-        data: { text: 'test works' },
-      },
-    });
-
-    const result = await db.get('storage', 'data');
-
-    expect(result[1]).toStrictEqual({
-      key: 'key',
-      data: { text: 'test works' },
-    });
-  });
+      });
+    },
+  );
 
   it('should update the only item in storage', async () => {
     const action = createTestAction({
@@ -515,7 +315,7 @@ describe('storage.update', () => {
       key: 'key',
       data: { text: 'test' },
     };
-    localStorage.setItem('appsemble-42-key', JSON.stringify(data));
+    writeStorage('localStorage', 'key', data);
 
     await action({
       key: 'key',
@@ -525,7 +325,7 @@ describe('storage.update', () => {
       },
     });
 
-    const result = JSON.parse(localStorage.getItem('appsemble-42-key'));
+    const result = await readStorage('localStorage', 'key');
 
     expect(result).toStrictEqual({
       key: 'key',
