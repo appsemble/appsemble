0b03a70115bdadab5dde8219dc4de647c554b777
Jean-Marc Dañe <jeanmarc@d-centralize.nl>
2022-11-22 10:02:07 +0100

    Add create roll and inheritance cycle

--- packages/studio/pages/apps/app/GuiEditor/SecurityTab/messages.ts
+++ packages/studio/pages/apps/app/GuiEditor/SecurityTab/messages.ts
@@ -4,23 +4,4 @@ export const messages = defineMessages({
   defaultTab: 'Default',
   teamsTab: 'Teams',
   rolesTab: 'Roles',
-<<<<<<< HEAD
-=======
-  defaultRoleLabel: 'Default role',
-  defaultPolicyLabel: 'Default policy',
-  noRoles: 'You need a role first to set to default!',
-  defaultCreateNewRole: 'Create new role',
-  defaultPageLabel: 'Default Page',
-  noneLabel: 'None',
-  roleDescriptionLabel: 'Description',
-  roleNameLabel: 'Name',
-  cancelRoleNameButton: 'Cancel',
-  editRoleNameButton: 'Edit',
-  editNameRoleTitle: 'Edit role name:',
-  editRoleNameDescription: 'Changing a role name will change all of its references in the app.',
-  roleAlreadyExists: 'Role already exists',
-  roleInheritedLabel: 'Inherits from',
-  addNewRoleLabel: 'Add new role',
-  noRoleOptions: 'No more roles available',
->>>>>>> 7df68998f (Security inheritance cycling check)
 });
