74340a308999d046a749cf61bdc4e5442b76c5a6
Jean-Marc Dañe <jeanmarc@d-centralize.nl>
2022-11-23 16:05:11 +0100

    Refactor security tab

--- packages/studio/pages/apps/app/GuiEditor/SecurityTab/TeamsPage/index.tsx
+++ packages/studio/pages/apps/app/GuiEditor/SecurityTab/TeamsPage/index.tsx
@@ -1,20 +1,31 @@
 import { Button } from '@appsemble/react-components';
 import { ReactElement, useCallback } from 'react';
 import { useIntl } from 'react-intl';
+<<<<<<< HEAD
+=======
+import { NavLink } from 'react-router-dom';
+>>>>>>> 16fc07f16 (Refactor security tab)
 
 import { useApp } from '../../../index.js';
 import { InputList } from '../../Components/InputList/index.js';
 import { OptionalList } from '../../Components/OptionalList/index.js';
+<<<<<<< HEAD
 import { tabChangeOptions } from '../index.js';
+=======
+>>>>>>> 16fc07f16 (Refactor security tab)
 import { messages } from './messages.js';
 
 const teamsJoinOptions = ['anyone', 'invite'] as const;
 const teamsInviteOptions = ['$team:member', '$team:manager'] as const;
 
+<<<<<<< HEAD
 interface TeamsPageProps {
   onChangeTab: (tab: (typeof tabChangeOptions)[number]) => void;
 }
 export function TeamsPage({ onChangeTab }: TeamsPageProps): ReactElement {
+=======
+export function TeamsPage(): ReactElement {
+>>>>>>> 16fc07f16 (Refactor security tab)
   const { app, setApp } = useApp();
   const { formatMessage } = useIntl();
 
@@ -58,6 +69,7 @@ export function TeamsPage({ onChangeTab }: TeamsPageProps): ReactElement {
     return (
       <>
         <p className="help is-danger">{formatMessage(messages.noRoles)}</p>
+<<<<<<< HEAD
         <Button
           className="is-primary"
           component="a"
@@ -65,6 +77,10 @@ export function TeamsPage({ onChangeTab }: TeamsPageProps): ReactElement {
           onClick={() => onChangeTab('createRole')}
         >
           {formatMessage(messages.createNewRole)}
+=======
+        <Button className="is-primary" component="a" icon="add">
+          <NavLink to="#security/roles">{formatMessage(messages.createNewRole)}</NavLink>
+>>>>>>> 16fc07f16 (Refactor security tab)
         </Button>
       </>
     );
