2b5f11a3b53cce66595f0d08055e8ed4e1247f5e
Jean-Marc Dañe <jeanmarc@d-centralize.nl>
2022-11-25 12:28:01 +0100

    Add regex instead of chars and fix duplicate formatMessage

--- packages/studio/pages/apps/app/GuiEditor/index.tsx
+++ packages/studio/pages/apps/app/GuiEditor/index.tsx
@@ -65,7 +65,6 @@ export default function EditPage(): ReactElement {
   const location = useLocation();
   const [leftPanelOpen, setLeftPanelOpen] = useState(true);
   const [rightPanelOpen, setRightPanelOpen] = useState(true);
-  const { formatMessage } = useIntl();
 
   const match = useMatch('/:lang/apps/:id/edit/gui/*');
   const matchTabPath = useMatch('/:lang/apps/:id/edit/gui/:tab/*');
