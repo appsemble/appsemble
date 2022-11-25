a4574976a03bdf45ccbf4f1171e3d2ec9be96f9f
mistazidane <ntokungwiazidane@yahoo.com>
2022-11-25 08:21:22 +0100

    Permission descriptions are now shown using title.

--- packages/studio/pages/organizations/messages.ts
+++ packages/studio/pages/organizations/messages.ts
@@ -3,4 +3,23 @@ import { defineMessages } from 'react-intl';
 export const messages = defineMessages({
   title: 'Organizations',
   description: 'Browse the different organizations who are using Appsemble',
+  CreateApps: 'The permission to create new apps or copy them from templates.',
+  DeleteApps: ' The permission to delete apps.',
+  EditAppMessages: 'The permission to edit the app’s translations.',
+  EditAppSettings: 'The permission to edit the app’s settings.',
+  EditApps: 'The permission to edit the app definition.',
+  EditOrganization: 'The permission to edit the name and logo of an organization.',
+  InviteMember:
+    'The permission to invite new members into an organization, removing existing invites, and resending invites.',
+  ManageAssets: 'The permission to create and delete assets.',
+  ManageMembers: 'The permission to remove organization members.',
+  ManageResources: 'The permission to create, edit, and delete resources.',
+  ManageRoles: 'The permission to change the roles of organization members.',
+  ManageTeams: 'The permission to create and delete teams and manage its members.',
+  PublishBlocks: 'The permission to publish blocks for an organization.',
+  PushNotifications: 'The permission to send manual push notifications for an app.',
+  ReadAssets: 'The permission to read assets.',
+  ReadResources: 'The permission to read resources.',
+  ViewApps: 'The permission to view private apps of an organization.',
+  ViewMembers: 'The permission to view the list of members in an organization.',
 });
