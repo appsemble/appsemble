17ff7416e3c0c5fd1fa06d608c972706e582854a
SamPhilipsen <sam@d-centralize.nl>
2022-11-30 13:52:09 +0100

    Stop page from always loading when there is no steps

--- packages/app/components/Page/index.tsx
+++ packages/app/components/Page/index.tsx
@@ -43,8 +43,8 @@ export function Page(): ReactElement {
 
   const [data, setData] = useState<unknown>({});
   const [dialog, setDialog] = useState<ShowDialogParams>();
-  const [loading, setLoading] = useState<boolean>(true);
   const [stepRef, setStepRef] = useState(useRef<unknown>());
+  const [loopSteps, setLoopSteps] = useState<SubPage[]>();
 
   const [shareDialogParams, setShareDialogParams] = useState<ShareDialogState>();
   const showShareDialog: ShowShareDialog = useCallback(
@@ -91,7 +91,7 @@ export function Page(): ReactElement {
   const internalPageName = page ? normalize(page.name) : null;
   const prefix = index === -1 ? null : `pages.${internalPageName}`;
   const prefixIndex = index === -1 ? null : `pages.${index}`;
-  const [loopSteps, setLoopSteps] = useState<SubPage[]>();
+  const [loading, setLoading] = useState<boolean>(page.type === 'flow');
 
   const steps = useMemo(() => {
     if (page.type === 'flow') {
@@ -99,9 +99,10 @@ export function Page(): ReactElement {
       if (flowPage.steps) {
         return flowPage.steps;
       }
+      setPage({ ...page, steps: loopSteps });
       return loopSteps;
     }
-  }, [loopSteps, page]);
+  }, [loopSteps, page, setPage]);
 
   const remapWithContext = useCallback(
     (mappers: Remapper, input: any, context: Record<string, any>) =>
@@ -176,7 +177,7 @@ export function Page(): ReactElement {
   );
 
   useEffect(() => {
-    if (steps) {
+    if (page.type !== 'flow' || steps) {
       setLoading(false);
     } else {
       setLoading(true);
