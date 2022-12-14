a5055b13e21dc16ca3b6dff82505a238b9abbdd0
Jean-Marc Dañe <jeanmarc@d-centralize.nl>
2022-12-14 15:18:59 +0100

    Remove routing themes tab

---
+++ packages/studio/pages/apps/app/GuiEditor/ThemeTab/ThemePage/messages.ts
@@ -0,0 +1,17 @@
+import { defineMessages } from 'react-intl';
+
+export const messages = defineMessages({
+  themeColor: 'Theme Color',
+  splashColor: 'Splash Color',
+  primaryColor: 'Primary Color',
+  linkColor: 'Link Color',
+  successColor: 'Success Color',
+  infoColor: 'Info Color',
+  warningColor: 'Warning Color',
+  dangerColor: 'Danger Color',
+  font: 'Font',
+  fontFamily: 'Family',
+  fontSource: 'Source',
+  tileLayers: 'Tile Layers',
+  defaultTheme: 'Default Theme',
+});
