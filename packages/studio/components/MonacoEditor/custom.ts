4ba24aabae30493af98288ab0e860b99f445fa84
mariuszslawinski <mariuszslawinski.po@gmail.com>
2023-03-02 09:18:09 +0100

    refactor: remove `MonacoEnvironment` declaration

--- packages/studio/components/MonacoEditor/custom.ts
+++ packages/studio/components/MonacoEditor/custom.ts
@@ -9,7 +9,6 @@ import 'monaco-editor/esm/vs/editor/contrib/format/browser/formatActions.js';
 import 'monaco-editor/esm/vs/editor/contrib/hover/browser/hover.js';
 import 'monaco-editor/esm/vs/editor/contrib/inlineCompletions/browser/ghostText.contribution.js';
 import 'monaco-editor/esm/vs/editor/contrib/inlineCompletions/browser/inlineCompletionsContribution.js';
-import { Environment } from 'monaco-editor/esm/vs/editor/editor.api.js';
 import 'monaco-editor/esm/vs/editor/standalone/browser/quickAccess/standaloneCommandsQuickAccess.js';
 import 'monaco-editor/esm/vs/language/css/monaco.contribution.js';
 import 'monaco-editor/esm/vs/language/json/monaco.contribution.js';
@@ -17,12 +16,6 @@ import { setDiagnosticsOptions } from 'monaco-yaml';
 
 import { appValidationLabel } from './appValidation/index.js';
 import './languages.js';
-
-declare global {
-  interface Window {
-    MonacoEnvironment: Environment;
-  }
-}
 
 window.MonacoEnvironment = {
   getWorker(workerId, label) {
