b9d3216139b1cbd96ef6cf00384b59a39a90c06a
ThomasVersaevel <thomas0699@gmail.com>
2023-03-16 16:01:12 +0100

    Add fix for switch to gui-editor button

--- packages/react-components/Meta/index.tsx
+++ packages/react-components/Meta/index.tsx
@@ -130,7 +130,7 @@ export function useMeta(title: Text, description?: Text): void {
   const { pathname } = useLocation();
   // Don't count empty string-, language- and first page segments
   const segmentCount = pathname.split('/').length - 3;
-  const url = `.${'/..'.repeat(segmentCount > 0 ? segmentCount - depth : 0)}`;
+  const url = String(pathname + '/..'.repeat(segmentCount > 0 ? segmentCount - depth : 0));
 
   useEffect(() => {
     const formatMaybe = (string: Text): string =>
