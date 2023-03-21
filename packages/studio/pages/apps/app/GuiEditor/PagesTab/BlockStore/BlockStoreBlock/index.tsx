e00584e46244505926b7e85dc2cbe22a463989e1
ThomasVersaevel <thomas0699@gmail.com>
2023-03-21 13:12:36 +0100

    Revert "Change BlockStoreBlock from function to class to make it have state"
    This reverts commit b5942025a86ee64dd79b30f38145bcef694f387a.

--- packages/studio/pages/apps/app/GuiEditor/PagesTab/BlockStore/BlockStoreBlock/index.tsx
+++ packages/studio/pages/apps/app/GuiEditor/PagesTab/BlockStore/BlockStoreBlock/index.tsx
@@ -1,63 +1,62 @@
-import { Button, Icon, Subtitle, Title } from '@appsemble/react-components';
+import { Icon, Subtitle, Title } from '@appsemble/react-components';
 import { BlockManifest } from '@appsemble/types';
 import { defaultLocale, parseBlockName } from '@appsemble/utils';
-import { ReactElement, useState } from 'react';
+import { ReactElement } from 'react';
 
-import { MouseBlock } from '../MouseBlock/index.js';
 import styles from './index.module.css';
 import { messages } from './messages.js';
 
 /**
- * The draggable block shown as a box with the block name in it.
- * On mouse down a MouseBlock is attached to the mouse and folows it until mouse up.
+ * The block store block (BSB) is the draggable block shown as a box with the block name in it.
+ * On mouse down a copy of the BSB is attached to the mouse and folows it until mouse up.
+ * If mouse up happens outside of the app preview the BSB is discarded.
+ * If mouse up happens over the app preview the block corresponding to the held BSB is
+ * appended to the app definition at the end of the blocks list.
  *
- * @returns BlockStoreBlock
+ * @returns BSB
  */
+
 interface BlockStoreBlockProps {
+  /**
+   * The block to display.
+   */
   block: BlockManifest;
 }
 export function BlockStoreBlock({ block }: BlockStoreBlockProps): ReactElement {
   const [org, name] = parseBlockName(block.name);
-  const [active, setActive] = useState(false);
 
   return (
-    <>
-      {active === true && <MouseBlock block={block} />}
-      <Button
-        className={`card is-flex ${styles.root} ${styles.BlockStoreBlock}`}
-        key={name}
-        onClick={() => {
-          setActive(true);
-        }}
-        title={name}
-      >
-        <header className="px-2 py-2 is-flex">
-          <figure className={`image is-64x64 ${styles.nogrow}`}>
-            {block.iconUrl ? (
-              <img alt={`${name} ${messages.blockLogo}`} src={block.iconUrl} />
-            ) : (
-              <Icon className={styles.iconFallback} icon="cubes" />
-            )}
-          </figure>
-          <div className={`pl-3 pr-1 ${styles.header} ${styles.ellipsis}`}>
-            <Title
-              className={`${styles.ellipsis} ${styles.title}`}
-              lang={defaultLocale}
-              level={6}
-              size={6}
-            >
-              {name}
-            </Title>
-            <Subtitle className={styles.ellipsis} lang={defaultLocale} level={6} size={6}>
-              <div className={styles.linkColor}>{`@${org}`}</div>
-            </Subtitle>
-          </div>
-          <span className={`${styles.versionNr} has-text-grey ${styles.nogrow}`}>
-            {block.version}
-          </span>
-        </header>
-      </Button>
-    </>
+    <div
+      className={`card is-flex ${styles.root} ${styles.BlockStoreBlock}`}
+      key={block.name}
+      title={block.name}
+    >
+      <header className="px-2 py-2 is-flex">
+        <figure className={`image is-64x64 ${styles.nogrow}`}>
+          {block.iconUrl ? (
+            <img alt={`${block.name} ${messages.blockLogo}`} src={block.iconUrl} />
+          ) : (
+            <Icon className={styles.iconFallback} icon="cubes" />
+          )}
+        </figure>
+        <div className={`pl-3 pr-1 ${styles.header} ${styles.ellipsis}`}>
+          <Title
+            className={`${styles.ellipsis} ${styles.title}`}
+            lang={defaultLocale}
+            level={6}
+            size={6}
+          >
+            {name}
+          </Title>
+          <Subtitle className={styles.ellipsis} lang={defaultLocale} level={6} size={6}>
+            <div className={styles.linkColor}>{`@${org}`}</div>
+          </Subtitle>
+        </div>
+        <span className={`${styles.versionNr} has-text-grey ${styles.nogrow}`}>
+          {block.version}
+        </span>
+      </header>
+    </div>
   );
 }
 
