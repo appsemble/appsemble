1e3674f4c6bc5eb320cbd94a25bfac2978bb9255
VasilVelikovDCentralize <vasil@d-centralize.nl>
2023-03-29 18:15:21 +0300

    fix: indexing error

---
+++ packages/cli/server/routes/appRouter/index.ts
@@ -0,0 +1,5 @@
+import { createAppRouter } from '@appsemble/node-utils/server/routes/appRouter/index.js';
+
+import { options } from './options/options.js';
+
+export const appRouter = createAppRouter(options);
