05a69b092f5b906f7c282f930a617c1547bfd2da
ThomasVersaevel <thomas0699@gmail.com>
2023-04-17 10:30:12 +0200

    Added RecursiveProperty for remappers

---
+++ packages/studio/pages/apps/app/GuiEditor/Components/PropertiesHandler/Inputs/RemapperProperty/index.tsx
@@ -0,0 +1,47 @@
+import { InputField, TextAreaField } from '@appsemble/react-components';
+import { Schema } from 'jsonschema';
+import { OpenAPIV3 } from 'openapi-types';
+import { ChangeEvent, ReactElement, useCallback } from 'react';
+
+interface RemapperPropertyProps {
+  value: any;
+  schema: Schema;
+  property: string;
+  onChange: (property: string, value: any) => void;
+}
+export function RemapperProperty({
+  onChange,
+  property,
+  schema,
+  value,
+}: RemapperPropertyProps): ReactElement {
+  const { example, format, maxLength, minLength, multipleOf } = schema as OpenAPIV3.SchemaObject;
+  const commonProps = {
+    maxLength,
+    minLength,
+    placeholder: example,
+    step: multipleOf,
+    value,
+  };
+
+  const onValueChange = useCallback(
+    (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>, newValue: string) => {
+      onChange(property, newValue);
+    },
+    [onChange, property],
+  );
+
+  if (schema.multiline) {
+    return <TextAreaField {...commonProps} onChange={onValueChange} />;
+  }
+
+  return (
+    <InputField
+      {...commonProps}
+      onChange={onValueChange}
+      type={format === 'email' ? format : 'text'}
+    />
+  );
+}
+
+export default RemapperProperty;
