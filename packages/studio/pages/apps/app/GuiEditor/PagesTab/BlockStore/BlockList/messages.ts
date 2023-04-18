5e92d472ff656f468381cfdb3e7dbd166c591135
ThomasVersaevel <thomas0699@gmail.com>
2023-04-18 11:21:56 +0200

    Add searching in block store

---
+++ packages/studio/pages/apps/app/GuiEditor/PagesTab/BlockStore/BlockList/messages.ts
@@ -0,0 +1,8 @@
+import { defineMessages } from 'react-intl';
+
+export const messages = defineMessages({
+  error: 'Error',
+  loading: 'Loading blocks…',
+  emptyBlocks: 'No blocks can be displayed for your organisation',
+  noBlocks: 'No blocks match your search',
+});
