261540de8165210d3eaf1c77363004f214c0c2bf
parakhinam185 <parakhinam185@gmail.com>
2023-04-25 23:57:15 +0200

    fix test to resolve pipeline failing

--- packages/e2e/studio/root.spec.ts
+++ packages/e2e/studio/root.spec.ts
@@ -4,7 +4,7 @@ test('should allow to switch languages', async ({ page }) => {
   await page.goto('/en/apps');
   await expect(page.getByRole('button', { name: 'Login' })).toBeVisible();
   await page.getByRole('button', { name: 'EN' }).click();
-  await page.getByRole('link', { name: 'Dutch (Nederlands)' }).click();
+  await page.click('a:has-text("Dutch (Nederlands)")');
   // Expect should be always awaited
   await expect(page.getByRole('button', { name: 'Inloggen' })).toBeVisible();
 });
