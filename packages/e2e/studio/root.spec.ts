7b971a69854bed0b349cb4457e65e92e475096a4
parakhinam185 <parakhinam185@gmail.com>
2023-04-26 22:49:33 +0200

    increase delay

--- packages/e2e/studio/root.spec.ts
+++ packages/e2e/studio/root.spec.ts
@@ -4,7 +4,7 @@ test('should allow to switch languages', async ({ page }) => {
   await page.goto('/en/apps');
   await expect(page.getByRole('button', { name: 'Login' })).toBeVisible();
   await page.getByRole('button', { name: 'EN' }).click();
-  await page.getByRole('link', { name: 'Dutch (Nederlands)' }).click({ delay: 1000 });
+  await page.getByRole('link', { name: 'Dutch (Nederlands)' }).click({ delay: 5000 });
   // Expect should be always awaited
   await expect(page.getByRole('button', { name: 'Inloggen' })).toBeVisible();
 });
