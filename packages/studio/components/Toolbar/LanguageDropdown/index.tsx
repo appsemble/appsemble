ec5f73932229869ab85d613ed2ae849eb2b450d3
parakhinam185 <parakhinam185@gmail.com>
2023-04-27 00:03:57 +0200

    increase delay

--- packages/studio/components/Toolbar/LanguageDropdown/index.tsx
+++ packages/studio/components/Toolbar/LanguageDropdown/index.tsx
@@ -1,5 +1,5 @@
 import { NavbarDropdown, NavbarItem } from '@appsemble/react-components';
-import { type ReactElement, useEffect, useState } from 'react';
+import { type ReactElement } from 'react';
 import { useLocation, useParams } from 'react-router-dom';
 
 import { supportedLanguages } from '../../../utils/constants.js';
@@ -14,25 +14,12 @@ interface LanguageDropdownProps {
 export function LanguageDropdown({ className }: LanguageDropdownProps): ReactElement {
   const { lang } = useParams<{ lang: string }>();
   const { hash, pathname, search } = useLocation();
-  const [selectedLanguage, setSelectedLanguage] = useState(lang);
-
-  useEffect(() => {
-    localStorage.setItem('preferredLanguage', selectedLanguage);
-  }, [selectedLanguage]);
-
-  function handleLanguageChange(language: string): void {
-    setSelectedLanguage(language);
-    const newUrl = `${pathname.replace(lang, language)}${search}${hash}`;
-    window.history.pushState(null, '', newUrl);
-    window.dispatchEvent(new Event('popstate'));
-  }
 
   return (
     <NavbarDropdown className={className} color="dark" label={lang.split('-')[0].toUpperCase()}>
       {Object.entries(supportedLanguages).map(([language, name]) => (
         <NavbarItem
           key={language}
-          onClick={() => handleLanguageChange(language)}
           to={{ pathname: pathname.replace(lang, language), hash, search }}
         >
           {name}
