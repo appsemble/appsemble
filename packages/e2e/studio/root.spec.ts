83adaaec09714db77ead3513d84fc866532e8513
parakhinam185 <parakhinam185@gmail.com>
2023-04-27 04:18:34 +0200

    try to modify tests to wait for page to load

--- packages/e2e/studio/root.spec.ts
+++ packages/e2e/studio/root.spec.ts
@@ -4,7 +4,8 @@ test('should allow to switch languages', async ({ page }) => {
   await page.goto('/en/apps');
   await expect(page.getByRole('button', { name: 'Login' })).toBeVisible();
   await page.getByRole('button', { name: 'EN' }).click();
-  await page.getByRole('link', { name: 'Dutch (Nederlands)' }).click({ delay: 5000 });
+  await page.waitForTimeout(1000);
+  await page.getByRole('link', { name: 'Dutch (Nederlands)' }).click();
   // Expect should be always awaited
   await expect(page.getByRole('button', { name: 'Inloggen' })).toBeVisible();
 });
