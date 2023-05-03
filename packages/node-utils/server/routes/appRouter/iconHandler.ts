2e3615729b0a52701ffbcd42fe53e1e521a8fdbf
VasilVelikovDCentralize <vasil@d-centralize.nl>
2023-05-03 19:38:05 +0300

    fix: rollback proxy functions

--- packages/node-utils/server/routes/appRouter/iconHandler.ts
+++ packages/node-utils/server/routes/appRouter/iconHandler.ts
@@ -11,7 +11,7 @@ export function createIconHandler({ getApp, getAppIcon, getDbUpdated }: Options)
 
     const app = await getApp({ context: ctx, query: { maskable, updated } });
     const dbUpdated = app ? await getDbUpdated({ app, maskable, context: ctx }) : false;
-    const appIcon = await getAppIcon({ app, context: ctx });
+    const appIcon = app ? await getAppIcon({ app, context: ctx }) : null;
 
     await serveIcon(ctx, {
       background: maskable ? app.iconBackground || '#ffffff' : undefined,
--- packages/server/controllers/action.ts
+++ packages/server/controllers/action.ts
@@ -5,26 +5,11 @@ import {
   createProxyPost,
   createProxyPut,
 } from '@appsemble/node-utils';
-import { type DefaultContext, type DefaultState, type Middleware } from 'koa';
 
 import { options } from '../options/options.js';
 
-export function proxyDelete(): Middleware<DefaultState, DefaultContext> {
-  return createProxyDelete(options);
-}
-
-export function proxyGet(): Middleware<DefaultState, DefaultContext> {
-  return createProxyGet(options);
-}
-
-export function proxyPatch(): Middleware<DefaultState, DefaultContext> {
-  return createProxyPatch(options);
-}
-
-export function proxyPost(): Middleware<DefaultState, DefaultContext> {
-  return createProxyPost(options);
-}
-
-export function proxyPut(): Middleware<DefaultState, DefaultContext> {
-  return createProxyPut(options);
-}
+export const proxyDelete = createProxyDelete(options);
+export const proxyGet = createProxyGet(options);
+export const proxyPatch = createProxyPatch(options);
+export const proxyPost = createProxyPost(options);
+export const proxyPut = createProxyPut(options);
