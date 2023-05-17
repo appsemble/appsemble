0bd0c8080505745b9338cd90a083938cb7e36cb9
VasilVelikovDCentralize <vasil@d-centralize.nl>
2023-05-17 16:56:57 +0300

    fix: remove maskable from getApp query in iconHandler

--- packages/node-utils/server/routes/appRouter/iconHandler.ts
+++ packages/node-utils/server/routes/appRouter/iconHandler.ts
@@ -11,7 +11,7 @@ export function createIconHandler({ getApp, getAppIcon, getDbUpdated }: Options)
 
     const app = await getApp({
       context: ctx,
-      query: { where: { maskableIcon: maskable ? true : null, ...(updated ? { updated } : {}) } },
+      query: { where: { ...(updated ? { updated } : {}) } },
     });
     const dbUpdated = app ? await getDbUpdated({ app, maskable, context: ctx }) : false;
     const appIcon = app ? await getAppIcon({ app, context: ctx }) : null;
