489fdb30010f9bfeeb560dccafee43b9234212b4
SamPhilipsen <sam@d-centralize.nl>
2023-05-30 16:33:37 +0200

    Add styling to DocSection button

--- packages/react-components/DocSection/index.tsx
+++ packages/react-components/DocSection/index.tsx
@@ -1,5 +1,6 @@
 import { Fragment, type ReactElement, type ReactNode, useCallback, useState } from 'react';
 
+import styles from './index.module.css';
 import { Icon } from '../Icon/index.js';
 
 interface DocSectionProps {
@@ -18,10 +19,13 @@ export function DocSection({ children }: DocSectionProps): ReactElement {
         if (index === '0') {
           return (
             <Fragment key={index}>
-              <button onClick={clickHideButton} type="button">
-                <Icon icon="chevron-down" />
-                {child}
-              </button>
+              <Icon
+                className={styles.icon}
+                icon={hideChildren ? 'chevron-up' : 'chevron-down'}
+                onClick={clickHideButton}
+                size="medium"
+              />
+              {child}
             </Fragment>
           );
         }
@@ -29,7 +33,6 @@ export function DocSection({ children }: DocSectionProps): ReactElement {
           return child;
         }
       })}
-      <span />
     </>
   );
 }
