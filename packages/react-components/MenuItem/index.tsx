38557d1fd1b32f76a654258bc0d9cb80c44840e4
SamPhilipsen <sam@d-centralize.nl>
2023-06-09 19:06:01 +0200

    Fix users going to the page instead of collapsing the section

--- packages/react-components/MenuItem/index.tsx
+++ packages/react-components/MenuItem/index.tsx
@@ -1,5 +1,6 @@
 import { type IconName } from '@fortawesome/fontawesome-common-types';
 import { type ReactElement, type ReactNode, useCallback, useContext } from 'react';
+import { useLocation } from 'react-router-dom';
 
 import styles from './index.module.css';
 import { CollapsedContext, Icon, NavLink } from '../index.js';
@@ -38,26 +39,35 @@ interface SideNavLinkProps {
  */
 export function MenuItem({ children, exact, icon, title, to }: SideNavLinkProps): ReactElement {
   const { collapsed, collapsible, setCollapsed } = useContext(CollapsedContext);
+  const location = useLocation();
   const clickHideButton = useCallback(() => {
     setCollapsed(!collapsed);
   }, [collapsed, setCollapsed]);
+
   return (
-    <NavLink
-      className={`is-flex is-align-items-center ${styles.root}`}
-      exact={exact}
-      title={title}
-      to={to}
-    >
-      {icon ? <Icon className={`mr-1 ${styles.middle}`} icon={icon} size="medium" /> : null}
-      <span className={styles.text}>{children}</span>
+    <div className={styles.menuContainer}>
+      <NavLink
+        className={`is-flex is-align-items-center ${styles.root}`}
+        exact={exact}
+        title={title}
+        to={to}
+      >
+        {icon ? <Icon className={`mr-1 ${styles.middle}`} icon={icon} size="medium" /> : null}
+        <span className={styles.text}>{children}</span>
+      </NavLink>
       {collapsible ? (
         <Icon
           className={styles.icon}
+          color={
+            location.pathname === to || (!exact && location.pathname.startsWith(`${to}/`))
+              ? 'white'
+              : 'dark'
+          }
           icon={collapsed ? 'chevron-up' : 'chevron-down'}
           onClick={clickHideButton}
           size="medium"
         />
       ) : null}
-    </NavLink>
+    </div>
   );
 }
