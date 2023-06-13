0602d22b3795bd8a7febb60bfbc786c235d89e88
VasilVelikovDCentralize <vasil@d-centralize.nl>
2023-06-13 15:29:35 +0300

    fix: sass

--- packages/node-utils/server/routes/appRouter/bulmaHandler.ts
+++ packages/node-utils/server/routes/appRouter/bulmaHandler.ts
@@ -5,7 +5,7 @@ import { type Theme as ThemeType } from '@appsemble/types';
 import { baseTheme } from '@appsemble/utils';
 import bulma from 'bulma/package.json' assert { type: 'json' };
 import { type Context, type Middleware } from 'koa';
-import { renderSync } from 'sass';
+import sass from 'sass';
 import stripBom from 'strip-bom';
 
 const require = createRequire(import.meta.url);
@@ -113,7 +113,7 @@ export function createBulmaHandler({ createTheme, getTheme }: Options): Middlewa
     if (!css) {
       css = stripBom(
         String(
-          renderSync({
+          sass.renderSync({
             data: processStyle(theme),
             outputStyle: 'compressed',
             logger: {
