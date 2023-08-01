7363c8cd10efd86030b9608b7f7c6e83505718ee
Nikolay Gruychev <nikolay@appsemble.com>
2023-08-01 16:00:26 +0200

    feat: add "add to collection" button on app page

---
+++ packages/studio/components/AddToCollectionButton/messages.ts
@@ -0,0 +1,20 @@
+import { defineMessages } from 'react-intl';
+
+export const messages = defineMessages({
+  alreadyInCollection: {
+    id: 'studio.XTIljc',
+    defaultMessage: 'App is already in collection',
+  },
+  addToCollection: {
+    id: 'studio.ub1kHa',
+    defaultMessage: 'Add to collection',
+  },
+  cancel: {
+    id: 'studio.47FYwb',
+    defaultMessage: 'Cancel',
+  },
+  error: {
+    id: 'studio.MnBkWQ',
+    defaultMessage: 'Something went wrong when adding app to collection',
+  },
+});
