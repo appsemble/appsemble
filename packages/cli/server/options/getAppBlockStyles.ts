10acdf65d68b553bc5cb31ca626e868301a1240f
VasilVelikovDCentralize <vasil@d-centralize.nl>
2023-08-22 17:46:04 +0300

    fix(server): fix block styles logic
    fix(server): fix block styles logic

--- packages/cli/server/options/getAppBlockStyles.ts
+++ packages/cli/server/options/getAppBlockStyles.ts
@@ -7,7 +7,6 @@ import {
   opendirSafe,
 } from '@appsemble/node-utils';
 import { parseBlockName } from '@appsemble/utils';
-import globalCacheDir from 'global-cache-dir';
 
 import { processCss } from '../../lib/processCss.js';
 
@@ -15,48 +14,21 @@ export async function getAppBlockStyles({
   context,
   name,
 }: GetAppBlockStylesParams): Promise<AppBlockStyleInterface[]> {
-  const { appBlocks, appPath, blockConfigs } = context;
+  const { appPath } = context;
 
-  const block = appBlocks.find((appBlock) => appBlock.name === name);
-  const filename = block.files.find((file) => file.endsWith('.css'));
-
-  const [organization, blockName] = parseBlockName(name);
-  let styleString = '';
-
-  if (filename) {
-    let stylePath;
-
-    if (existsSync(join(process.cwd(), 'blocks', blockName))) {
-      const blockConfig = blockConfigs.find((config) => config.name === name);
-      stylePath = `${blockConfig.dir}/${blockConfig.output}/${filename}`;
-    } else {
-      const cacheDir = await globalCacheDir('appsemble');
-      stylePath = join(
-        cacheDir,
-        'blocks',
-        organization,
-        blockName,
-        block.version,
-        'assets',
-        filename,
-      );
-    }
-
-    const style = await processCss(stylePath);
-    styleString += style;
-  }
+  const [, blockName] = parseBlockName(name);
+  let style = '';
 
   await opendirSafe(join(appPath, 'theme'), async (themeDir, themeStat) => {
     const blockDir = join(themeDir, blockName);
     if (themeStat.name.startsWith('@') && existsSync(blockDir)) {
       await opendirSafe(blockDir, async (file) => {
         if (file.endsWith('.css')) {
-          const blockThemeStyle = await processCss(join(file));
-          styleString += blockThemeStyle;
+          style = await processCss(join(file));
         }
       });
     }
   });
 
-  return [{ style: styleString }];
+  return [{ style }];
 }
