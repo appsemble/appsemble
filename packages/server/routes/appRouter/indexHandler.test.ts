079c6c6cac59cc40bdb0723d370ffff82d2d2bf6
Prikshit Gautam <prikshit@appsemble.com>
2024-01-05 09:26:00 +0530

    fix csp error in studio

--- packages/server/routes/appRouter/indexHandler.test.ts
+++ packages/server/routes/appRouter/indexHandler.test.ts
@@ -221,7 +221,7 @@ it('should render the index page', async () => {
 
   expect(response).toMatchInlineSnapshot(`
     HTTP/1.1 200 OK
-    Content-Security-Policy: connect-src * blob: data:; default-src 'self'; font-src * data:; frame-src 'self' *.vimeo.com *.weseedo.nl *.youtube.com; img-src * blob: data: http://host.example; media-src * blob: data: http://host.example; script-src 'nonce-AAAAAAAAAAAAAAAAAAAAAA==' 'self' 'sha256-5TjCAOw45eTGiOwQpXti57jYy1CXWHO2kkmgUKryodQ=' 'unsafe-eval'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com
+    Content-Security-Policy: connect-src * blob: data:; default-src 'self'; font-src * data:; frame-src 'self' *.vimeo.com *.weseedo.nl *.youtube.com http://host.example; img-src * blob: data: http://host.example; media-src * blob: data: http://host.example; script-src 'nonce-AAAAAAAAAAAAAAAAAAAAAA==' 'self' 'sha256-5TjCAOw45eTGiOwQpXti57jYy1CXWHO2kkmgUKryodQ=' 'unsafe-eval'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com
     Content-Type: text/html; charset=utf-8
 
     {
