312d00ec76c87f1511850894d270c9d92931b001
Ekhorn <koen@kschellingerhout.nl>
2024-01-31 22:23:38 +0100

    test(studio): unsaved changes in code editor

--- packages/e2e/studio/apps/app.spec.ts
+++ packages/e2e/studio/apps/app.spec.ts
@@ -1,10 +1,65 @@
-import { test } from '../../fixtures/test/index.js';
+import { expect, test } from '../../fixtures/test/index.js';
 
 test.describe('/apps/:appId', () => {
   test.beforeEach(async ({ login, page }) => {
     await login('/en/apps');
     await page.getByRole('link', { name: 'Person Appsemble' }).first().click();
     await page.waitForSelector('text=Clone App');
+  });
+
+  test('should prompt when user has unsaved changes', async ({ page }) => {
+    await page.click('text=Editor');
+    await expect(page.getByRole('button', { name: 'Publish' })).toBeDisabled();
+    await page.getByRole('code').getByText('Person', { exact: true }).dblclick();
+    await page.getByRole('code').getByText('Person', { exact: true }).press('Backspace');
+    await page.getByRole('code').getByText('name:').first().press('t+e+s+t');
+    await expect(page.getByRole('button', { name: 'Publish' })).toBeEnabled();
+    await page.getByLabel('Open menu').click();
+
+    // TODO: could not get close or reload to work within this test.
+    // page.close({ runBeforeUnload: true });
+    // page.once('dialog', (dialog) => { dialog.dismiss(); }).reload();
+    await page
+      .once('dialog', (dialog) => {
+        expect(dialog.type()).toBe('confirm');
+        expect(dialog.message()).toBe('You have unsaved changes. Do you wish to continue?');
+        dialog.dismiss();
+      })
+      .getByRole('link', { name: 'Details' })
+      .click();
+    await expect(page.getByText('Clone App')).toBeHidden();
+
+    await page
+      .once('dialog', (dialog) => {
+        expect(dialog.type()).toBe('confirm');
+        expect(dialog.message()).toBe('You have unsaved changes. Do you wish to continue?');
+        dialog.accept();
+      })
+      .getByRole('link', { name: 'Details' })
+      .click();
+    await expect(page.getByText('Clone App')).toBeVisible();
+  });
+
+  test('should not prompt when user has saved their changes', async ({ page }) => {
+    await page.click('text=Editor');
+    await expect(page.getByRole('button', { name: 'Publish' })).toBeDisabled();
+    await page.getByRole('code').getByText('Person', { exact: true }).dblclick();
+    await page.getByRole('code').getByText('Person', { exact: true }).press('Backspace');
+    // Write out the same app name but add a new line. To avoid breaking the test on repeat.
+    await page.getByRole('code').getByText('name:').first().press('P+e+r+s+o+n+Enter');
+    await page.getByRole('button', { name: 'Publish' }).click();
+    // The validation needs to catch up otherwise the dialog will still open.
+    await page.waitForResponse('/api/apps/*');
+    await page.getByLabel('Open menu').click();
+
+    await page
+      .once('dialog', (dialog) => {
+        expect(dialog.message()).toBe('this should not be shown');
+        expect(dialog.type()).toBe('this should not be shown');
+      })
+      .getByRole('link', { name: 'Details' })
+      .click();
+    await expect(page.getByText('Clone App')).toBeVisible();
   });
 
   test('should link to the asset viewer', async ({ page }) => {
