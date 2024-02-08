16c966edb79e23ec373ee0eed944470c92dca7fc
Alex Svetoslavov <xfowth@gmail.com>
2024-02-08 11:05:07 +0000

    feat: add messages based on input to docs search

--- packages/studio/pages/docs/search/messages.ts
+++ packages/studio/pages/docs/search/messages.ts
@@ -5,4 +5,16 @@ export const messages = defineMessages({
     id: 'studio.xmcVZ0',
     defaultMessage: 'Search',
   },
+  noResultsFound: {
+    id: 'studio.hX5PAb',
+    defaultMessage: 'No results found',
+  },
+  minCharLength: {
+    id: 'studio.xHhWgf',
+    defaultMessage: 'You need at least 3 characters to start searching',
+  },
+  searching: {
+    id: 'studio.sZBZsJ',
+    defaultMessage: 'Searching for: {cleanedNeedle}',
+  },
 });
