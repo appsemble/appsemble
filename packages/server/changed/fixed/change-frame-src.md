3e57c31269fb4d1abcb73f6ae40f6ccc5e2ca176
Prikshit Gautam <prikshit@appsemble.com>
2024-03-08 05:11:20 +0000

    chore: limit csp frame src

---
+++ packages/server/changed/fixed/change-frame-src.md
@@ -0,0 +1,1 @@
+Change `frame-src csp` settings wildcard directive.
--- packages/server/routes/studioRouter/indexHandler.ts
+++ packages/server/routes/studioRouter/indexHandler.ts
@@ -5,6 +5,7 @@ import { createSettings, makeCSP, render } from '@appsemble/node-utils';
 import { type Context } from 'koa';
 
 import { argv } from '../../utils/argv.js';
+import { fetchCustomAppDomains } from '../../utils/fetchCustomDomains.js';
 import { githubPreset, gitlabPreset, googlePreset } from '../../utils/OAuth2Presets.js';
 import { getSentryClientSettings } from '../../utils/sentry.js';
 
@@ -14,14 +15,16 @@ import { getSentryClientSettings } from '../../utils/sentry.js';
  * @param ctx The Koa context.
  * @returns void
  */
-export function indexHandler(ctx: Context): Promise<void> {
+export async function indexHandler(ctx: Context): Promise<void> {
   const {
     hostname,
     state: { appCollectionId },
   } = ctx;
   const { disableRegistration, githubClientId, gitlabClientId, googleClientId, host } = argv;
   const logins = [];
+  const domains = await fetchCustomAppDomains();
   ctx.set('x-content-type-options', 'no-sniff');
+
   if (githubClientId) {
     logins.push({
       authorizationUrl: githubPreset.authorizationUrl,
@@ -82,7 +85,7 @@ export function indexHandler(ctx: Context): Promise<void> {
       'https://fonts.googleapis.com',
     ],
     'font-src': ["'self'", 'https://fonts.gstatic.com'],
-    'frame-src': [`*.${new URL(host).host}`, host, '*'],
+    'frame-src': [`*.${new URL(host).host}`, host, ...domains],
   });
   ctx.set('Content-Security-Policy', csp);
   return render(ctx, 'studio/index.html', { nonce, settings });
