948fad6b8f79a57b37b04e8852c04ba1e0a86923
Ekhorn <koen@kschellingerhout.nl>
2024-03-11 09:55:28 +0100

    chore: update migration to minor version

--- packages/server/migrations/0.26.0.ts
+++ packages/server/migrations/0.26.0.ts
@@ -1,7 +1,7 @@
 import { logger } from '@appsemble/node-utils';
 import { DataTypes, type Sequelize } from 'sequelize';
 
-export const key = '0.25.3';
+export const key = '0.26.0';
 
 /**
  * Summary:
