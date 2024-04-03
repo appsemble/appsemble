85b4e203b09855ed3058f15b56845ea37122b9e6
Angel Angelov <7129907-angelovdev@users.noreply.gitlab.com>
2024-04-03 06:54:27 +0000

    feat: security.txt file

---
+++ packages/server/routes/studioRouter/securitytxtHandler.ts
@@ -0,0 +1,6 @@
+import { readAsset } from '@appsemble/node-utils';
+import { type Context } from 'koa';
+
+export async function securitytxtHandler(ctx: Context): Promise<void> {
+  ctx.body = await readAsset('security.txt');
+}
