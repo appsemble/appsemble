1b03adec7ac71c48dde22193ee161cfc07e09eab
Prikshit Gautam <prikshit@appsemble.com>
2024-05-14 12:31:21 +0530

    feat(cli): add get app variables endpoint for dev server

--- packages/cli/server/controllers/index.ts
+++ packages/cli/server/controllers/index.ts
@@ -2,6 +2,7 @@ export * from './action.js';
 export * from './appMessages.js';
 export * from './resources.js';
 export * from './assets.js';
+export * from './appVariables.js';
 export * from './appMembers.js';
 export * from './teams.js';
 export * from './oauth2Provider.js';
