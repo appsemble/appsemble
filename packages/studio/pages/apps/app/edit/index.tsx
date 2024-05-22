2ecd320be1ef522c3bfb421946d44b2dec65de86
Thomas Versaevel <thomas0699@gmail.com>
2024-05-22 14:51:47 +0000

    Normalize app name in URL

--- packages/studio/pages/apps/app/edit/index.tsx
+++ packages/studio/pages/apps/app/edit/index.tsx
@@ -268,7 +268,7 @@ export default function EditPage(): ReactNode {
 
   useEffect(() => {
     setBreadCrumbsDecoration(
-      <Link id="guiEditorSwitch" to={`apps/${id}/${app.definition.name}/edit/gui/pages`}>
+      <Link id="guiEditorSwitch" to={`apps/${id}/${normalize(app.definition.name)}/edit/gui/pages`}>
         <Button className="button is-fullwidth is-rounded is-transparent is-bordered is-small">
           {`${formatMessage(messages.switchToGuiEditor)} ${formatMessage(messages.experimental)}`}
         </Button>
