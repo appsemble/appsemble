27cce7e9cdd13aaa020e85fbdedd00f34b203ad3
Prikshit Gautam <prikshit@appsemble.com>
2025-03-12 08:56:23 +0530

    docs(actions): fix app member docs link

--- packages/studio/pages/docs/actions/index.ts
+++ packages/studio/pages/docs/actions/index.ts
@@ -4,10 +4,17 @@ interface DocModule {
   default: MDXContent;
   searchIndex: [];
   title: string;
+  path: string;
 }
 export function importDocs(): DocModule[] {
   const mdxFiles = require.context('.', false, /\.mdx$/);
   const keys = mdxFiles.keys();
 
-  return keys.map((key) => mdxFiles(key));
+  return keys.map((key) => ({
+    ...mdxFiles(key),
+    path: key
+      .replace(/^\.\//, '')
+      .replace(/\.mdx?$/, '')
+      .toLowerCase(),
+  }));
 }
