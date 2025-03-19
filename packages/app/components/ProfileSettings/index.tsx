446c9e1a1b1cceefe142829288997175eadb86bf
Prikshit Gautam <prikshit@appsemble.com>
2025-03-19 10:49:10 +0530

    refactor(app): simplify changing the locale for an app member

--- packages/app/components/ProfileSettings/index.tsx
+++ packages/app/components/ProfileSettings/index.tsx
@@ -2,6 +2,7 @@ import {
   FileUpload,
   FormButtons,
   Icon,
+  SelectField,
   SimpleForm,
   SimpleFormError,
   SimpleFormField,
@@ -9,22 +10,27 @@ import {
   useMessages,
   WebcamImageUpload,
 } from '@appsemble/react-components';
+import { getLanguageDisplayName } from '@appsemble/utils';
 import axios from 'axios';
 import { type ReactNode, useCallback, useState } from 'react';
 import { FormattedMessage, useIntl } from 'react-intl';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
 import { messages } from './messages.js';
 import { PicturePreview } from './PicturePreview/index.js';
-import { apiUrl, appId } from '../../utils/settings.js';
+import { apiUrl, appId, languages } from '../../utils/settings.js';
 import { useAppMember } from '../AppMemberProvider/index.js';
 
 export function ProfileSettings(): ReactNode {
   const { formatMessage } = useIntl();
   const { appMemberInfo, setAppMemberInfo } = useAppMember();
-  const { lang } = useParams<{ lang: string }>();
   const push = useMessages();
   const [pictureCamera, setPictureCamera] = useState<Blob | null>(null);
+  const navigate = useNavigate();
+  const { lang } = useParams<{ lang: string }>();
+
+  const preferredLanguage =
+    appMemberInfo.locale || localStorage.getItem('preferredLanguage') || lang;
 
   const onCapture = useCallback(
     (data: Blob) => {
@@ -33,10 +39,10 @@ export function ProfileSettings(): ReactNode {
     [setPictureCamera],
   );
   const onSaveProfile = useCallback(
-    async (values: { name: string; picture: File }) => {
+    async (values: { name: string; picture: File; locale: string }) => {
       const formData = new FormData();
       formData.append('name', values.name);
-      formData.append('locale', lang);
+      formData.append('locale', values.locale);
 
       if (values.picture) {
         formData.append('picture', values.picture);
@@ -47,15 +53,19 @@ export function ProfileSettings(): ReactNode {
       const { data } = await axios.patch<{
         name: string;
         picture: string;
+        locale: string;
       }>(`${apiUrl}/api/apps/${appId}/members/current`, formData);
       setAppMemberInfo({
         ...appMemberInfo,
         name: data.name,
         picture: data.picture,
+        locale: data.locale,
       });
+      localStorage.setItem('preferredLanguage', data.locale);
       push({ body: formatMessage(messages.submitSuccess), color: 'success' });
+      navigate(`/${data.locale ?? lang}/Settings`, { replace: true });
     },
-    [formatMessage, push, pictureCamera, setAppMemberInfo, appMemberInfo, lang],
+    [formatMessage, lang, navigate, push, pictureCamera, setAppMemberInfo, appMemberInfo],
   );
 
   return (
@@ -63,6 +73,7 @@ export function ProfileSettings(): ReactNode {
       defaultValues={{
         name: appMemberInfo?.name || '',
         picture: null,
+        locale: preferredLanguage,
       }}
       onSubmit={onSaveProfile}
     >
@@ -94,6 +105,19 @@ export function ProfileSettings(): ReactNode {
         value={pictureCamera}
         videoButtonLabel={<FormattedMessage {...messages.clickPicture} />}
       />
+      <SimpleFormField
+        component={SelectField}
+        help={<FormattedMessage {...messages.languageDescription} />}
+        icon="language"
+        label={<FormattedMessage {...messages.languageLabel} />}
+        name="locale"
+      >
+        {languages.map((language) => (
+          <option key={language} value={language}>
+            {getLanguageDisplayName(language)}
+          </option>
+        ))}
+      </SimpleFormField>
       <FormButtons>
         <SimpleSubmit>
           <FormattedMessage {...messages.saveProfile} />
