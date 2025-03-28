2e10f60223d426d917b79d09d21436b93490518d
VasilVelikovDCentralize <vasil@d-centralize.nl>
2025-03-28 13:42:44 +0200

    handle notify action in dynamic tabs

--- packages/app/utils/actions/notify.ts
+++ packages/app/utils/actions/notify.ts
@@ -3,9 +3,22 @@ import axios from 'axios';
 import { type ActionCreator } from './index.js';
 import { apiUrl, appId } from '../settings.js';
 
-export const notify: ActionCreator<'notify'> = ({ prefixIndex }) => [
+export const notify: ActionCreator<'notify'> = ({ appDefinition, prefixIndex }) => [
   async (data) => {
-    const url = `${apiUrl}/api/apps/${appId}/actions/${prefixIndex}`;
+    const pageMatch = prefixIndex.match(/pages\.(\d+)/);
+    const pageNumber = pageMatch ? Number(pageMatch[1]) : null;
+
+    // Handle the prefixIndex for dynamically generated tabs
+    let prefix = prefixIndex;
+    if (
+      pageNumber &&
+      appDefinition.pages[pageNumber].type === 'tabs' &&
+      appDefinition.pages[pageNumber].definition.foreach
+    ) {
+      prefix = prefix.replace(/tabs\.\d+/, 'definition.foreach');
+    }
+
+    const url = `${apiUrl}/api/apps/${appId}/actions/${prefix}`;
     await axios.post(url, data || {});
 
     return data;
