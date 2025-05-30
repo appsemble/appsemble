c61a42a896236d981fd33e03a2f5da0f0db0ac06
Sam Philipsen <sam@d-centralize.nl>
2025-05-30 17:46:48 +0200

    Define chapter in training tree

--- packages/studio/components/TrainingTree/nodePositions.ts
+++ packages/studio/components/TrainingTree/nodePositions.ts
@@ -1,22 +1,27 @@
+import { type XYPosition } from 'reactflow';
+
 /**
  * Defines where the chapters should be placed on the grid
  */
-export const chapterNodes = [
+export const chapterNodes: { id: string; position: XYPosition }[] = [
   { id: 'introduction', position: { x: 0, y: 0 } },
   { id: 'how-to-create-an-app', position: { x: 0, y: 200 } },
   { id: 'data-flow', position: { x: 0, y: 500 } },
   { id: 'storing-data', position: { x: 0, y: 700 } },
   { id: 'data-transformation', position: { x: -200, y: 1050 } },
   { id: 'styling-apps', position: { x: 200, y: 1050 } },
+  { id: 'basic-app', position: { x: 0, y: 1400 } },
 ];
 
 /**
  * Defines the connections that get drawn between chapters
  */
-export const chapterEdges = [
+export const chapterEdges: { from: string; to: string }[] = [
   { from: 'introduction', to: 'how-to-create-an-app' },
   { from: 'how-to-create-an-app', to: 'data-flow' },
   { from: 'data-flow', to: 'storing-data' },
   { from: 'storing-data', to: 'data-transformation' },
   { from: 'storing-data', to: 'styling-apps' },
+  { from: 'styling-apps', to: 'basic-app' },
+  { from: 'data-transformation', to: 'basic-app' },
 ];
