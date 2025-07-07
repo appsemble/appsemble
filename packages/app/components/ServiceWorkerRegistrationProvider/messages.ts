928c926ea6147889529b838d0500998c3024a019
VasilVelikovDCentralize <vasil@d-centralize.nl>
2025-07-07 15:48:51 +0300

    handle service worker error

---
+++ packages/app/components/ServiceWorkerRegistrationProvider/messages.ts
@@ -0,0 +1,9 @@
+import { defineMessages } from 'react-intl';
+
+export const messages = defineMessages({
+  error: {
+    id: 'app.c7Slxf',
+    defaultMessage:
+      'You are using an in-app browser that doesn’t support all features such as notifications. For a better experience, open this page in a browser app.',
+  },
+});
