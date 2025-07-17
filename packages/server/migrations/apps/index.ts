84575c4662154e9d6aef377a394db9b470d95b59
VasilVelikovDCentralize <vasil@d-centralize.nl>
2025-07-17 13:06:28 +0300

    move migration 0.33.6 to app migrations

--- packages/server/migrations/apps/index.ts
+++ packages/server/migrations/apps/index.ts
@@ -1,4 +1,5 @@
 import * as m0_33_0 from './0.33.0.js';
+import * as m0_33_6 from './0.33.6.js';
 import { type Migration } from '../../utils/migrate.js';
 
-export const migrations: Migration[] = [m0_33_0];
+export const migrations: Migration[] = [m0_33_0, m0_33_6];
