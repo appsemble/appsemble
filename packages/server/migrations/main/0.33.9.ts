b56d61bf0e99b3017181647509db65708d2d317c
VasilVelikovDCentralize <vasil@d-centralize.nl>
2025-07-24 16:12:03 +0300

    add migrations 0.33.9 and 0.34.1 to help tracking app migration state in 0.34.0

---
+++ packages/server/migrations/main/0.33.9.ts
@@ -0,0 +1,30 @@
+import { logger } from '@appsemble/node-utils';
+import { DataTypes, type Sequelize, type Transaction } from 'sequelize';
+
+export const key = '0.33.9';
+
+/**
+ * Summary:
+ * - Add column `migratedAt` to `App` table
+ *
+ * Helps with tracking app migration status in 0.34.0
+ */
+
+export async function up(_transaction: Transaction, db: Sequelize): Promise<void> {
+  const queryInterface = db.getQueryInterface();
+
+  logger.info('Add column `migratedAt` to `App` table');
+  await queryInterface.addColumn('App', 'migratedAt', { type: DataTypes.DATE, allowNull: true });
+}
+
+/**
+ * Summary:
+ * - Remove column `migratedAt` from `App` table
+ */
+
+export async function down(_transaction: Transaction, db: Sequelize): Promise<void> {
+  const queryInterface = db.getQueryInterface();
+
+  logger.info('Remove column `migratedAt` from `App` table');
+  await queryInterface.removeColumn('App', 'migratedAt');
+}
