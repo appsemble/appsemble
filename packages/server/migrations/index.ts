95dc490898bdb4eafccb218500ebbf3c5fc2be54
Nikolay Gruychev <nikolay@appsemble.com>
2025-08-03 03:14:51 +0200

    feat(server): add index to BlockAsset.BlockVersionId for faster joins

--- packages/server/migrations/index.ts
+++ packages/server/migrations/index.ts
@@ -30,6 +30,7 @@ import * as m0_32_1_test_16 from './0.32.1-test.16.js';
 import * as m0_32_1_test_8 from './0.32.1-test.8.js';
 import * as m0_32_3_test_2 from './0.32.3-test.2.js';
 import * as m0_33_6 from './0.33.6.js';
+import * as m0_33_7 from './0.33.7.js';
 import { type Migration } from '../utils/migrate.js';
 
 export const migrations: Migration[] = [
@@ -65,5 +66,6 @@ export const migrations: Migration[] = [
   m0_32_1_test_16,
   m0_32_3_test_2,
   m0_33_6,
+  m0_33_7,
   // TODO Add m0_32_2 later for safety
 ];
