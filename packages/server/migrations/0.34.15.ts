3b00b1418599d50402e8fe9b7ac2d6dd8d000b54
Nikolay Gruychev <nikolay@appsemble.com>
2025-09-09 15:01:58 +0200

    fix: proper migration key

--- packages/server/migrations/0.34.15.ts
+++ packages/server/migrations/0.34.15.ts
@@ -1,7 +1,7 @@
 import { logger } from '@appsemble/node-utils';
 import { DataTypes, type Sequelize, type Transaction } from 'sequelize';
 
-export const key = '0.34.16';
+export const key = '0.34.15';
 
 /**
  * Summary:
