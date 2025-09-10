b672da90efd53300543e8b65d8c67ebe5f6c84c9
Sam Philipsen <sam@d-centralize.nl>
2025-09-10 17:50:06 +0200

    Isolate app collection tests

--- packages/e2e/fixtures/test/app-collection/index.ts
+++ packages/e2e/fixtures/test/app-collection/index.ts
@@ -1,3 +1,6 @@
+import { openAsBlob } from 'node:fs';
+
+import { type AppCollection, type AppCollectionVisibility } from '@appsemble/types';
 import { test as base } from '@playwright/test';
 
 import { expect } from '../../../index.js';
@@ -8,89 +11,106 @@ export interface AppCollectionFixtures {
    *
    * @param organizationId ID of the organization to create the collection for
    * @param collectionName Name of the collection
+   * @param visibility Visibility of the collection
    * @param expertName Name of the expert
+   * @param expertPhotoPath Path of the photo that represents the expert
+   * @param headerImagePath Path of the image that will be placed as the header of the collection
    * @param expertDescription Description of the expert
-   * @param headerImage Image that will be placed as the header of the collection
-   * @param expertPhoto Photo that represents the expert
-   * @param isPrivate Whether the collection should be private or not
    */
   createAppCollection: (
     organizationId: string,
     collectionName: string,
+    visibility: AppCollectionVisibility,
     expertName: string,
-    expertDescription: string,
-    headerImage: string,
-    expertPhoto: string,
-    isPrivate: boolean,
-  ) => Promise<void>;
+    expertPhotoPath: string,
+    headerImagePath: string,
+    expertDescription?: string,
+  ) => Promise<AppCollection>;
 
   /**
    * Delete an existing app collection
    *
-   * @param organizationId ID of the organization which the collection belongs to
-   * @param collectionName Name of the collection
+   * @param collectionId ID of the collection to delete
    */
-  deleteAppCollection: (organizationId: string, collectionName: string) => Promise<void>;
+  deleteAppCollection: (collectionId: number) => Promise<void>;
+
+  /**
+   * Adds an app to an app collection
+   *
+   * @param collectionId ID of the app collection
+   * @param appId ID of the app to add
+   */
+  addAppToAppCollection: (collectionId: number, appId: number) => Promise<void>;
+}
+
+async function getBlobFromPath(imagePath: string): Promise<Blob> {
+  const extension = imagePath.split('.').pop();
+  const blob = await openAsBlob(imagePath, { type: `image/${extension}` });
+  return blob;
 }
 
 export const test = base.extend<AppCollectionFixtures>({
-  async createAppCollection({ page }, use) {
+  async createAppCollection({ request }, use) {
     await use(
       async (
         organizationId,
         collectionName,
+        visibility,
         expertName,
+        expertPhotoPath,
+        headerImagePath,
         expertDescription,
-        headerImage,
-        expertPhoto,
-        isPrivate,
       ) => {
-        await page.goto(`/en/organizations/${organizationId}/collections`);
+        const formData = new FormData();
+        formData.append('name', collectionName);
+        formData.append('visibility', visibility);
+        formData.append('expertName', expertName);
+        formData.append('expertProfileImage', await getBlobFromPath(expertPhotoPath));
+        formData.append('headerImage', await getBlobFromPath(headerImagePath));
 
-        await page.getByRole('link', { name: 'App collections' }).first().click();
-        await page.getByRole('button', { name: 'Create new app collection' }).click();
-
-        await page.getByLabel('Name', { exact: true }).fill(collectionName);
-        await page
-          .locator('span')
-          .filter({ hasText: 'Header image' })
-          .first()
-          .setInputFiles(headerImage);
-
-        if (isPrivate) {
-          await page.getByText('Hide this app collection from the public list').click();
+        if (expertDescription) {
+          formData.append('expertDescription', expertDescription);
         }
 
-        await page.getByLabel('Expert name').fill(expertName);
-        await page.getByLabel('Expert description').fill(expertDescription);
+        const response = await request.post(
+          `/api/organizations/${organizationId}/app-collections`,
+          {
+            multipart: formData,
+            headers: {
+              Authorization: `Bearer ${process.env.ACCESS_TOKEN}`,
+            },
+          },
+        );
+        expect(response.status()).toBe(201);
 
-        await page
-          .locator('span')
-          .filter({ hasText: 'Expert photo' })
-          .first()
-          .setInputFiles(expertPhoto);
-
-        // Avatar editor
-        await page.getByRole('button', { name: 'Save' }).click();
-
-        await page.getByRole('button', { name: 'Create app collection' }).click();
-
-        await expect(page.getByRole('heading', { name: collectionName })).toBeVisible();
+        const appCollection = (await response.json()) as unknown as AppCollection;
+        return appCollection;
       },
     );
   },
 
-  async deleteAppCollection({ page }, use) {
-    await use(async (organizationId, collectionName) => {
-      await page.goto(`/en/organizations/${organizationId}/collections`);
+  async deleteAppCollection({ request }, use) {
+    await use(async (collectionId) => {
+      const response = await request.delete(`/api/app-collections/${collectionId}`, {
+        headers: {
+          Authorization: `Bearer ${process.env.ACCESS_TOKEN}`,
+        },
+      });
+      expect(response.status()).toBe(204);
+    });
+  },
 
-      await page.getByRole('link', { name: collectionName }).click();
-      await page.getByRole('link', { name: 'Settings' }).click();
-
-      await page.getByRole('button', { name: 'Delete app collection' }).click();
-      await page.getByRole('button', { name: 'Delete app collection', exact: true }).click();
-
-      await expect(page.getByRole('heading', { name: collectionName })).toBeHidden();
+  async addAppToAppCollection({ request }, use) {
+    await use(async (collectionId, appId) => {
+      const response = await request.post(`/api/app-collections/${collectionId}/apps`, {
+        data: {
+          AppId: appId,
+        },
+        headers: {
+          Authorization: `Bearer ${process.env.ACCESS_TOKEN}`,
+        },
+      });
+      expect(response.status()).toBe(204);
     });
   },
 });
