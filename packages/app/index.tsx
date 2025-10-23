2d63e7f3c946c96d099b19de87f801461b4791ca
VasilVelikovDCentralize <vasil@d-centralize.nl>
2025-10-23 11:27:18 +0300

    gracefully handle service worker errors

--- packages/app/index.tsx
+++ packages/app/index.tsx
@@ -7,9 +7,21 @@ import { sentryDsn, sentryEnvironment } from './utils/settings.js';
 
 setupSentry(sentryDsn, sentryEnvironment);
 
-const serviceWorkerRegistrationPromise =
-  navigator.serviceWorker?.register('/service-worker.js') ||
-  Promise.reject(new Error('Service worker not available'));
+let serviceWorkerRegistration: ServiceWorkerRegistration | null = null;
+
+if ('serviceWorker' in navigator) {
+  try {
+    serviceWorkerRegistration = await navigator.serviceWorker.register('/service-worker.js');
+  } catch (err) {
+    // eslint-disable-next-line no-console
+    console.warn('Service worker registration failed:', err);
+  }
+} else {
+  // eslint-disable-next-line no-console
+  console.warn('Service workers not supported in this environment.');
+}
+
+const serviceWorkerRegistrationPromise = Promise.resolve(serviceWorkerRegistration);
 
 createRoot(document.getElementById('app')!).render(
   <App serviceWorkerRegistrationPromise={serviceWorkerRegistrationPromise} />,
