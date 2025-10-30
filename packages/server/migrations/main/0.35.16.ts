98a6e7ddcc060b152d37b19af41c3c14be318d73
ante <ante@d-centralize.nl>
2025-10-30 14:27:10 +0100

    fix: add locale to organization
    Added locale column to organization table.
    
    refs: #1811

---
+++ packages/server/migrations/main/0.35.16.ts
@@ -0,0 +1,42 @@
+import { logger } from '@appsemble/node-utils';
+import { DataTypes, type Sequelize, type Transaction } from 'sequelize';
+
+export const key = '0.35.16';
+
+/**
+ * Summary:
+ * - Add column locale to table `Organization`
+ *
+ * @param transaction The sequelize Transaction.
+ * @param db The sequelize database.
+ */
+
+export async function up(transaction: Transaction, db: Sequelize): Promise<void> {
+  const queryInterface = db.getQueryInterface();
+
+  logger.info('Add column `locale` to table `Organization`');
+  await queryInterface.addColumn(
+    'Organization',
+    'locale',
+    {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: 'en',
+    },
+    { transaction },
+  );
+}
+
+/**
+ * Summary:
+ * - Remove column locale from table `Organization`
+ *
+ * @param transaction The sequelize Transaction.
+ * @param db The sequelize database.
+ */
+
+export async function down(transaction: Transaction, db: Sequelize): Promise<void> {
+  const queryInterface = db.getQueryInterface();
+  logger.info('Remove column locale from `Organization` table');
+  await queryInterface.removeColumn('Organization', 'locale', { transaction });
+}
