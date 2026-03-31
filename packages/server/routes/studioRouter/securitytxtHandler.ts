546406794e189d453e936ccc75381493a4beece7
Prikshit Gautam <prikshit@appsemble.com>
2026-03-31 12:16:00 +0530

    refactor: use dynamic data for security txt

--- packages/server/routes/studioRouter/securitytxtHandler.ts
+++ packages/server/routes/studioRouter/securitytxtHandler.ts
@@ -1,6 +1,17 @@
-import { readAsset } from '@appsemble/node-utils';
 import { type Context } from 'koa';
 
-export async function securitytxtHandler(ctx: Context): Promise<void> {
-  ctx.body = await readAsset('security.txt', 'utf8');
+import securitytxtDefaults from '../../assets/securitytxtDefaults.json' with { type: 'json' };
+
+import { argv } from '../../utils/argv.js';
+
+export function securitytxtHandler(ctx: Context): void {
+  ctx.type = 'text/plain; charset=utf-8';
+  ctx.body = [
+    `Contact: mailto:${argv.securityEmail}`,
+    `Expires: ${securitytxtDefaults.expires}`,
+    `Canonical: ${ctx.URL.origin}${securitytxtDefaults.canonicalPath}`,
+    `Policy: ${securitytxtDefaults.policy}`,
+    `Preferred-Languages: ${securitytxtDefaults.preferredLanguages}`,
+    '',
+  ].join('\n');
 }
