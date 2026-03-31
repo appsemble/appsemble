9a8bcacc1a431576e46c7e300518e08110654d78
Kees van den Broek <kvdb@d-centralize.nl>
2026-03-31 06:55:22 +0200

    fix: strip bootstrapped security permissions

--- packages/server/options/createSettings.ts
+++ packages/server/options/createSettings.ts
@@ -87,6 +87,7 @@ export async function createSettings({
 
   const persistedApp = (await App.findOne({
     attributes: [
+      'definition',
       'id',
       'icon',
       'updated',
@@ -160,7 +161,10 @@ export async function createSettings({
         })),
       ],
       vapidPublicKey: persistedApp.vapidPublicKey,
-      definition: sanitizeAppDefinitionForPublicSettings(app.definition, app.showAppDefinition),
+      definition: sanitizeAppDefinitionForPublicSettings(
+        persistedApp.definition,
+        app.showAppDefinition,
+      ),
       snapshotId: persistedApp.AppSnapshots?.[0]?.id,
       demoMode: persistedApp.demoMode,
       showAppsembleLogin: persistedApp.showAppsembleLogin ?? false,
